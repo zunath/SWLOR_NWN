cb913b654ffacd50bda8df687827cace2fc2ee18
zunath <coolty3001@yahoo.com>
2018-09-02 13:08:10 +0800

    Add custom phenotypes enumeration. Add ACP fighting animations to 2das. Add CEP to module.

---
+++ SWLOR.Game.Server/Enumeration/CustomPhenoType.cs
@@ -0,0 +1,22 @@
+﻿namespace SWLOR.Game.Server.Enumeration
+{
+    public enum CustomPhenoType
+    {
+        Normal = 0,
+        Large = 2,
+        Normal_M = 3,
+        N_Joust_M = 4,
+        L_Joust_M = 8,
+        Kensai = 15,
+        Assassin = 16,
+        Heavy = 17,
+        Fencing = 18,
+        Arcane = 19,
+        Demonblade = 20,
+        Warrior = 21,
+        MA_TigerFang=30,
+        MA_SunFist = 31,
+        MA_DragonPalm = 32,
+        MA_BearClaw = 33
+    }
+}

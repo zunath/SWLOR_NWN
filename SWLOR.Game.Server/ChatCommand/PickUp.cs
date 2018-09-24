32fe2e9f5dbe1e697782234eb238a7db95f09042
zunath <coolty3001@yahoo.com>
2018-09-24 17:48:52 +0800

    Add a bunch of emotes and other chat commands.

---
+++ SWLOR.Game.Server/ChatCommand/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using NWN;
+using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Plays a pick up animation.", CommandPermissionType.Player | CommandPermissionType.DM)]
+    public class PickUp : IChatCommand
+    {
+        private readonly INWScript _;
+
+        public PickUp(INWScript script)
+        {
+            _ = script;
+        }
+
+        public void DoAction(NWPlayer user, params string[] args)
+        {
+            user.AssignCommand(() =>
+            {
+                _.ActionPlayAnimation(ANIMATION_LOOPING_GET_LOW);
+            });
+        }
+    }
+}

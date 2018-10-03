84c074998b4452214313d1029f5984a0b306a6ae
zunath <coolty3001@yahoo.com>
2018-10-03 14:24:31 +0800

    Check for key item when accessing teleport objects, if necessary.

--- SWLOR.Game.Server/Placeable/WarpDevice/OnUsed.cs
+++ SWLOR.Game.Server/Placeable/WarpDevice/OnUsed.cs
@@ -2,6 +2,7 @@
 using SWLOR.Game.Server.GameObject;
 
 using NWN;
+using SWLOR.Game.Server.Service.Contracts;
 using static NWN.NWScript;
 
 namespace SWLOR.Game.Server.Placeable.WarpDevice
@@ -9,15 +10,19 @@ namespace SWLOR.Game.Server.Placeable.WarpDevice
     public class OnUsed: IRegisteredEvent
     {
         private readonly INWScript _;
+        private readonly IKeyItemService _keyItem;
 
-        public OnUsed(INWScript script)
+        public OnUsed(
+            INWScript script,
+            IKeyItemService keyItem)
         {
             _ = script;
+            _keyItem = keyItem;
         }
 
         public bool Run(params object[] args)
         {
-            NWObject oPC = (_.GetLastUsedBy());
+            NWPlayer oPC = _.GetLastUsedBy();
 
             if (_.GetIsInCombat(oPC) == TRUE)
             {
@@ -25,9 +30,28 @@ namespace SWLOR.Game.Server.Placeable.WarpDevice
                 return false;
             }
 
-            NWPlaceable self = (Object.OBJECT_SELF);
+            NWPlaceable self = Object.OBJECT_SELF;
             string destination = self.GetLocalString("DESTINATION");
             int visualEffectID = self.GetLocalInt("VISUAL_EFFECT");
+            int keyItemID = self.GetLocalInt("KEY_ITEM_ID");
+            string missingKeyItemMessage = self.GetLocalString("MISSING_KEY_ITEM_MESSAGE");
+
+            if (keyItemID > 0)
+            {
+                if (!_keyItem.PlayerHasKeyItem(oPC, keyItemID))
+                {
+                    if (!string.IsNullOrWhiteSpace(missingKeyItemMessage))
+                    {
+                        oPC.SendMessage(missingKeyItemMessage);
+                    }
+                    else
+                    {
+                        oPC.SendMessage("You don't have the necessary key item to access that object.");
+                    }
+
+                    return false;
+                }
+            }
 
             if (visualEffectID > 0)
             {

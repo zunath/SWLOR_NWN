e82e70673bad9db234281864e9ca3715e248abdd
zunath <coolty3001@yahoo.com>
2018-10-11 21:20:54 +0800

    Add planetary impound retrieval NPC and dialogue.

--- SWLOR.Game.Server/Service/Contracts/ISerializationService.cs
+++ SWLOR.Game.Server/Service/Contracts/ISerializationService.cs
@@ -8,6 +8,7 @@ namespace SWLOR.Game.Server.Service.Contracts
         NWCreature DeserializeCreature(string base64String, Location location);
         NWItem DeserializeItem(string base64String, Location targetLocation);
         NWItem DeserializeItem(string base64String, NWPlaceable target);
+        NWItem DeserializeItem(string base64String, NWCreature target);
         string Serialize(NWObject @object);
     }
 }

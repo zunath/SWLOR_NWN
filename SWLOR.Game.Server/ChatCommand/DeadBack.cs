ffa5f4a14b2db26fcfe1a7b9e16fe198ee39168c
zunath <coolty3001@yahoo.com>
2018-10-12 18:30:07 +0800

    Add a few looping animations which take an optional duration parameter.

---
+++ SWLOR.Game.Server/ChatCommand/DeadBack.cs
@@ -0,0 +1,27 @@
+﻿using NWN;
+using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Plays a dead back animation.", CommandPermissionType.Player | CommandPermissionType.DM)]
+    public class DeadBack : LoopingAnimationCommand
+    {
+        private readonly INWScript _;
+
+        public DeadBack(INWScript script)
+        {
+            _ = script;
+        }
+
+        protected override void DoAction(NWPlayer user, float duration)
+        {
+            user.AssignCommand(() =>
+            {
+                _.ActionPlayAnimation(ANIMATION_LOOPING_DEAD_BACK, 1.0f, duration);
+            });
+        }
+    }
+}
---
+++ SWLOR.Game.Server/ChatCommand/Listen.cs
@@ -0,0 +1,27 @@
+﻿using NWN;
+using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Plays a listen animation.", CommandPermissionType.Player | CommandPermissionType.DM)]
+    public class Listen : LoopingAnimationCommand
+    {
+        private readonly INWScript _;
+
+        public Listen(INWScript script)
+        {
+            _ = script;
+        }
+        
+        protected override void DoAction(NWPlayer user, float duration)
+        {
+            user.AssignCommand(() =>
+            {
+                _.ActionPlayAnimation(ANIMATION_LOOPING_LISTEN, 1.0f, duration);
+            });
+        }
+    }
+}

1823df65b721faa754321b9efc3c2d674fe287b8
Liareth <liarethnwn@gmail.com>
2018-10-22 16:07:33 +0100

    Add the EmoteStyle service.

---
+++ SWLOR.Game.Server/Service/Contracts/IEmoteStyleService.cs
@@ -0,0 +1,11 @@
+﻿using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+
+namespace SWLOR.Game.Server.Service.Contracts
+{
+    public interface IEmoteStyleService
+    {
+        EmoteStyle GetEmoteStyle(NWObject obj);
+        void SetEmoteStyle(NWObject obj, EmoteStyle style);
+    }
+}

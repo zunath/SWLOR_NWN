77469b60afc1e341562b3d6af30b6dab91b77c3a
zunath <coolty3001@yahoo.com>
2018-10-24 17:36:01 +0800

    Replace IsStance with CustomEffectCategoryID in CustomEffects.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-10-24.2.sql
@@ -0,0 +1,50 @@
+﻿
+CREATE TABLE CustomEffectCategory(
+	CustomEffectCategoryID INT NOT NULL PRIMARY KEY,
+	Name NVARCHAR(32) NOT NULL DEFAULT ''
+)
+
+GO 
+
+ALTER TABLE dbo.CustomEffects
+ADD CustomEffectCategoryID INT NOT NULL DEFAULT 1
+GO
+
+INSERT INTO dbo.CustomEffectCategory ( CustomEffectCategoryID ,
+                                   Name )
+VALUES ( 1 , -- CustomEffectCategoryID - int
+         N'Normal Effect' -- Name - nvarchar(32)
+    )
+
+INSERT INTO dbo.CustomEffectCategory ( CustomEffectCategoryID ,
+                                   Name )
+VALUES ( 2 , -- CustomEffectCategoryID - int
+         N'Stance' -- Name - nvarchar(32)
+    )
+
+INSERT INTO dbo.CustomEffectCategory ( CustomEffectCategoryID ,
+                                   Name )
+VALUES ( 3 , -- CustomEffectCategoryID - int
+         N'Food Effect' -- Name - nvarchar(32)
+    )
+
+GO
+
+ALTER TABLE dbo.CustomEffects
+ADD CONSTRAINT FK_CustomEffects_CustomEffectCategoryID FOREIGN KEY(CustomEffectCategoryID)
+	REFERENCES dbo.CustomEffectCategory(CustomEffectCategoryID)
+
+GO
+
+EXEC dbo.ADM_Drop_Column @TableName = N'CustomEffects' , -- nvarchar(200)
+                         @ColumnName = N'IsStance'  -- nvarchar(200)
+
+--GO 
+
+UPDATE dbo.CustomEffects
+SET CustomEffectCategoryID = 2
+WHERE CustomEffectID IN (
+15,
+22,
+23
+)
---
+++ SWLOR.Game.Server/Enumeration/CustomEffectCategoryType.cs
@@ -0,0 +1,9 @@
+﻿namespace SWLOR.Game.Server.Enumeration
+{
+    public enum CustomEffectCategoryType
+    {
+        NormalEffect = 1,
+        Stance = 2,
+        FoodEffect = 3
+    }
+}

c23ee0c344c2bdb0831411002c451a2e920b6a28
zunath <coolty3001@yahoo.com>
2018-10-24 16:56:52 +0800

    Fix DB migration logic error when determining filter on updates. Stub out a food item script. Add a proc for more easily dropping constraints on columns.

---
+++ SWLOR.Game.Server/Item/Food.cs
@@ -0,0 +1,55 @@
+﻿using NWN;
+using SWLOR.Game.Server.GameObject;
+using SWLOR.Game.Server.Item.Contracts;
+using SWLOR.Game.Server.ValueObject;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.Item
+{
+    public class Food: IActionItem
+    {
+        public CustomData StartUseItem(NWCreature user, NWItem item, NWObject target, Location targetLocation)
+        {
+            return null;
+        }
+
+        public void ApplyEffects(NWCreature user, NWItem item, NWObject target, Location targetLocation, CustomData customData)
+        {
+        }
+
+        public float Seconds(NWCreature user, NWItem item, NWObject target, Location targetLocation, CustomData customData)
+        {
+            return 4.5f;
+        }
+
+        public bool FaceTarget()
+        {
+            return false;
+        }
+
+        public int AnimationID()
+        {
+            return ANIMATION_FIREFORGET_SALUTE;
+        }
+
+        public float MaxDistance(NWCreature user, NWItem item, NWObject target, Location targetLocation)
+        {
+            return 0;
+        }
+
+        public bool ReducesItemCharge(NWCreature user, NWItem item, NWObject target, Location targetLocation, CustomData customData)
+        {
+            return false;
+        }
+
+        public string IsValidTarget(NWCreature user, NWItem item, NWObject target, Location targetLocation)
+        {
+            return null;
+        }
+
+        public bool AllowLocationTarget()
+        {
+            return false;
+        }
+    }
+}

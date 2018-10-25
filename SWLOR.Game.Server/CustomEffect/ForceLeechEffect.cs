362c8f4c0b8645110f883d2532ce0dcb56dca6dd
Jesse Fulton <jesseafulton01@yahoo.com>
2018-10-25 04:16:54 -0400

    Force Leech Added
    ---
    ForceLeech.cs
    ForceLeechEffect.cs
    Force Leech .sql migration info added.
    feats/spells/cls_feat_fight 2da's Updated.
    swlor_talk.tlk updated.
    ---

---
+++ SWLOR.Game.Server/CustomEffect/ForceLeechEffect.cs
@@ -0,0 +1,53 @@
+﻿using NWN;
+using SWLOR.Game.Server.CustomEffect.Contracts;
+using SWLOR.Game.Server.GameObject;
+using System;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.CustomEffect
+{
+    public class ForceLeechEffect : ICustomEffect
+    {
+        private readonly INWScript _;
+
+        public ForceLeechEffect(INWScript script)
+        {
+            _ = script;
+        }
+
+        public string Apply(NWCreature oCaster, NWObject oTarget, int effectiveLevel)
+        {
+            return null;
+        }
+
+        public void Tick(NWCreature oCaster, NWObject oTarget, int currentTick, int effectiveLevel, string data)
+        {
+            string[] split = data.Split(',');
+            int damage = Convert.ToInt32(split[0]);
+            float percent = Convert.ToSingle(split[1]);
+            oCaster.AssignCommand(() =>
+            {
+                Effect effect = _.EffectDamage(damage, DAMAGE_TYPE_NEGATIVE);
+                _.ApplyEffectToObject(DURATION_TYPE_INSTANT, effect, oTarget);
+            });
+
+            // Bail out early if oCaster is no longer valid (player logged out)
+            if (oCaster == null || !oCaster.IsValid) return;
+
+            int heal = (int)(damage * percent);
+            if (heal < 1) heal = 1;
+
+            oCaster.AssignCommand(() =>
+            {
+                Effect healEffect = _.EffectHeal(heal);
+                _.ApplyEffectToObject(NWScript.DURATION_TYPE_INSTANT, healEffect, oCaster);
+
+            });
+
+        }
+
+        public void WearOff(NWCreature oCaster, NWObject oTarget, int effectiveLevel, string data = "")
+        {
+        }
+    }
+}

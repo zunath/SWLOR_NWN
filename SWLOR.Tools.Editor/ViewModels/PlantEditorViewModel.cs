26e42fc49250bd3b6d872db030dc7e19de94230a
zunath <coolty3001@yahoo.com>
2018-10-30 23:39:31 +0800

    Plant editor

---
+++ SWLOR.Tools.Editor/ViewModels/PlantEditorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using SWLOR.Tools.Editor.ViewModels.Contracts;
+using SWLOR.Tools.Editor.ViewModels.Data;
+
+namespace SWLOR.Tools.Editor.ViewModels
+{
+    public class PlantEditorViewModel : BaseEditorViewModel<PlantViewModel>, IPlantEditorViewModel
+    {
+        public PlantEditorViewModel(
+            IEventAggregator eventAggregator,
+            IObjectListViewModel<PlantViewModel> objListVM)
+            : base(eventAggregator, objListVM)
+        {
+        }
+
+        protected override PlantViewModel CreateNew()
+        {
+            var obj = new PlantViewModel();
+            return obj;
+        }
+    }
+}

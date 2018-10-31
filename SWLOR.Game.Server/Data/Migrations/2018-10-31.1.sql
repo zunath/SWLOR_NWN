1607a016e14e338bd83e413f47c89f090a63e2f3
zunath <coolty3001@yahoo.com>
2018-10-31 23:41:12 +0800

    Adjust loot tables on kath hounds. Move the scripts folder into the Data directory.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-10-31.1.sql
@@ -0,0 +1,34 @@
+﻿
+INSERT INTO dbo.LootTables ( LootTableID ,
+                             Name )
+VALUES ( 44 , -- LootTableID - int
+         N'Kath Hounds - Rare Loot' -- Name - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItems ( LootTableID ,
+                                 Resref ,
+                                 MaxQuantity ,
+                                 Weight ,
+                                 IsActive ,
+                                 SpawnRule )
+VALUES ( 44 ,    -- LootTableID - int
+         'k_hound_fur' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         10 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItems ( LootTableID ,
+                                 Resref ,
+                                 MaxQuantity ,
+                                 Weight ,
+                                 IsActive ,
+                                 SpawnRule )
+VALUES ( 44 ,    -- LootTableID - int
+         'k_hound_tooth' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         10 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )

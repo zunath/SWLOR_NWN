4d5b0b968e66693d1ffa11bf9a0ffe2904fd4d29
zunath <coolty3001@yahoo.com>
2018-11-01 22:32:07 +0800

    Process packages in order of their export date.

--- SWLOR.Game.Server/Service/DataPackageService.cs
+++ SWLOR.Game.Server/Service/DataPackageService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using FluentValidation.Results;
+﻿using FluentValidation.Results;
 using Newtonsoft.Json;
 using SWLOR.Game.Server.Data;
 using SWLOR.Game.Server.Data.Contracts;
@@ -11,13 +6,19 @@ using SWLOR.Game.Server.Data.Processor;
 using SWLOR.Game.Server.Extension;
 using SWLOR.Game.Server.Service.Contracts;
 using SWLOR.Game.Server.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace SWLOR.Game.Server.Service
 {
     public class DataPackageService : IDataPackageService
     {
         private readonly IDataContext _db;
-        
+        const string PackagesPath = "./DataPackages/";
+
         public DataPackageService(IDataContext db)
         {
             _db = db;
@@ -25,49 +26,22 @@ namespace SWLOR.Game.Server.Service
 
         public void OnModuleLoad()
         {
-            const string PackagesPath = "./DataPackages/";
-            
             // Look for an existing DataPackages folder. If it's missing, create it.
-            if(!Directory.Exists(PackagesPath))
+            if (!Directory.Exists(PackagesPath))
             {
                 Directory.CreateDirectory(PackagesPath);
             }
 
-            // Pull back all of the packages we've already attempted to import.
-            var importedPackages = _db.DataPackages.ToList();
-
             // Enumerate all packages in the directory.
             // We will process these later in order by their export date.
             // In other words, processing occurs from oldest to newest.
-            string[] files = Directory.GetFiles(PackagesPath, "*.json");
-            foreach (var file in files)
+            var packages = BuildPackageList();
+
+            foreach (var package in packages)
             {
-                string checksum;
-
-                using (var sha1 = SHA1.Create())
-                {
-                    using (var stream = File.OpenRead(file))
-                    {
-                        var hash = sha1.ComputeHash(stream);
-                        checksum = BitConverter.ToString(hash).Replace("-", "").ToLower();
-                    }
-                }
-                
-                // If a file with this checksum has already been imported, move to the next one.
-                if (importedPackages.FirstOrDefault(x => x.Checksum == checksum) != null)
-                    continue;
-                
-                DataPackage package = new DataPackage
-                {
-                    Checksum = checksum,
-                    DateFound = DateTime.UtcNow,
-                    FileName = Path.GetFileName(file),
-                    ErrorMessage = string.Empty
-                };
-
                 try
                 {
-                    package.Content = File.ReadAllText(file);
+                    package.Content = File.ReadAllText(PackagesPath + package.FileName);
                     DataPackageFile dpf = JsonConvert.DeserializeObject<DataPackageFile>(package.Content);
                     package.DateExported = dpf.ExportDate;
                     package.PackageName = dpf.PackageName;
@@ -83,10 +57,10 @@ namespace SWLOR.Game.Server.Service
                         package.ImportedSuccessfully = true;
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     package.ErrorMessage = ex.ToMessageAndCompleteStacktrace();
-                    package.ImportedSuccessfully = false;   
+                    package.ImportedSuccessfully = false;
                 }
 
                 _db.DataPackages.Add(package);
@@ -98,10 +72,49 @@ namespace SWLOR.Game.Server.Service
                 }
                 else
                 {
-                    File.WriteAllText("IMPORT_FAILURE_" + DateTime.UtcNow.ToString("yyyy-dd-M--HH-mm-ss") + ".log", package.ErrorMessage);
+                    File.WriteAllText(PackagesPath + "IMPORT_FAILURE_" + package.FileName + "_" + DateTime.UtcNow.ToString("yyyy-dd-M--HH-mm-ss") + ".log", package.ErrorMessage);
                     Console.WriteLine("FAILURE: Package " + package.PackageName + " failed to import. Check the logs for errors.");
                 }
             }
+        }
+
+        private List<DataPackage> BuildPackageList()
+        {
+            // Pull back all of the packages we've already attempted to import.
+            var importedPackages = _db.DataPackages.ToList();
+
+            List<DataPackage> packages = new List<DataPackage>();
+            string[] files = Directory.GetFiles(PackagesPath, "*.json");
+            foreach (var file in files)
+            {
+                string checksum;
+                using (var sha1 = SHA1.Create())
+                {
+                    using (var stream = File.OpenRead(file))
+                    {
+                        var hash = sha1.ComputeHash(stream);
+                        checksum = BitConverter.ToString(hash).Replace("-", "").ToLower();
+                    }
+                }
+
+                // If a file with this checksum has already been imported, move to the next one.
+                if (importedPackages.FirstOrDefault(x => x.Checksum == checksum) != null)
+                    continue;
+
+                DataPackage package = new DataPackage
+                {
+                    Checksum = checksum,
+                    DateFound = DateTime.UtcNow,
+                    FileName = Path.GetFileName(file),
+                    ErrorMessage = string.Empty, 
+                    PackageName = string.Empty, 
+                    Content = string.Empty
+                };
+
+                packages.Add(package);
+            }
+
+            return packages.OrderBy(o => o.DateExported).ToList();
         }
 
         private string ProcessDataPackageFile(DataPackageFile dpf)
@@ -157,7 +170,7 @@ namespace SWLOR.Game.Server.Service
                     errors += ValidateAndProcess(db, new SkillProcessor(), obj) + "\n";
                 foreach (var obj in dpf.Spawns)
                     errors += ValidateAndProcess(db, new SpawnProcessor(), obj) + "\n";
-                
+
                 // Nothing in the package gets committed to the database if any error occurs.
                 if (string.IsNullOrWhiteSpace(errors))
                 {
@@ -167,31 +180,31 @@ namespace SWLOR.Game.Server.Service
 
             return errors;
         }
-        
+
         private string ValidateAndProcess<T>(IDataContext db, IDataProcessor<T> processor, T dataObject)
         {
             string errors = string.Empty;
-            
-                ValidationResult validationResult = processor.Validator.Validate(dataObject);
 
-                if (!validationResult.IsValid)
+            ValidationResult validationResult = processor.Validator.Validate(dataObject);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
                 {
-                    foreach (var error in validationResult.Errors)
-                    {
-                        errors += error.ErrorMessage + "\n";
-                    }
+                    errors += error.ErrorMessage + "\n";
                 }
-                else
+            }
+            else
+            {
+                try
                 {
-                    try
-                    {
-                        processor.Process(db, dataObject);
-                    }
-                    catch (Exception ex)
-                    {
-                        errors += "Failed to process object of type: " + dataObject.GetType() + " Reason: " + ex.ToMessageAndCompleteStacktrace();
-                    }
-                };
+                    processor.Process(db, dataObject);
+                }
+                catch (Exception ex)
+                {
+                    errors += "Failed to process object of type: " + dataObject.GetType() + " Reason: " + ex.ToMessageAndCompleteStacktrace();
+                }
+            };
 
             return errors;
         }

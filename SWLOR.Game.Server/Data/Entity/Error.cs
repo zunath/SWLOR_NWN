984404dfebf4ef4dca590eca997c2a0d6853240d
zunath <coolty3001@yahoo.com>
2018-11-08 16:28:21 +0800

    Log errors to the database.

---
+++ SWLOR.Game.Server/Data/Entity/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SWLOR.Game.Server.Data.Contracts;
+
+namespace SWLOR.Game.Server.Data.Entity
+{
+    [Table("[Error]")]
+    public class Error: IEntity
+    {
+        public Error()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        [ExplicitKey]
+        public Guid ID { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        public string Caller { get; set; }
+    }
+}
--- SWLOR.Game.Server/Service/ErrorService.cs
+++ SWLOR.Game.Server/Service/ErrorService.cs
@@ -1,21 +1,42 @@
 ﻿using System;
+using SWLOR.Game.Server.Data.Entity;
+using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.Extension;
 using SWLOR.Game.Server.Service.Contracts;
+using SWLOR.Game.Server.ValueObject;
 
 namespace SWLOR.Game.Server.Service
 {
     public class ErrorService: IErrorService
     {
+        private readonly IDataService _data;
+
+        public ErrorService(IDataService data)
+        {
+            _data = data;
+        }
+
         public void LogError(Exception ex, string @event = "")
         {
-            string message = ex.ToMessageAndCompleteStacktrace();
+            string stackTrace = ex.ToMessageAndCompleteStacktrace();
 
-            message = "*****************" + Environment.NewLine +
+            stackTrace = "*****************" + Environment.NewLine +
                       "EVENT ERROR (C#)" + Environment.NewLine +
                       (string.IsNullOrWhiteSpace(@event) ? string.Empty : @event + Environment.NewLine) +
                       "*****************" + Environment.NewLine +
-                      " EXCEPTION:" + Environment.NewLine + Environment.NewLine + message;
-            Console.WriteLine(message); // todo: log in database
+                      " EXCEPTION:" + Environment.NewLine + Environment.NewLine + stackTrace;
+            Console.WriteLine(stackTrace);
+
+            Error log = new Error
+            {
+                DateCreated = DateTime.UtcNow, 
+                Caller = @event, 
+                Message = ex.Message,
+                StackTrace = stackTrace
+            };
+            DatabaseAction action = new DatabaseAction(log, DatabaseActionType.Insert);
+            // Bypass the caching logic and directly enqueue the insert.
+            _data.DataQueue.Enqueue(action);
         }
     }
 }

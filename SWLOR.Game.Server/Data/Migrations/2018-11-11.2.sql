cb3024ac01dfc32a3f909cc8b3b4160619eb9151
zunath <coolty3001@yahoo.com>
2018-11-11 22:23:45 +0800

    Reduce base levels on tier 1+ weaponsmith and armorsmith blueprints.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-11.2.sql
@@ -0,0 +1,16 @@
+﻿
+-- Cut Tier 1+ item base levels in half for weaponsmith
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = BaseLevel / 2
+WHERE SkillID = 12
+	AND ItemName NOT LIKE '%repair kit%'
+	AND CraftCategoryID NOT IN (13)
+	AND ItemName NOT LIKE '%basic%'
+
+-- Cut Tier 1+ item base levels in half for armorsmith
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = BaseLevel / 2
+WHERE SkillID = 13
+	AND ItemName NOT LIKE '%repair kit%'
+	AND CraftCategoryID NOT IN (13)
+	AND ItemName NOT LIKE '%basic%'

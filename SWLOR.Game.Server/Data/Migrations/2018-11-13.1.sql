c42ca55955a60684491f11026566d5d1ca2aaf0b
zunath <coolty3001@yahoo.com>
2018-11-13 15:05:00 +0800

    Fix the device from weaponsmith to engineering for lightsaber and firearms

--- SWLOR.Game.Server/Data/Migrations/2018-11-13.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-13.1.sql
@@ -5,3 +5,10 @@ SET Storage = 40 WHERE ID = 153
 
 UPDATE dbo.BaseStructure
 SET Storage = 50 WHERE ID = 154
+
+
+-- Fix the device from weaponsmith to engineering for lightsaber and firearms
+UPDATE dbo.CraftBlueprint
+SET CraftDeviceID = 4
+WHERE SkillID = 22
+	AND CraftDeviceID = 2

d5bb36900a1a54b0f9b9d98157ad4eb864ea14f4
zunath <coolty3001@yahoo.com>
2018-11-16 13:11:58 +0800

    Reduce base levels on emitters, power clusters, saber hilts, and lightsabers.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-16.1.sql
@@ -0,0 +1,21 @@
+﻿
+-- Emitter: Drop to level 1
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 1
+WHERE ID = 611
+
+-- Power clusters: Drop to level 1
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 1
+WHERE ID IN (
+113,114,115,116,222)
+
+-- Saber hilt: Drop to level 1
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 1
+WHERE ID = 221
+
+-- Lightsabers: Drop to level 2
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 2
+WHERE ID IN( 211, 613, 622, 632)

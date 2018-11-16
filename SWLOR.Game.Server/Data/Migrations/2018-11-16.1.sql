638fc2e5438a976894868af788ba1e63f0898dce
zunath <coolty3001@yahoo.com>
2018-11-16 14:29:32 +0800

    Move ranged weapon core, pistol barrel, and rifle barrel to engineering device menu

--- SWLOR.Game.Server/Data/Migrations/2018-11-16.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-16.1.sql
@@ -19,3 +19,12 @@ WHERE ID = 221
 UPDATE dbo.CraftBlueprint
 SET BaseLevel = 2
 WHERE ID IN( 211, 613, 622, 632)
+
+
+-- Move ranged weapon core, pistol barrel, and rifle barrel to engineering device menu
+UPDATE dbo.CraftBlueprint
+SET CraftDeviceID = 4
+WHERE ID IN (
+100, 101, 102
+
+)

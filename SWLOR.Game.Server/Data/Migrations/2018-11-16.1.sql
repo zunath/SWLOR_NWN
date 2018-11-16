c907717809fdc546bef61aca6bd681430d170825
zunath <coolty3001@yahoo.com>
2018-11-16 18:40:13 +0800

    Add the Electronic Repair perk. Move repair kits for blasters and lightsabers over to use it.

--- SWLOR.Game.Server/Data/Migrations/2018-11-16.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-16.1.sql
@@ -33,3 +33,129 @@ WHERE ID IN (
 UPDATE dbo.Perk
 SET PerkCategoryID = 33
 WHERE ID = 151
+
+
+
+-- Add the electronics repair perk
+INSERT INTO dbo.Perk ( ID ,
+                       Name ,
+                       FeatID ,
+                       IsActive ,
+                       ScriptName ,
+                       BaseFPCost ,
+                       BaseCastingTime ,
+                       Description ,
+                       PerkCategoryID ,
+                       CooldownCategoryID ,
+                       ExecutionTypeID ,
+                       ItemResref ,
+                       IsTargetSelfOnly ,
+                       Enmity ,
+                       EnmityAdjustmentRuleID ,
+                       CastAnimationID )
+VALUES ( 152 ,    -- ID - int
+         'Electronic Repair' ,   -- Name - varchar(64)
+         NULL ,    -- FeatID - int
+         1 , -- IsActive - bit
+         'Engineering.ElectronicRepair' ,   -- ScriptName - varchar(64)
+         0 ,    -- BaseFPCost - int
+         0.0 ,  -- BaseCastingTime - float
+         N'Enables the use of electronic repair kits used for firarms, lightsabers, and other mechanically-based items.' ,  -- Description - nvarchar(256)
+         33 ,    -- PerkCategoryID - int
+         NULL ,    -- CooldownCategoryID - int
+         0 ,    -- ExecutionTypeID - int
+         NULL ,  -- ItemResref - nvarchar(16)
+         0 , -- IsTargetSelfOnly - bit
+         0 ,    -- Enmity - int
+         0 ,    -- EnmityAdjustmentRuleID - int
+         NULL      -- CastAnimationID - int
+    )
+
+
+DECLARE @ID INT 
+
+INSERT INTO dbo.PerkLevel ( PerkID ,
+                            Level ,
+                            Price ,
+                            Description )
+VALUES ( 152 , -- PerkID - int
+         1 , -- Level - int
+         3 , -- Price - int
+         N'Can use tech 1 electronics repair kits.' -- Description - nvarchar(512)
+    )
+
+SET @ID = SCOPE_IDENTITY()
+
+
+INSERT INTO dbo.PerkLevelSkillRequirement ( PerkLevelID ,
+                                            SkillID ,
+                                            RequiredRank )
+VALUES ( @ID , -- PerkLevelID - int
+         22 , -- SkillID - int
+         10   -- RequiredRank - int
+    )
+	
+INSERT INTO dbo.PerkLevel ( PerkID ,
+                            Level ,
+                            Price ,
+                            Description )
+VALUES ( 152 , -- PerkID - int
+         2 , -- Level - int
+         3 , -- Price - int
+         N'Can use tech 2 electronics repair kits.' -- Description - nvarchar(512)
+    )
+
+SET @ID = SCOPE_IDENTITY()
+
+
+INSERT INTO dbo.PerkLevelSkillRequirement ( PerkLevelID ,
+                                            SkillID ,
+                                            RequiredRank )
+VALUES ( @ID , -- PerkLevelID - int
+         22 , -- SkillID - int
+         20   -- RequiredRank - int
+    )
+
+	
+INSERT INTO dbo.PerkLevel ( PerkID ,
+                            Level ,
+                            Price ,
+                            Description )
+VALUES ( 152 , -- PerkID - int
+         3 , -- Level - int
+         3 , -- Price - int
+         N'Can use tech 3 electronics repair kits.' -- Description - nvarchar(512)
+    )
+
+SET @ID = SCOPE_IDENTITY()
+
+
+INSERT INTO dbo.PerkLevelSkillRequirement ( PerkLevelID ,
+                                            SkillID ,
+                                            RequiredRank )
+VALUES ( @ID , -- PerkLevelID - int
+         22 , -- SkillID - int
+         30   -- RequiredRank - int
+    )
+
+	
+INSERT INTO dbo.PerkLevel ( PerkID ,
+                            Level ,
+                            Price ,
+                            Description )
+VALUES ( 152 , -- PerkID - int
+         4 , -- Level - int
+         3 , -- Price - int
+         N'Can use tech 4 electronics repair kits.' -- Description - nvarchar(512)
+    )
+
+SET @ID = SCOPE_IDENTITY()
+
+
+INSERT INTO dbo.PerkLevelSkillRequirement ( PerkLevelID ,
+                                            SkillID ,
+                                            RequiredRank )
+VALUES ( @ID , -- PerkLevelID - int
+         22 , -- SkillID - int
+         40   -- RequiredRank - int
+    )
---
+++ SWLOR.Game.Server/Perk/Engineering/ElectronicRepair.cs
@@ -0,0 +1,61 @@
+﻿using SWLOR.Game.Server.GameObject;
+
+namespace SWLOR.Game.Server.Perk.Engineering
+{
+    public class ElectronicRepair: IPerk
+    {
+        public bool CanCastSpell(NWPlayer oPC, NWObject oTarget)
+        {
+            return false;
+        }
+
+        public string CannotCastSpellMessage(NWPlayer oPC, NWObject oTarget)
+        {
+            return null;
+        }
+
+        public int FPCost(NWPlayer oPC, int baseFPCost)
+        {
+            return 0;
+        }
+
+        public float CastingTime(NWPlayer oPC, float baseCastingTime)
+        {
+            return 0;
+        }
+
+        public float CooldownTime(NWPlayer oPC, float baseCooldownTime)
+        {
+            return 0;
+        }
+
+        public void OnImpact(NWPlayer player, NWObject target, int perkLevel)
+        {
+        }
+
+        public void OnPurchased(NWPlayer oPC, int newLevel)
+        {
+        }
+
+        public void OnRemoved(NWPlayer oPC)
+        {
+        }
+
+        public void OnItemEquipped(NWPlayer oPC, NWItem oItem)
+        {
+        }
+
+        public void OnItemUnequipped(NWPlayer oPC, NWItem oItem)
+        {
+        }
+
+        public void OnCustomEnmityRule(NWPlayer oPC, int amount)
+        {
+        }
+
+        public bool IsHostile()
+        {
+            return false;
+        }
+    }
+}

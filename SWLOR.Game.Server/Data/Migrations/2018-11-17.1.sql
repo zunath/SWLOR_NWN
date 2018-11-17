a2f6a62c87fe9c5d0bad7c5ef2a7e874ea92085c
zunath <coolty3001@yahoo.com>
2018-11-17 13:11:08 +0800

    Add the Wookiee rug placeable to fabrication crafting.

--- SWLOR.Game.Server/Data/Migrations/2018-11-17.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-17.1.sql
@@ -1,4 +1,4 @@
-﻿
+﻿-- Crystal quest
 INSERT INTO dbo.Quest ( ID ,
                         Name ,
                         JournalTag ,
@@ -84,4 +84,88 @@ INSERT INTO dbo.QuestRewardItem ( QuestID ,
 VALUES ( 9 ,   -- QuestID - int
          N'p_crystal_red_qs' , -- Resref - nvarchar(16)
          1     -- Quantity - int
+    )
+
+
+-- Wookiee Rug fabrication structure.
+
+
+INSERT INTO dbo.BaseStructure ( ID ,
+                                BaseStructureTypeID ,
+                                Name ,
+                                PlaceableResref ,
+                                ItemResref ,
+                                IsActive ,
+                                Power ,
+                                CPU ,
+                                Durability ,
+                                Storage ,
+                                HasAtmosphere ,
+                                ReinforcedStorage ,
+                                RequiresBasePower ,
+                                ResourceStorage ,
+                                RetrievalRating ,
+                                FuelRating )
+VALUES ( 178 ,    -- ID - int
+         8 ,    -- BaseStructureTypeID - int
+         N'Wookiee Rug' ,  -- Name - nvarchar(64)
+         N'wookiee_rug' ,  -- PlaceableResref - nvarchar(16)
+         N'furniture' ,  -- ItemResref - nvarchar(16)
+         1 , -- IsActive - bit
+         0.0 ,  -- Power - float
+         0.0 ,  -- CPU - float
+         0.0 ,  -- Durability - float
+         0 ,    -- Storage - int
+         1 , -- HasAtmosphere - bit
+         0 ,    -- ReinforcedStorage - int
+         0 , -- RequiresBasePower - bit
+         0 ,    -- ResourceStorage - int
+         0 ,    -- RetrievalRating - int
+         0      -- FuelRating - int
+    )
+
+INSERT INTO dbo.CraftBlueprint ( ID ,
+                                 CraftCategoryID ,
+                                 BaseLevel ,
+                                 ItemName ,
+                                 ItemResref ,
+                                 Quantity ,
+                                 SkillID ,
+                                 CraftDeviceID ,
+                                 PerkID ,
+                                 RequiredPerkLevel ,
+                                 IsActive ,
+                                 MainComponentTypeID ,
+                                 MainMinimum ,
+                                 SecondaryComponentTypeID ,
+                                 SecondaryMinimum ,
+                                 TertiaryComponentTypeID ,
+                                 TertiaryMinimum ,
+                                 EnhancementSlots ,
+                                 MainMaximum ,
+                                 SecondaryMaximum ,
+                                 TertiaryMaximum ,
+                                 BaseStructureID )
+VALUES ( 642 ,    -- ID - int
+         30 ,    -- CraftCategoryID - int
+         10 ,    -- BaseLevel - int
+         N'Wookiee Rug' ,  -- ItemName - nvarchar(64)
+         N'furniture' ,  -- ItemResref - nvarchar(16)
+         1 ,    -- Quantity - int
+         15 ,    -- SkillID - int
+         5 ,    -- CraftDeviceID - int
+         2 ,    -- PerkID - int
+         3 ,    -- RequiredPerkLevel - int
+         1 , -- IsActive - bit
+         13 ,    -- MainComponentTypeID - int
+         1 ,    -- MainMinimum - int
+         12 ,    -- SecondaryComponentTypeID - int
+         2 ,    -- SecondaryMinimum - int
+         0 ,    -- TertiaryComponentTypeID - int
+         0 ,    -- TertiaryMinimum - int
+         0 ,    -- EnhancementSlots - int
+         1 ,    -- MainMaximum - int
+         2 ,    -- SecondaryMaximum - int
+         0 ,    -- TertiaryMaximum - int
+         178      -- BaseStructureID - int
     )

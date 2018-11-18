ea3551efcd7a402237a768a56c5149c891de1cba
zunath <coolty3001@yahoo.com>
2018-11-18 18:22:58 +0800

    Add spawn to mountain valley

--- SWLOR.Game.Server/Data/Migrations/2018-11-18.3.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-18.3.sql
@@ -135,3 +135,76 @@ VALUES ( 3 ,   -- ID - int
          N'SightAggroBehaviour' , -- BehaviourScript - nvarchar(64)
          0     -- DeathVFXID - int
     )
+
+
+INSERT INTO dbo.NPCGroup ( ID ,
+                           Name )
+VALUES ( 15 , -- ID - int
+         N'Valley Nashtah' -- Name - nvarchar(32)
+    )
+
+INSERT INTO dbo.SpawnObject ( ID ,
+                              SpawnID ,
+                              Resref ,
+                              Weight ,
+                              SpawnRule ,
+                              NPCGroupID ,
+                              BehaviourScript ,
+                              DeathVFXID )
+VALUES ( 4 ,   -- ID - int
+         25 ,   -- SpawnID - int
+         N'vall_nashtah' , -- Resref - nvarchar(16)
+         20 ,   -- Weight - int
+         N'' , -- SpawnRule - nvarchar(32)
+         15 ,   -- NPCGroupID - int
+         N'SoundAggroRandomWalkBehaviour' , -- BehaviourScript - nvarchar(64)
+         0     -- DeathVFXID - int
+    )
+
+INSERT INTO dbo.LootTable ( ID ,
+                            Name )
+VALUES ( 50 , -- ID - int
+         N'Viscara - Valley Nashtah' -- Name - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItem ( LootTableID ,
+                                Resref ,
+                                MaxQuantity ,
+                                Weight ,
+                                IsActive ,
+                                SpawnRule )
+VALUES ( 50 ,    -- LootTableID - int
+         'nashtah_meat' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         30 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItem ( LootTableID ,
+                                Resref ,
+                                MaxQuantity ,
+                                Weight ,
+                                IsActive ,
+                                SpawnRule )
+VALUES ( 50 ,    -- LootTableID - int
+         'nash_scale' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         10 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItem ( LootTableID ,
+                                Resref ,
+                                MaxQuantity ,
+                                Weight ,
+                                IsActive ,
+                                SpawnRule )
+VALUES ( 50 ,    -- LootTableID - int
+         'nash_tail' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         2 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )

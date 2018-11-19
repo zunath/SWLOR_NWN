41feb19b474cef5c94318405ebf657ea84243407
zunath <coolty3001@yahoo.com>
2018-11-19 15:15:34 +0800

    Reduce Speedy First Aid perk skill requirements in half. Fix description on level 6 of Speedy First Aid perk.

--- SWLOR.Game.Server/Data/Migrations/2018-11-19.3.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-19.3.sql
@@ -1,4 +1,21 @@
 ﻿
+-- Fix Dash perk description
 UPDATE dbo.Perk
 SET Description = 'Increases your movement speed for a short period of time.'
 WHERE ID = 89
+
+
+-- Fix required rank on Speedy First Aid perk.
+UPDATE dbo.PerkLevelSkillRequirement
+SET RequiredRank = RequiredRank / 2
+WHERE PerkLevelID IN (
+	SELECT ID 
+	FROM dbo.PerkLevel
+	WHERE PerkID = 20
+)
+
+-- Fix description on rank 6 of Speedy First Aid perk.
+UPDATE dbo.PerkLevel
+SET Description = '+60% Chance'
+WHERE PerkID = 20
+	AND Level = 6

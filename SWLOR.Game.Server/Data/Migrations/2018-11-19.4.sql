2c933e06468d749037dd26001c66b42d38f28571
zunath <coolty3001@yahoo.com>
2018-11-19 19:38:36 +0800

    Fix the description on Mass Tranquilizer perk.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-19.4.sql
@@ -0,0 +1,5 @@
+﻿
+UPDATE dbo.Perk
+SET Description = 'Your next attack will tranquilize all creatures within range of your target, putting them to sleep for a short time. Damage will break the effect prematurely. Must be equipped with a Blaster Rifle to use. Length of effect depends on Tranquilizer perk.',
+	ScriptName = 'Blaster.MassTranquilizer'
+WHERE ID = 75

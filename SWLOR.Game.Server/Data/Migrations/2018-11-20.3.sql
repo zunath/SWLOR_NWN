3f24ae545cf4ac53a1ef4954da600f508622b3dc
zunath <coolty3001@yahoo.com>
2018-11-20 10:20:46 +0800

    Fix Twin Blades description

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-20.3.sql
@@ -0,0 +1,4 @@
+﻿-- Fix Twin Blades description
+UPDATE dbo.Skill
+SET Description = 'Ability to use double edged weapons like twin vibroblades in combat. Higher skill levels unlock more powerful twin blade weapons.'
+WHERE ID = 3

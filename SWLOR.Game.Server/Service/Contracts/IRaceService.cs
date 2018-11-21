d03c8cc36bac35fcbce3c11d927a542c52729eeb
zunath <coolty3001@yahoo.com>
2018-11-21 17:48:24 +0800

    Scale Wookiee size up.

--- SWLOR.Game.Server/Service/Contracts/IRaceService.cs
+++ SWLOR.Game.Server/Service/Contracts/IRaceService.cs
@@ -4,6 +4,7 @@ namespace SWLOR.Game.Server.Service.Contracts
 {
     public interface IRaceService
     {
+        void OnModuleEnter();
         void ApplyDefaultAppearance(NWPlayer player);
     }
 }

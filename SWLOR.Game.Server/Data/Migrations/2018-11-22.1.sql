174ab535e6e2dd4a01379e9265b97dcc1cf3787f
zunath <coolty3001@yahoo.com>
2018-11-22 14:29:41 +0800

    Fix crystal colors for lightsaber crafting.

--- SWLOR.Game.Server/Data/Migrations/2018-11-22.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-22.1.sql
@@ -12,3 +12,20 @@ WHERE PerkID = 133
 UPDATE dbo.Quest
 SET RewardGold = 0
 WHERE ID = 30
+
+
+
+-- Adjust component types for red lightsabers and saberstaffs
+UPDATE dbo.CraftBlueprint
+SET TertiaryComponentTypeID = 29
+WHERE ID IN (612,613,614,615,616,617,618,619,620,621)
+
+-- Adjust component types for green lightsabers and saberstaffs
+UPDATE dbo.CraftBlueprint
+SET TertiaryComponentTypeID	= 31
+WHERE ID IN (622,623,624,625,626, 627,628,629,630,631)
+
+-- Adjust component types for yellow lightsabers and saberstaffs
+UPDATE dbo.CraftBlueprint
+SET TertiaryComponentTypeID	= 31
+WHERE ID IN (632,633,634,635,636, 637,638,639,640,641)

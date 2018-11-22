d0d47b186e4df9691da3d6f45ba9b6139e5aa9dd
zunath <coolty3001@yahoo.com>
2018-11-22 10:00:24 +0800

    Fix text on Precision Targeting perk. Fix category for Plasma Cell. Remove gold reward for First Rites quest.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-22.1.sql
@@ -0,0 +1,14 @@
+﻿
+UPDATE dbo.Perk
+SET PerkCategoryID = 8
+WHERE ID = 94
+
+UPDATE dbo.PerkLevel
+SET Description = 'Grants the Precision Targeting perk.'
+WHERE PerkID = 133
+
+
+-- Remove gold reward for first rites quest.
+UPDATE dbo.Quest
+SET RewardGold = 0
+WHERE ID = 30

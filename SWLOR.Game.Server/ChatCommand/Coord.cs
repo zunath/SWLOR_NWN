bf7caf03833e38b7da9f6c60d94240dd885f3ac0
zunath <coolty3001@yahoo.com>
2018-11-25 16:40:34 +0800

    Display sector in the /coord command.

--- SWLOR.Game.Server/ChatCommand/Coord.cs
+++ SWLOR.Game.Server/ChatCommand/Coord.cs
@@ -2,12 +2,20 @@
 using SWLOR.Game.Server.ChatCommand.Contracts;
 using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.GameObject;
+using SWLOR.Game.Server.Service.Contracts;
 
 namespace SWLOR.Game.Server.ChatCommand
 {
     [CommandDetails("Displays your current coordinates in the area.", CommandPermissionType.Player | CommandPermissionType.DM)]
     public class Coord : IChatCommand
     {
+        private readonly IBaseService _base;
+
+        public Coord(IBaseService @base)
+        {
+            _base = @base;
+        }
+
         /// <summary>
         /// Returns the X and Y position, in tiles, of the user.
         /// </summary>
@@ -17,11 +25,16 @@ namespace SWLOR.Game.Server.ChatCommand
         /// <param name="args"></param>
         public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
         {
+            NWArea area = user.Area;
             Vector position = user.Position;
             int cellX = (int)(position.m_X / 10);
             int cellY = (int)(position.m_Y / 10);
+            string sector = "N/A"; 
 
-            user.SendMessage($"Current Area Coordinates: ({cellX}, {cellY})");
+            if(area.Width == 32 && area.Height == 32)
+                sector = _base.GetSectorOfLocation(user.Location);
+
+            user.SendMessage($"Current Area Coordinates: ({cellX}, {cellY}) [Sector: " + sector + "]");
         }
 
         public string ValidateArguments(NWPlayer user, params string[] args)

14f82f955b200dd37d356a72f2fe99d30269ce5c
zunath <coolty3001@yahoo.com>
2018-11-25 14:31:09 +0800

    Fix the component type for green lightsabers and saberstaffs.

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-25.2.sql
@@ -0,0 +1,5 @@
+﻿
+-- Adjust component types for green lightsabers and saberstaffs
+UPDATE dbo.CraftBlueprint
+SET TertiaryComponentTypeID	= 30
+WHERE ID IN (622,623,624,625,626, 627,628,629,630,631)

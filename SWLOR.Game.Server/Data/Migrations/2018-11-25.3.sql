1ad26db98dbea8b4b827679bf3797c24584f9567
zunath <coolty3001@yahoo.com>
2018-11-25 23:11:28 +0800

    Fix perk level requirements to craft repair kits for lightsabers and saberstaffs

---
+++ SWLOR.Game.Server/Data/Migrations/2018-11-25.3.sql
@@ -0,0 +1,27 @@
+﻿
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 1
+WHERE ID = 558
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 3
+WHERE ID = 559
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 5
+WHERE ID = 560
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 7
+WHERE ID = 561
+
+
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 1
+WHERE ID = 570
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 3
+WHERE ID = 571
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 5
+WHERE ID = 572
+UPDATE dbo.CraftBlueprint
+SET RequiredPerkLevel = 7
+WHERE ID = 573

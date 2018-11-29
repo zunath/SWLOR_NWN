56e7bfd2023f758e294321479a5f2c1b233636c5
zunath <coolty3001@yahoo.com>
2018-11-29 16:52:18 +0800

    Remove the background worker and use a raw thread instead. Fix an issue where the server would fail to shutdown cleanly sometimes due to a deadlock. Ensure all data gets committed to the DB before killing the DB thread.

--- SWLOR.Game.Server/Threading/BackgroundThreadManager.cs
+++ SWLOR.Game.Server/Threading/BackgroundThreadManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading;
 using SWLOR.Game.Server.Threading.Contracts;
 
 namespace SWLOR.Game.Server.Threading
@@ -7,40 +8,49 @@ namespace SWLOR.Game.Server.Threading
     public class BackgroundThreadManager: IBackgroundThreadManager
     {
         private readonly IDatabaseThread _dbThread;
-        private readonly BackgroundWorker _dbWorker;
+        private readonly Thread _dbWorker;
+        private volatile bool _isShuttingDown;
+        private volatile bool _threadHasShutDown;
 
         public BackgroundThreadManager(
             IDatabaseThread databaseThread)
         {
             _dbThread = databaseThread;
-
-            _dbWorker = new BackgroundWorker();
-            _dbWorker.DoWork += ProcessDatabaseThread;
-            _dbWorker.WorkerSupportsCancellation = true;
-
+            _dbWorker = new Thread(x => ProcessDatabaseThread());
+            _dbWorker.IsBackground = true;
+            _isShuttingDown = false;
+            
             AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
         }
         
-        private void ProcessDatabaseThread(object sender, DoWorkEventArgs e)
+        private void ProcessDatabaseThread()
         {
-            while(!e.Cancel)
+            Console.WriteLine("DB thread starting");
+            while (!_isShuttingDown)
             {
                 _dbThread.Run();
             }
 
-            Console.WriteLine("Database thread shut down successfully!");
+            _threadHasShutDown = true;
         }
 
         public void Start()
         {
             Console.WriteLine("Starting database thread...");
-            _dbWorker.RunWorkerAsync();
+            _dbWorker.Start();
         }
 
         private void CurrentDomainOnProcessExit(object sender, EventArgs e)
         {
-            Console.WriteLine("Shutting down database thread...");
-            _dbWorker.CancelAsync();
+            Console.WriteLine("Shutting down database thread. Please be patient while all data is committed to the database.");
+            _isShuttingDown = true;
+
+            while (!_threadHasShutDown)
+            {
+                // Spin until DB thread has shut down completely.
+            }
+
+            Console.WriteLine("Database thread shut down successfully!");
         }
     }
 

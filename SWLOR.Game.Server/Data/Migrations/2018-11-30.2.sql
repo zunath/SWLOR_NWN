4a5ebf6371ac81b2cda5a8b6da0683fc81ed2b02
zunath <coolty3001@yahoo.com>
2018-11-30 15:33:08 +0800

    Fix a bad migration script

--- SWLOR.Game.Server/Data/Migrations/2018-11-30.2.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-11-30.2.sql
@@ -3,7 +3,7 @@ UPDATE dbo.BaseStructure
 SET Storage = 55 WHERE ID = 153
 
 UPDATE dbo.BaseStructure
-SET Storage = 65 WHERE ID = 153
+SET Storage = 65 WHERE ID = 154
 
 UPDATE dbo.BaseStructure
-SET Storage = 75 WHERE ID = 153
+SET Storage = 75 WHERE ID = 155

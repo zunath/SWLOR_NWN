0fd35d3e9e1e6a54f99076d71e7074138e3e9de5
Couls <coul422@gmail.com>
2018-12-01 02:02:45 -0500

    Implement /FP command

--- SWLOR.Game.Server/ChatCommand/GetFP.cs
+++ SWLOR.Game.Server/ChatCommand/GetFP.cs
@@ -10,21 +10,32 @@ using SWLOR.Game.Server.Service.Contracts;
 
 namespace SWLOR.Game.Server.ChatCommand
 {
-    [CommandDetails("Gets the Current FP of player.",CommandPermissionType.Player)]
-    public class GetFP : IChatCommand
+    [CommandDetails("Gets the Current FP of player.", CommandPermissionType.Player)]
+    public class FP : IChatCommand
     {
-
-        public GetFP(
+        private readonly INWScript _;
+        private readonly IColorTokenService _color;
+        private readonly IDataService _data;
+        public FP(
             INWScript script,
+            IColorTokenService color,
             IDataService data)
         {
             _ = script;
+            _color = color;
             _data = data;
         }
         public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
         {
-                              if (!user.IsPlayer) return;
-                              Player entity = _data.Get<Player>(oPC.GlobalID);
-                             user.SendMessage(_color.Custom("FP: " + entity.CurrentFP + " / " + entity.MaxFP, 32, 223, 219)););
+            if (!user.IsPlayer) return;
+            Player entity = _data.Get<Player>(user.GlobalID);
+            user.SendMessage(_color.Custom("FP: " + entity.CurrentFP + " / " + entity.MaxFP, 32, 223, 219));
+        }
+        public string ValidateArguments(NWPlayer user, params string[] args)
+        {
+            return string.Empty;
+        }
+
+        public bool RequiresTarget => false;
     }
 }

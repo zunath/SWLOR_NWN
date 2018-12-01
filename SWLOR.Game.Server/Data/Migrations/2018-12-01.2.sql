89a4d88ef5000e9840812298b08fba5e141e5bda
zunath <coolty3001@yahoo.com>
2018-12-01 18:42:43 +0800

    Remove mysterious obelisk from the craft and structure menus.

--- SWLOR.Game.Server/Data/Migrations/2018-12-01.2.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-12-01.2.sql
@@ -1,4 +1,15 @@
-﻿--Remove casting time from Dash
+﻿--Reduce casting time for Dash
 UPDATE dbo.Perk
 SET BaseCastingTime = 0
 WHERE ID = 89
+
+
+-- Remove the broken Mysterious Obelisk placeable (no longer available)
+DELETE FROM dbo.PCBaseStructure
+WHERE BaseStructureID = 152
+
+DELETE FROM dbo.CraftBlueprint
+WHERE ID = 470
+
+DELETE FROM dbo.BaseStructure
+WHERE ID = 152

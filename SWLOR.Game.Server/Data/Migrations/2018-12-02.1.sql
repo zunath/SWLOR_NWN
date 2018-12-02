a9d0df879753318b588cf67f87f2984cf4ad1778
Couls <coul422@gmail.com>
2018-12-02 15:19:24 -0500

    Update 2018-12-02.1.sql

--- SWLOR.Game.Server/Data/Migrations/2018-12-02.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-12-02.1.sql
@@ -1,45 +1,45 @@
 ﻿UPDATE dbo.Perk
-SET Description = 'Provides a bonus to durability when using Electronic repair kits.'
+SET Description = 'Provides a bonus to repairs when using Electronic repair kits.'
 WHERE ID = 152
 UPDATE dbo.Perk
-SET Description = 'Provides a bonus to durability when using armor repair kits'
+SET Description = 'Provides a bonus to repairs when using armor repair kits'
 WHERE ID = 150
 UPDATE dbo.Perk
-SET Description = 'Provides a bonus to durability when using weapon repair kits'
+SET Description = 'Provides a bonus to repairs when using weapon repair kits'
 WHERE ID = 149
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +2 to the durability of armor when using an armor repair kit'
+SET Description = 'Gains +2 to the repair of armor when using an armor repair kit'
 WHERE ID = 2066
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +4 to the durability of armor when using an armor repair kit'
+SET Description = 'Gains +4 to the repair of armor when using an armor repair kit'
 WHERE ID = 2067
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +6 to the durability of armor when using an armor repair kit'
+SET Description = 'Gains +6 to the repair of armor when using an armor repair kit'
 WHERE ID = 2068
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +8 to the durability of armor when using an armor repair kit'
+SET Description = 'Gains +8 to the repair of armor when using an armor repair kit'
 WHERE ID = 2069
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +2 to the durability of armor when using an weapon repair kit'
+SET Description = 'Gains +2 to the repair of weapon when using an weapon repair kit'
 WHERE ID = 2062
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +4 to the durability of armor when using an weapon repair kit'
+SET Description = 'Gains +4 to the repair of weapon when using an weapon repair kit'
 WHERE ID = 2063
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +6 to the durability of armor when using an weapon repair kit'
+SET Description = 'Gains +6 to the repair of weapon when using an weapon repair kit'
 WHERE ID = 2064
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +8 to the durability of armor when using an weapon repair kit'
+SET Description = 'Gains +8 to the repair of weapon when using an weapon repair kit'
 WHERE ID = 2065
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +2 to the durability of armor when using an electronic repair kit'
+SET Description = 'Gains +2 to the repair of item when using an electronic repair kit'
 WHERE ID = 1957
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +4 to the durability of armor when using an electronic repair kit'
+SET Description = 'Gains +4 to the repair of item when using an electronic repair kit'
 WHERE ID = 1958
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +6 to the durability of armor when using an electronic repair kit'
+SET Description = 'Gains +6 to the repair of item when using an electronic repair kit'
 WHERE ID = 1959
 UPDATE dbo.PerkLevel
-SET Description = 'Gains +8 to the durability of armor when using an electronic repair kit'
+SET Description = 'Gains +8 to the repair of item when using an electronic repair kit'
 WHERE ID = 1960

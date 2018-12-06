3f750aa84cfff62b6cca6ac76aceb01d3e0a4438
zunath <coolty3001@yahoo.com>
2018-12-06 18:02:49 +0800

    Add /name command for DM use.

---
+++ SWLOR.Game.Server/ChatCommand/Name.cs
@@ -0,0 +1,48 @@
+﻿using NWN;
+using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN.NWScript;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Changes the name of a target.", CommandPermissionType.DM)]
+    public class Name : IChatCommand
+    {
+        private readonly INWScript _;
+
+        public Name(INWScript script)
+        {
+            _ = script;
+        }
+
+        public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
+        {
+            if (target.IsPlayer || target.IsDM)
+            {
+                user.SendMessage("PCs cannot be targeted with this command.");
+                return;
+            }
+
+            string name = string.Empty;
+            foreach (var arg in args)
+            {
+                name += " " + arg;
+            }
+
+            target.Name = name;
+        }
+
+        public string ValidateArguments(NWPlayer user, params string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                return "Please enter a name. Example: /name My Creature";
+            }
+            
+            return string.Empty;
+        }
+
+        public bool RequiresTarget => true;
+    }
+}

64584d20a37594ea8548017e62a891796d99b793
Couls <coul422@gmail.com>
2018-12-10 02:38:14 -0500

    Update 2018-12-10.2.sql

--- SWLOR.Game.Server/Data/Migrations/2018-12-10.2.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-12-10.2.sql
@@ -1,6 +1,3 @@
 ﻿  UPDATE dbo.SpawnObject
   SET BehaviourScript = 'PackAggroBehaviour'
-  WHERE NPCGroupID IS NOT NULL
-  UPDATE dbo.SpawnObject
-  SET BehaviourScript = 'SightAggroBehaviour'
-  WHERE Resref = 'mynock' OR Resref = 'malsecdroid' OR Resref = 'malspiderdroid' OR Resref = 'colicoidexp'
+  WHERE BehaviourScript =  'SightAggroBehaviour'

7cceb1b748df13abc59e8fb21bfe0529c455f0ff
zunath <coolty3001@yahoo.com>
2018-12-10 10:22:58 +0800

    Fix resref on craft blueprint for FP Regen mods.

--- SWLOR.Game.Server/Data/Migrations/2018-12-10.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-12-10.1.sql
@@ -13,3 +13,17 @@ WHERE ID IN (
 UPDATE dbo.CraftBlueprint
 SET SkillID = 14
 WHERE PerkID = 151
+
+
+
+UPDATE dbo.CraftBlueprint
+SET ItemResref = 'rune_manareg1'
+WHERE ID = 143
+
+UPDATE dbo.CraftBlueprint
+SET ItemResref = 'rune_manareg2'
+WHERE ID = 176
+
+UPDATE dbo.CraftBlueprint
+SET ItemResref = 'rune_manareg3'
+WHERE ID = 206

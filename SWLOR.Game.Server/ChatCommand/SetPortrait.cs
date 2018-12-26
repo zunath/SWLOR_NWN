a36bad237014a8fb506891e3e85d47332c39e19f
zunath <coolty3001@yahoo.com>
2018-12-26 10:34:24 +0800

    Check for missing argument in the /SetPortrait command.

--- SWLOR.Game.Server/ChatCommand/SetPortrait.cs
+++ SWLOR.Game.Server/ChatCommand/SetPortrait.cs
@@ -43,7 +43,12 @@ namespace SWLOR.Game.Server.ChatCommand
 
         public string ValidateArguments(NWPlayer user, params string[] args)
         {
-            if (args.Length > 0 && args[0].Length > 15)
+            if (args.Length <= 0)
+            {
+                return "Please enter the name of the portrait and try again. Example: /SetPortrait po_myportrait";
+            }
+
+            if (args[0].Length > 16)
             {
                 return "The portrait you entered is too long. Portrait names should be between 1 and 16 characters.";
             }

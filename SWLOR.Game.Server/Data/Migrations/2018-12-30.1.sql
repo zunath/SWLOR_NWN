2f75e66b7d9e623eea365b5229ab5ac61cb15ad7
Mithreas <jdt@metaswitch.com>
2019-01-01 18:34:06 +1000

    Don't include the database name in migration scripts, because on test servers it fails :)

--- SWLOR.Game.Server/Data/Migrations/2018-12-30.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2018-12-30.1.sql
@@ -1,10 +1,10 @@
 ﻿
 -- Fix the categories for prismatic item blueprints.
-update SWLOR.dbo.CraftBlueprint set CraftCategoryID = 19 where ID=275;
-update SWLOR.dbo.CraftBlueprint set CraftCategoryID = 18 where ID=276;
+update dbo.CraftBlueprint set CraftCategoryID = 19 where ID=275;
+update dbo.CraftBlueprint set CraftCategoryID = 18 where ID=276;
 
 -- Fix the required components for prismatic item blueprints.
-update SWLOR.dbo.CraftBlueprint set SecondaryComponentTypeID=33,TertiaryComponentTypeID=36 where ID=307;
-update SWLOR.dbo.CraftBlueprint set SecondaryComponentTypeID=35,TertiaryComponentTypeID=38 where ID=275;
-update SWLOR.dbo.CraftBlueprint set SecondaryComponentTypeID=34,TertiaryComponentTypeID=37 where ID=276;
-update SWLOR.dbo.CraftBlueprint set SecondaryComponentTypeID=34,TertiaryComponentTypeID=37 where ID=306;
+update dbo.CraftBlueprint set SecondaryComponentTypeID=33,TertiaryComponentTypeID=36 where ID=307;
+update dbo.CraftBlueprint set SecondaryComponentTypeID=35,TertiaryComponentTypeID=38 where ID=275;
+update dbo.CraftBlueprint set SecondaryComponentTypeID=34,TertiaryComponentTypeID=37 where ID=276;
+update dbo.CraftBlueprint set SecondaryComponentTypeID=34,TertiaryComponentTypeID=37 where ID=306;
--- SWLOR.Game.Server/Data/Migrations/2019-01-01.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-01-01.1.sql
@@ -1,3 +1,3 @@
-update swlor.dbo.LootTableItem set Resref='nw_it_gold001' where Resref='nw_it_ld001';
+update dbo.LootTableItem set Resref='nw_it_gold001' where Resref='nw_it_ld001';
 
-update swlor.dbo.CraftBlueprint set EnhancementSlots=1 where CraftCategoryID in (30,31,32,33,34,35,36,37,38);
+update dbo.CraftBlueprint set EnhancementSlots=1 where CraftCategoryID in (30,31,32,33,34,35,36,37,38);

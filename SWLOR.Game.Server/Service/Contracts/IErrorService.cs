d51050ecd2008561e9db187b0aec53025ecb5a44
Mithreas <38130103+Mithreas@users.noreply.github.com>
2019-01-08 23:27:42 +1000

    Implement weather system
    * See https://github.com/zunath/SWLOR_NWN/issues/736 for patch notes.
    
    * Applied requested changes.  Making the property work required a few more
    changes (e.g. to NWItem/CustomItemPropertyType) which I've applied as
    well.
    
    Also changed the quality to be randomised (strongly weighted towards Low
    & Normal quality) and made the Luck perk & stat give a % increase to the
    quality chance.
    
    * Stashing changes - currently still crashes the server.
    
    * Tested and working.  Note area configuration changes needed before this
    goes live: see #645 for details.

--- SWLOR.Game.Server/Service/Contracts/IErrorService.cs
+++ SWLOR.Game.Server/Service/Contracts/IErrorService.cs
@@ -5,5 +5,6 @@ namespace SWLOR.Game.Server.Service.Contracts
     public interface IErrorService
     {
         void LogError(Exception ex, string @event = "");
+        void Trace(string component, string log);
     }
 }

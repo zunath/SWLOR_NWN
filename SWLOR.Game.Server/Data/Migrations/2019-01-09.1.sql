b1fe031b7b0c4a6ec409912c2940d82fcae8304c
Mithreas <38130103+Mithreas@users.noreply.github.com>
2019-01-09 22:12:19 +1000

    Fix bad resrefs on carpet fixtures. Fixes #765
    * Migration script to fix up three carpet fixtures.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-01-09.1.sql
@@ -0,0 +1,4 @@
+﻿-- Correct the resrefs of three carpets (and clarify the names of two of them).  
+update dbo.BaseStructure set PlaceableResref='x0_roundrugorien' where ID=54;
+update dbo.BaseStructure set Name='Carpet, Fancy', PlaceableResref='x0_rugoriental' where ID=93;
+update dbo.BaseStructure set Name='Carpet, Fancy, Smaller', PlaceableResref='x0_rugoriental2' where ID=96;

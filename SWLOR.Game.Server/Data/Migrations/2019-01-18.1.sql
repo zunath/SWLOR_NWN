5822425a465c8782b84a23d90526a6ebb8c15d8e
zunath <coolty3001@yahoo.com>
2019-01-18 18:34:53 -0500

    Fix naming issues on helmets.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-01-18.1.sql
@@ -0,0 +1,35 @@
+﻿
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Basic Force Helmet' WHERE ID = 643
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Force Helmet I' WHERE ID = 644
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Force Helmet II' WHERE ID = 645
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Force Helmet III' WHERE ID = 646
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Force Helmet IV' WHERE ID = 647
+
+
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Basic Heavy Helmet' WHERE ID = 649
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Heavy Helmet I' WHERE ID = 650
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Heavy Helmet II' WHERE ID = 651
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Heavy Helmet III' WHERE ID = 652
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Heavy Helmet IV' WHERE ID = 653
+
+
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Basic Light Helmet' WHERE ID = 654
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Light Helmet I' WHERE ID = 655
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Light Helmet II' WHERE ID = 656
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Light Helmet III' WHERE ID = 657
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Light Helmet IV' WHERE ID = 658

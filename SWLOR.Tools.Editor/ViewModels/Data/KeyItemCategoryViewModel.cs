3210312c11e82ce55f41925cfa8d270d9a966811
zunath <coolty3001@yahoo.com>
2019-01-29 19:50:53 -0500

    Start work on key item editor

--- SWLOR.Tools.Editor/ViewModels/Data/KeyItemCategoryViewModel.cs
+++ SWLOR.Tools.Editor/ViewModels/Data/KeyItemCategoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 using SWLOR.Game.Server.Data;
 using SWLOR.Game.Server.Data.Entity;
 using SWLOR.Tools.Editor.Attributes;
@@ -8,8 +9,16 @@ namespace SWLOR.Tools.Editor.ViewModels.Data
     [Folder(nameof(KeyItemCategory))]
     public class KeyItemCategoryViewModel : DBObjectViewModelBase
     {
-        private string _displayName;
+        public KeyItemCategoryViewModel()
+        {
+            KeyItems = new ObservableCollection<KeyItemViewModel>();
+            Name = "New Key Item Category";
 
+            TrackProperty(this, x => x.Name);
+            TrackProperty(this, x => x.IsActive);
+        }
+
+        private string _displayName;
         public override string DisplayName
         {
             get => _displayName;
@@ -33,6 +42,30 @@ namespace SWLOR.Tools.Editor.ViewModels.Data
                 _displayName = value;
                 NotifyOfPropertyChange(() => Name);
                 NotifyOfPropertyChange(() => DisplayName);
+            }
+        }
+
+        private bool _isActive;
+        [JsonProperty(nameof(IsActive))]
+        public bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                _isActive = value;
+                NotifyOfPropertyChange(() => IsActive);
+            }
+        }
+
+        private ObservableCollection<KeyItemViewModel> _keyItems;
+        [JsonProperty(nameof(KeyItems))]
+        public ObservableCollection<KeyItemViewModel> KeyItems
+        {
+            get => _keyItems;
+            set
+            {
+                _keyItems = value;
+                NotifyOfPropertyChange(() => KeyItems);
             }
         }
     }

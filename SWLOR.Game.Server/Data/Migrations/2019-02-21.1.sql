3623954a05ff4de73458f1e075330f214d5b3e8d
zunath <coolty3001@yahoo.com>
2019-02-21 15:40:39 -0500

    Change reinforced mode change from 1 unit per 30 minutes to 1 unit per 6 seconds. Bump control tower storage from 24 units of Stronidium to 7200. Convert existing units inside towers to the new rate.
    Increase the range you can access a control tower by 50%. (10.0 meters -> 15.0 meters)
    
    Fix a display issue where the tower shields would not show the proper 25%.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-02-21.1.sql
@@ -0,0 +1,10 @@
+﻿
+-- Increase reinforced storage to 7200 units of Stronidium since they burn at 1 unit per 6 seconds now.
+UPDATE dbo.BaseStructure
+SET ReinforcedStorage = 7200
+WHERE ID IN (1, 2, 3)
+
+-- Convert existing Stronidium storage to the new rate. I.E: 24 units -> 7200 units after the change.
+UPDATE dbo.PCBase
+SET ReinforcedFuel = ReinforcedFuel * 300
+WHERE Sector IN ('SE', 'SW', 'NE', 'NW')

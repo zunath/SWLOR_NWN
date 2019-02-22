8481bef23b4863e27ba6c491953f6ef6781f885a
zunath <coolty3001@yahoo.com>
2019-02-22 17:59:08 -0500

    Refund perk purchases for Health and FP. Change the price of Health and FP perks to a static 3 SP across all perk levels.

--- SWLOR.Game.Server/Data/Migrations/2019-02-22.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-02-22.1.sql
@@ -2,3 +2,16 @@
 UPDATE dbo.CraftBlueprint
 SET IsActive = 0
 WHERE ID IN (175, 205)
+
+
+
+-- Refund Health perk for all players.
+EXEC dbo.ADM_RefundPlayerPerk @PerkID = 16 -- int
+
+-- Refund FP perk for all players.
+EXEC dbo.ADM_RefundPlayerPerk @PerkID = 17 -- int
+
+-- Change price of Health and FP perks to 3 SP
+UPDATE dbo.PerkLevel
+SET Price = 3
+WHERE ID IN (16, 17)

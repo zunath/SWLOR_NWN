8a00c5843ffd7832d09da9b708b74f1d53bbf039
zunath <coolty3001@yahoo.com>
2019-02-22 12:56:17 -0500

    Put a limit of 20 items being sold on the market per character.

--- SWLOR.Game.Server/Service/Contracts/IMarketService.cs
+++ SWLOR.Game.Server/Service/Contracts/IMarketService.cs
@@ -6,6 +6,7 @@ namespace SWLOR.Game.Server.Service.Contracts
 {
     public interface IMarketService
     {
+        int NumberOfItemsAllowedToBeSoldAtATime { get; }
         PCMarketData GetPlayerMarketData(NWPlayer player);
         void ClearPlayerMarketData(NWPlayer player);
         int GetMarketRegionID(NWPlaceable terminal);

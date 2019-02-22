1e803b3f071eb4ddafc84ebc2f6fcd9133ee147c
Tyler <coolty3001@yahoo.com>
2019-02-22 12:27:32 -0500

    Reinforced mode changes (#817)
    * Change reinforced mode change from 1 unit per 30 minutes to 1 unit per 6 seconds. Bump control tower storage from 24 units of Stronidium to 7200. Convert existing units inside towers to the new rate.
    
    Increase the range you can access a control tower by 50%. (10.0 meters -> 15.0 meters)
    
    Fix a display issue where the tower shields would not show the proper 25%.
    
    * Add the Stronidium Refining perk.
    
    * Fix missing column

--- SWLOR.Game.Server/Placeable/ControlTower/OnDamaged.cs
+++ SWLOR.Game.Server/Placeable/ControlTower/OnDamaged.cs
@@ -58,27 +58,35 @@ namespace SWLOR.Game.Server.Placeable.ControlTower
                     player.SendMessage("Your base in " + attacker.Area.Name + " " + sector + " is under attack!");
                 }
             }
+
+            // Apply damage to the shields. Never fall below 0.
             pcBase.ShieldHP -= damage;
             if (pcBase.ShieldHP <= 0) pcBase.ShieldHP = 0;
+
+            // Calculate the amount of shield percentage remaining. If the tower is in reinforced mode, HP 
+            // will always be set back to 25% of shields.
             float hpPercentage = (float)pcBase.ShieldHP / (float)maxShieldHP * 100.0f;
             if (hpPercentage <= 25.0f && pcBase.ReinforcedFuel > 0)
             {
                 pcBase.IsInReinforcedMode = true;
                 pcBase.ShieldHP = (int)(maxShieldHP * 0.25f);
+                hpPercentage = (float) pcBase.ShieldHP / (float) maxShieldHP * 100.0f;
             }
 
+            // Notify the attacker.
             attacker.SendMessage("Tower Shields: " + hpPercentage.ToString("0.00") + "%");
-
             if (pcBase.IsInReinforcedMode)
             {
                 attacker.SendMessage("Control tower is in reinforced mode and cannot be damaged. Reinforced mode will be disabled when the tower runs out of fuel.");
             }
 
+            // HP is tracked in the database. Heal the placeable so it doesn't get destroyed.
             _.ApplyEffectToObject(DURATION_TYPE_INSTANT, _.EffectHeal(9999), tower.Object);
 
             var durability = _durability.GetDurability(weapon) - _random.RandomFloat(0.01f, 0.03f);
             _durability.SetDurability(weapon, durability);
 
+            // If the shields have fallen to zero, the tower will begin to take structure damage.
             if (pcBase.ShieldHP <= 0)
             {
                 pcBase.ShieldHP = 0;
@@ -87,6 +95,7 @@ namespace SWLOR.Game.Server.Placeable.ControlTower
                 if (structure.Durability < 0.0f) structure.Durability = 0.0f;
                 attacker.SendMessage("Structure Durability: " + structure.Durability.ToString("0.00"));
 
+                // If the structure durability drops to zero, destroy everything in the base.
                 if (structure.Durability <= 0.0f)
                 {
                     structure.Durability = 0.0f;

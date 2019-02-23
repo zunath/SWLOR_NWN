22052e46442eb9bbbeedae28400dc6b8956a1b2c
zunath <coolty3001@yahoo.com>
2019-02-22 23:49:59 -0500

    Add /customise command which maps to the existing /customize command.

---
+++ SWLOR.Game.Server/ChatCommand/Customise.cs
@@ -0,0 +1,11 @@
+﻿using SWLOR.Game.Server.Service.Contracts;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    public class Customise: Customize
+    {
+        public Customise(IDialogService dialog) : base(dialog)
+        {
+        }
+    }
+}

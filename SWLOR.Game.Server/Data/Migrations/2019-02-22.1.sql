85cb4f581936a2ab4d7743576a1f60371e03a963
zunath <coolty3001@yahoo.com>
2019-02-22 20:21:26 -0500

    Increase the HP and FP recovery amount for Meditate and Rest.

--- SWLOR.Game.Server/Data/Migrations/2019-02-22.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-02-22.1.sql
@@ -15,3 +15,50 @@ EXEC dbo.ADM_RefundPlayerPerk @PerkID = 17 -- int
 UPDATE dbo.PerkLevel
 SET Price = 3
 WHERE ID IN (16, 17)
+
+
+
+-- Update perk level descriptions to reflect changes to recovery amounts.
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 5 minutes'
+WHERE PerkID = 7 AND Level = 1
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 4 minutes, 30 seconds'
+WHERE PerkID = 7 AND Level = 2
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 7 AND Level = 3
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 7 AND Level = 4
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 3 minutes, 30 seconds'
+WHERE PerkID = 7 AND Level = 5
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 7 AND Level = 6
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 14 HP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 7 AND Level = 7
+
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 5 minutes'
+WHERE PerkID = 103 AND Level = 1
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 4 minutes, 30 seconds'
+WHERE PerkID = 103 AND Level = 2
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 103 AND Level = 3
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 103 AND Level = 4
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 3 minutes, 30 seconds'
+WHERE PerkID = 103 AND Level = 5
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 103 AND Level = 6
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 14 FP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 103 AND Level = 7

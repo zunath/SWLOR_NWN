acc1ed744d18ecf101392d5a5aeb2c25a74285ca
zunath <coolty3001@yahoo.com>
2019-02-23 22:02:32 -0500

    Add Starship Equipment category. Increase number items allowed to be sold at a time to 50. Treat belt, cloak, and boots as armor on the market.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-02-23.2.sql
@@ -0,0 +1,8 @@
+﻿
+INSERT INTO dbo.MarketCategory ( ID ,
+                                 Name ,
+                                 IsActive )
+VALUES ( 124 ,   -- ID - int
+         N'Starship Equipment' , -- Name - nvarchar(64)
+         1  -- IsActive - bit
+    )

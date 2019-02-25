69c8ee15034eb291b507324cf1ecfc32be0cc1dd
zunath <coolty3001@yahoo.com>
2019-02-25 18:43:05 -0500

    Fix the jukebox category.

--- SWLOR.Game.Server/Data/Migrations/2019-02-23.2.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-02-23.2.sql
@@ -6,3 +6,17 @@ VALUES ( 124 ,   -- ID - int
          N'Starship Equipment' , -- Name - nvarchar(64)
          1  -- IsActive - bit
     )
+
+
+	
+INSERT INTO dbo.CraftBlueprintCategory ( ID ,
+                                         Name ,
+                                         IsActive )
+VALUES ( 47 ,   -- ID - int
+         N'Special Furniture' , -- Name - nvarchar(32)
+         1  -- IsActive - bit
+    )
+
+UPDATE dbo.CraftBlueprint
+SET CraftCategoryID = 47
+WHERE ID = 682

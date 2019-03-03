714892d7fe93e6cac1ea476c9af0b909c646518a
zunath <coolty3001@yahoo.com>
2019-03-02 20:17:48 -0500

    Fix numbering on migration scripts.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-02-26.1.sql
@@ -0,0 +1,60 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		zunath
+-- Create date: 2019-02-26
+-- Description:	Returns player market listings 
+-- =============================================
+CREATE PROCEDURE ADM_GetMarketListings
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	SELECT pcml.ID AS ListingID,
+		 pcml.SellerPlayerID,
+		 p1.CharacterName AS SellerName,
+		 pcml.Note,
+		 pcml.Price,
+		 mr.Name AS RegionName,
+		 mc.Name AS MarketCategoryName,
+		 pcml.DatePosted,
+		 pcml.DateExpires,
+		 pcml.DateSold,
+		 pcml.DateRemoved,
+		 pcml.BuyerPlayerID,
+		 p2.CharacterName AS BuyerName,
+		 pcml.ItemID,
+		 pcml.ItemName,
+		 pcml.ItemStackSize,
+		 pcml.ItemTag,
+		 pcml.ItemResref,
+		 pcml.ItemObject,
+		 pcml.ItemRecommendedLevel
+	FROM dbo.PCMarketListing pcml
+	JOIN dbo.MarketRegion mr ON mr.ID = pcml.MarketRegionID
+	JOIN dbo.MarketCategory mc ON mc.ID = pcml.MarketCategoryID
+	JOIN dbo.Player p1 ON p1.ID = pcml.SellerPlayerID
+	LEFT JOIN dbo.Player p2 ON p2.ID = pcml.BuyerPlayerID
+	ORDER BY pcml.DatePosted
+
+END
+
+
+
+
--- SWLOR.Game.Server/Data/Migrations/2019-02-26.2.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-02-26.2.sql
@@ -1,60 +1,64 @@
-﻿-- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
--- =============================================
--- Author:		zunath
--- Create date: 2019-02-26
--- Description:	Returns player market listings 
--- =============================================
-CREATE PROCEDURE ADM_GetMarketListings
-
-AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-	
-	SELECT pcml.ID AS ListingID,
-		 pcml.SellerPlayerID,
-		 p1.CharacterName AS SellerName,
-		 pcml.Note,
-		 pcml.Price,
-		 mr.Name AS RegionName,
-		 mc.Name AS MarketCategoryName,
-		 pcml.DatePosted,
-		 pcml.DateExpires,
-		 pcml.DateSold,
-		 pcml.DateRemoved,
-		 pcml.BuyerPlayerID,
-		 p2.CharacterName AS BuyerName,
-		 pcml.ItemID,
-		 pcml.ItemName,
-		 pcml.ItemStackSize,
-		 pcml.ItemTag,
-		 pcml.ItemResref,
-		 pcml.ItemObject,
-		 pcml.ItemRecommendedLevel
-	FROM dbo.PCMarketListing pcml
-	JOIN dbo.MarketRegion mr ON mr.ID = pcml.MarketRegionID
-	JOIN dbo.MarketCategory mc ON mc.ID = pcml.MarketCategoryID
-	JOIN dbo.Player p1 ON p1.ID = pcml.SellerPlayerID
-	LEFT JOIN dbo.Player p2 ON p2.ID = pcml.BuyerPlayerID
-	ORDER BY pcml.DatePosted
-
-END
+﻿-- Disable the +2 and +3 BAB Mods
+UPDATE dbo.CraftBlueprint
+SET IsActive = 0
+WHERE ID IN (175, 205)
 
 
 
+-- Refund Health perk for all players.
+EXEC dbo.ADM_RefundPlayerPerk @PerkID = 16 -- int
 
+-- Refund FP perk for all players.
+EXEC dbo.ADM_RefundPlayerPerk @PerkID = 17 -- int
+
+-- Change price of Health and FP perks to 3 SP
+UPDATE dbo.PerkLevel
+SET Price = 3
+WHERE ID IN (16, 17)
+
+
+
+-- Update perk level descriptions to reflect changes to recovery amounts.
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 5 minutes'
+WHERE PerkID = 7 AND Level = 1
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 4 minutes, 30 seconds'
+WHERE PerkID = 7 AND Level = 2
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 HP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 7 AND Level = 3
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 7 AND Level = 4
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 3 minutes, 30 seconds'
+WHERE PerkID = 7 AND Level = 5
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 HP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 7 AND Level = 6
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 14 HP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 7 AND Level = 7
+
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 5 minutes'
+WHERE PerkID = 103 AND Level = 1
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 4 minutes, 30 seconds'
+WHERE PerkID = 103 AND Level = 2
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 6 FP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 103 AND Level = 3
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 4 minutes'
+WHERE PerkID = 103 AND Level = 4
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 3 minutes, 30 seconds'
+WHERE PerkID = 103 AND Level = 5
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 10 FP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 103 AND Level = 6
+UPDATE dbo.PerkLevel
+SET Description = 'Restores 14 FP every 6 seconds. Recast time: 3 minutes'
+WHERE PerkID = 103 AND Level = 7

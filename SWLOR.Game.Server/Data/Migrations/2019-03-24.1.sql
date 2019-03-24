f50b8769fdc82b640421186d7d2788b93c8a1c0a
zunath <coolty3001@yahoo.com>
2019-03-24 19:02:16 -0400

    Don't drop the custom effect category ID. It's referenced in the DB still

--- SWLOR.Game.Server/Data/Migrations/2019-03-24.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-03-24.1.sql
@@ -15,11 +15,6 @@ EXEC dbo.ADM_Drop_Column @TableName = N'dbo.CustomEffect' , -- nvarchar(200)
 EXEC dbo.ADM_Drop_Column @TableName = N'dbo.CustomEffect' , -- nvarchar(200)
                          @ColumnName = N'WornOffMessage'  -- nvarchar(200)
 
--- Drop the CustomEffectCategoryID as well. Stored in the handler classes.
-EXEC dbo.ADM_Drop_Column @TableName = N'dbo.CustomEffect' , -- nvarchar(200)
-                         @ColumnName = N'CustomEffectCategoryID'  -- nvarchar(200)
-
-			
 -- Remove the food effect until we implement cooking.
 DELETE FROM dbo.CustomEffect
 WHERE ID = 25

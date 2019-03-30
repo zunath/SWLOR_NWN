08660ad353aee23a2d84cde1944f10c1bcfb312e
Tyler <coolty3001@yahoo.com>
2019-03-29 21:45:35 -0400

    Attempt to clean stores up every 30 minutes. (#867)

--- SWLOR.Game.Server/Event/Store/on_close_store.cs
+++ SWLOR.Game.Server/Event/Store/on_close_store.cs
@@ -1,18 +1,17 @@
-﻿using SWLOR.Game.Server;
-using SWLOR.Game.Server.Event.Player;
+﻿using SWLOR.Game.Server.Event.Store;
 using SWLOR.Game.Server.Messaging;
 
 // ReSharper disable once CheckNamespace
 namespace NWN.Scripts
 {
 #pragma warning disable IDE1006 // Naming Styles
-    internal class pc_on_heartbeat
+    internal class on_close_store
 #pragma warning restore IDE1006 // Naming Styles
     {
         // ReSharper disable once UnusedMember.Local
         private static void Main()
         {
-            MessageHub.Instance.Publish(new OnPlayerHeartbeat());
+            MessageHub.Instance.Publish(new OnCloseStore());
         }
     }
 }

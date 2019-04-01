20cdfef54dce16bc7a2b0a6a7b87cb27367627f8
zunath <coolty3001@yahoo.com>
2019-03-31 22:16:25 -0400

    Calculate based on tier for the 51-capped stats. Update all of the mod item names.

--- SWLOR.Game.Server/Mod/HarvestingMod.cs
+++ SWLOR.Game.Server/Mod/HarvestingMod.cs
@@ -19,8 +19,9 @@ namespace SWLOR.Game.Server.Mod
 
         public void Apply(NWPlayer player, NWItem target, params string[] args)
         {
-            int value = Convert.ToInt32(args[0]);
-            int newValue = target.HarvestingBonus + value;
+            int tier = Convert.ToInt32(args[0]);
+            int amount = tier * 3;
+            int newValue = target.HarvestingBonus + amount;
             if (newValue > MaxValue) newValue = MaxValue;
             target.HarvestingBonus = newValue;
         }
--- SWLOR.Game.Server/Mod/HpMod.cs
+++ SWLOR.Game.Server/Mod/HpMod.cs
@@ -19,8 +19,9 @@ namespace SWLOR.Game.Server.Mod
 
         public void Apply(NWPlayer player, NWItem target, params string[] args)
         {
-            int value = Convert.ToInt32(args[0]);
-            int newValue = target.HPBonus + value;
+            int tier = Convert.ToInt32(args[0]);
+            int amount = tier * 3;
+            int newValue = target.HPBonus + amount;
             if (newValue > MaxValue) newValue = MaxValue;
             target.HPBonus = newValue;
         }

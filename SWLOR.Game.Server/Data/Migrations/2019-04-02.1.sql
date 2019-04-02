d9ddab035849988b58a006050a5cefb612042c5d
zunath <coolty3001@yahoo.com>
2019-04-02 14:54:34 -0400

    Add icons to new force abilities. Remove defense/potency mods. Provide error message to players if they try to use a disabled mod. Rename Activation Speed to Cooldown Reduction.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-04-02.1.sql
@@ -0,0 +1,18 @@
+﻿
+-- Disable the force Defense/Potency mod blueprints
+UPDATE dbo.CraftBlueprint
+SET IsActive = 0
+WHERE ID IN (333,334,352,129,162,192,389,405,412,195,323,329,364,367,368,128,161,191,371,381,384,132,165,180)
+
+-- Rename Activation Speed to Cooldown Reduction
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Cooldown Reduction +1'
+WHERE ID = 121
+
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Cooldown Reduction +2'
+WHERE ID = 152
+
+UPDATE dbo.CraftBlueprint
+SET ItemName = 'Cooldown Reduction +3'
+WHERE ID = 184

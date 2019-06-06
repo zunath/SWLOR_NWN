6980f334db7f82712c2813a889e9c68e8822360f
zunath <coolty3001@yahoo.com>
2019-06-06 16:40:02 -0400

    Handle lightsaber appearance transitions.

--- SWLOR.Game.Server/Service/ModuleMigrationService.cs
+++ SWLOR.Game.Server/Service/ModuleMigrationService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using NWN;
+using SWLOR.Game.Server.Bioware;
 using SWLOR.Game.Server.Data;
 using SWLOR.Game.Server.Data.Entity;
 using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.GameObject;
 using SWLOR.Game.Server.Messaging;
 using SWLOR.Game.Server.NWN.Events.Module;
+using SWLOR.Game.Server.NWNX;
 
 namespace SWLOR.Game.Server.Service
 {
@@ -38,7 +40,7 @@ namespace SWLOR.Game.Server.Service
                 {
                     NWItem deserialized = SerializationService.DeserializeItem(item.ItemObject, storage);
                     PlayerMigrationService.ProcessVersion6RemoveACFromItem(deserialized);
-                    PlayerMigrationService.ProcessVersion6LightsaberRename(deserialized);
+                    ProcessVersion1LightsaberItem(deserialized);
                     item.ItemObject = SerializationService.Serialize(deserialized);
                     DataService.Connection.Update(item);
                     deserialized.Destroy();
@@ -50,7 +52,7 @@ namespace SWLOR.Game.Server.Service
                 {
                     NWItem deserialized = SerializationService.DeserializeItem(item.ItemObject, storage);
                     PlayerMigrationService.ProcessVersion6RemoveACFromItem(deserialized);
-                    PlayerMigrationService.ProcessVersion6LightsaberRename(deserialized);
+                    ProcessVersion1LightsaberItem(deserialized);
                     item.ItemObject = SerializationService.Serialize(deserialized);
                     DataService.Connection.Update(item);
                     deserialized.Destroy();
@@ -62,7 +64,7 @@ namespace SWLOR.Game.Server.Service
                 {
                     NWItem deserialized = SerializationService.DeserializeItem(item.ItemObject, storage);
                     PlayerMigrationService.ProcessVersion6RemoveACFromItem(deserialized);
-                    PlayerMigrationService.ProcessVersion6LightsaberRename(deserialized);
+                    ProcessVersion1LightsaberItem(deserialized);
                     item.ItemObject = SerializationService.Serialize(deserialized);
                     DataService.Connection.Update(item);
                     deserialized.Destroy();
@@ -74,7 +76,7 @@ namespace SWLOR.Game.Server.Service
                 {
                     NWItem deserialized = SerializationService.DeserializeItem(item.ItemObject, storage);
                     PlayerMigrationService.ProcessVersion6RemoveACFromItem(deserialized);
-                    PlayerMigrationService.ProcessVersion6LightsaberRename(deserialized);
+                    ProcessVersion1LightsaberItem(deserialized);
                     item.ItemObject = SerializationService.Serialize(deserialized);
                     DataService.Connection.Update(item);
                     deserialized.Destroy();
@@ -87,6 +89,73 @@ namespace SWLOR.Game.Server.Service
             }
 
             DataService.SubmitDataChange(config, DatabaseActionType.Update);
+        }
+
+        private static void ProcessVersion1LightsaberItem(NWItem item)
+        {
+            // Lightsaber appearances have changed because they're now considered light foils. 
+            // Depending on the type of lightsaber, modify the name, appearance and other details of the item.
+            int baseItemType;
+
+            // Appearance strings were built by running NWNXItem.GetEntireItemAppearance on the new design.
+            // Then the result was copied over to here. Unfortunately, setting each individual part's graphic didn't work
+            // so I used this instead.
+            string appearanceString;
+
+            switch (item.Resref)
+            {
+                // Blue lightsabers
+                case "lightsaber_b":
+                case "lightsaber_1":
+                case "lightsaber_2":
+                case "lightsaber_3":
+                case "lightsaber_4":
+                    baseItemType = 1; // 1 = Longsword
+                    appearanceString = "000000000000FBFBD300000000000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+                    break;
+
+                // Red Lightsabers
+                case "lightsaber_r_b":
+                case "lightsaber_r_1":
+                case "lightsaber_r_2":
+                case "lightsaber_r_3":
+                case "lightsaber_r_4":
+                    baseItemType = 1; // 1 = Longsword
+                    appearanceString = "000000000000FBFBD500000000000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+                    break;
+
+                // Green Lightsabers
+                case "lightsaber_g_b":
+                case "lightsaber_g_1":
+                case "lightsaber_g_2":
+                case "lightsaber_g_3":
+                case "lightsaber_g_4":
+                    baseItemType = 1; // 1 = Longsword
+                    appearanceString = "000000000000FBFBD400000000000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+                    break;
+
+                // Yellow Lightsabers
+                case "lightsaber_y_b":
+                case "lightsaber_y_1":
+                case "lightsaber_y_2":
+                case "lightsaber_y_3":
+                case "lightsaber_y_4":
+                    baseItemType = 1; // 1 = Longsword
+                    appearanceString = "000000000000FBFBB500000000000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+                    // Also give a VFX item property for yellow sabers.
+                    var ip = _.ItemPropertyVisualEffect(_.ITEM_VISUAL_HOLY);
+                    BiowareXP2.IPSafeAddItemProperty(item, ip, 0.0f, AddItemPropertyPolicy.ReplaceExisting, false, false);
+                    break;
+
+                default: return;
+            }
+
+
+            NWNXItem.SetBaseItemType(item, baseItemType);
+            NWNXItem.RestoreItemAppearance(item, appearanceString);
+
+            item.SetLocalInt("LIGHTSABER", _.TRUE);
+            item.Name = item.Name.Replace("Lightsaber", "Light Foil").Replace("Saberstaff", "Lightfoil Staff");
         }
 
     }

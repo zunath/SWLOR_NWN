d34bee3b8dcec0cf187b903830ac93a30757499a
zunath <coolty3001@yahoo.com>
2019-06-12 19:41:19 -0400

    Remove the epic dodge feat from Force Speed.

--- SWLOR.Game.Server/Perk/ForceControl/ForceSpeed.cs
+++ SWLOR.Game.Server/Perk/ForceControl/ForceSpeed.cs
@@ -1,47 +1,13 @@
 ﻿using System;
 using NWN;
-using NWN.Scripts;
 using SWLOR.Game.Server.Enumeration;
-using SWLOR.Game.Server.Event.Effect;
 using SWLOR.Game.Server.GameObject;
-using SWLOR.Game.Server.Messaging;
-using SWLOR.Game.Server.NWNX;
 using SWLOR.Game.Server.Service;
-
-
-// ReSharper disable once CheckNamespace
-namespace NWN.Scripts
-{
-#pragma warning disable IDE1006 // Naming Styles
-    internal class force_speed_exp
-#pragma warning restore IDE1006 // Naming Styles
-    {
-        // This one's a little weird.
-        // The Epic Dodge feat is granted to players while this effect is active.
-        // We apply it at the time the effect is applied, but we need to register and subscribe to 
-        // the event which fires at the time it expires, so that we can remove the feat.
-
-        // ReSharper disable once UnusedMember.Local
-        private static void Main()
-        {
-            var data = NWNXEffect.GetEffectExpiredData();
-            var creator = NWNXEffect.GetEffectExpiredCreator();
-            OnEffectExpired<force_speed_exp> @event = new OnEffectExpired<force_speed_exp>(data, creator);
-            MessageHub.Instance.Publish(@event);
-        }
-    }
-}
-
 
 namespace SWLOR.Game.Server.Perk.ForceControl
 {
     public class ForceSpeed: IPerkHandler
     {
-        public static void SubscribeEvents()
-        {
-            MessageHub.Instance.Subscribe<OnEffectExpired<force_speed_exp>>(message => OnEffectExpired(message.Data, message.Creator, message.AppliedTo));
-        }
-
         public PerkType PerkType => PerkType.ForceSpeed;
         public string CanCastSpell(NWPlayer oPC, NWObject oTarget, int spellTier)
         {
@@ -107,11 +73,9 @@ namespace SWLOR.Game.Server.Perk.ForceControl
                     break;
                 case 5:
                     effect = _.EffectMovementSpeedIncrease(50);
-                    effect = _.EffectLinkEffects(effect, _.EffectAbilityIncrease(_.ABILITY_DEXTERITY, 6));
+                    effect = _.EffectLinkEffects(effect, _.EffectAbilityIncrease(_.ABILITY_DEXTERITY, 10));
                     effect = _.EffectLinkEffects(effect, _.EffectModifyAttacks(1));
                     duration = 180f;
-                    NWNXCreature.AddFeatByLevel(target.Object, _.FEAT_EPIC_DODGE, 1);
-                    effect = NWNXEffect.SetEffectExpiredScript(effect, "force_speed_exp");
                     break;
                 default:
                     throw new ArgumentException(nameof(perkLevel) + " invalid. Value " + perkLevel + " is unhandled.");
@@ -158,14 +122,8 @@ namespace SWLOR.Game.Server.Perk.ForceControl
             return false;
         }
 
-        private static void OnEffectExpired(string[] data, NWObject creator, NWObject appliedTo)
-        {
-            NWNXCreature.RemoveFeat(appliedTo.Object, _.FEAT_EPIC_DODGE);
-        }
-
         public void OnConcentrationTick(NWPlayer player, NWObject target, int perkLevel, int tick)
         {
-            throw new NotImplementedException();
         }
     }
 }

0b63637ea0bd31e69861e3a033f547386f2ea788
zunath <coolty3001@yahoo.com>
2019-06-12 18:55:04 -0400

    Move all force abilities under their respective skill's category in the perk menu.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-06-12.1.sql
@@ -0,0 +1,27 @@
+﻿
+UPDATE dbo.PerkCategory
+SET Name = 'Force Alter'
+WHERE ID = 40
+
+UPDATE dbo.PerkCategory
+SET Name = 'Force Control'
+WHERE ID = 43
+
+UPDATE dbo.PerkCategory
+SET Name = 'Force Sense'
+WHERE ID = 46
+
+UPDATE dbo.Perk
+SET PerkCategoryID = 40
+WHERE PerkCategoryID IN (41, 42, 49)
+
+UPDATE dbo.Perk
+SET PerkCategoryID = 43
+WHERE PerkCategoryID IN (44, 45, 50)
+
+UPDATE dbo.Perk
+SET PerkCategoryID = 46
+WHERE PerkCategoryID IN (47, 48, 51)
+
+DELETE FROM dbo.PerkCategory
+WHERE ID IN (41, 42, 49, 44, 45, 50, 47, 48, 51)

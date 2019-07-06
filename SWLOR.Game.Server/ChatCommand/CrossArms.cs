1d9dd91daf4daa51db13679935026ff6bb3a70dc
givemedeath <benco.moore@gmail.com>
2019-07-05 21:33:46 -0400

    Emote Animations
    Add new looping animation emotes:
    -cower
    -pointing
    -crouch
    -crossarms
    Change default length of looping animation emotes to 9999.

---
+++ SWLOR.Game.Server/ChatCommand/CrossArms.cs
@@ -0,0 +1,19 @@
+﻿using NWN;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN._;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Plays a cross arms animation.", CommandPermissionType.Player | CommandPermissionType.DM)]
+    public class CrossArms : LoopingAnimationCommand
+    {
+        protected override void DoAction(NWPlayer user, float duration)
+        {
+            user.AssignCommand(() =>
+            {
+                _.ActionPlayAnimation(ANIMATION_LOOPING_CUSTOM4, 1.0f, duration);
+            });
+        }
+    }
+}

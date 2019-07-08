18784a1d5da70b595410902fce6fc2f78e546a1a
zunath <coolty3001@yahoo.com>
2019-07-08 01:56:41 -0400

    Add Tat content from Lestat. Add /time command which returns the current server date and time.

---
+++ SWLOR.Game.Server/ChatCommand/Time.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NWN;
+using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using static NWN._;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Returns the current UTC server time.", CommandPermissionType.Player | CommandPermissionType.DM)]
+    public class Time : IChatCommand
+    {
+        public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
+        {
+            DateTime now = DateTime.UtcNow;
+            string nowText = now.ToString("yyyy-MM-dd hh:mm:ss");
+
+            user.SendMessage("Current Server Date: " + nowText);
+        }
+
+        public string ValidateArguments(NWPlayer user, params string[] args)
+        {
+            return string.Empty;
+        }
+
+        public bool RequiresTarget => false;
+    }
+}

e6aba5e3a9c2b2e514f97de39394b9c0319f61ad
zunath <coolty3001@yahoo.com>
2019-07-16 01:58:44 -0400

    Few more

--- SWLOR.Game.Server/Caching/PCMapPinCache.cs
+++ SWLOR.Game.Server/Caching/PCMapPinCache.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Collections.Generic;
 using SWLOR.Game.Server.Data.Entity;
 
 namespace SWLOR.Game.Server.Caching
 {
     public class PCMapPinCache: CacheBase<PCMapPin>
     {
+        private Dictionary<Guid, Dictionary<Guid, PCMapPin>> ByPlayerID { get; } = new Dictionary<Guid, Dictionary<Guid, PCMapPin>>();
+
         protected override void OnCacheObjectSet(PCMapPin entity)
         {
+            SetEntityIntoDictionary(entity.PlayerID, entity.ID, entity, ByPlayerID);
         }
 
         protected override void OnCacheObjectRemoved(PCMapPin entity)
         {
+            RemoveEntityFromDictionary(entity.PlayerID, entity.ID, ByPlayerID);
         }
 
         protected override void OnSubscribeEvents()
@@ -20,6 +25,14 @@ namespace SWLOR.Game.Server.Caching
         public PCMapPin GetByID(Guid id)
         {
             return ByID[id];
+        }
+
+        public IEnumerable<PCMapPin> GetAllByPlayerID(Guid playerID)
+        {
+            if(!ByPlayerID.ContainsKey(playerID))
+                return new List<PCMapPin>();
+
+            return ByPlayerID[playerID].Values;
         }
     }
 }

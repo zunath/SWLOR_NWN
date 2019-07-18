ecb30d52d04e0f90ece3d032e33ffe47f86b07ca
zunath <coolty3001@yahoo.com>
2019-07-18 12:11:11 -0400

    Don't require a target for the /bug command. Players get confused about it and bugs don't get submitted properly.

--- SWLOR.Game.Server/ChatCommand/Bug.cs
+++ SWLOR.Game.Server/ChatCommand/Bug.cs
@@ -33,7 +33,7 @@ namespace SWLOR.Game.Server.ChatCommand
                 SenderPlayerID = user.IsPlayer ? new Guid?(user.GlobalID): null,
                 CDKey = _.GetPCPublicCDKey(user),
                 Text = message,
-                TargetName = target.IsValid ? target.Name : string.Empty,
+                TargetName = target.IsValid ? target.Name : user.Name,
                 AreaResref = user.Area.Resref,
                 SenderLocationX = user.Location.X,
                 SenderLocationY = user.Location.Y,
@@ -58,6 +58,6 @@ namespace SWLOR.Game.Server.ChatCommand
             return string.Empty;
         }
 
-        public bool RequiresTarget => true;
+        public bool RequiresTarget => false;
     }
 }

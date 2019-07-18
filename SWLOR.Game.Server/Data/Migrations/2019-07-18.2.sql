c540d97abc5644ca99c20873b98f55c1c10fcc0f
zunath <coolty3001@yahoo.com>
2019-07-18 11:51:18 -0400

    Remove DataPackage, DiscordChatQueue, and User objects, caches, unit tests, and tables. These are deprecated and won't be used any longer. Update NSubstitute nuget package.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-07-18.2.sql
@@ -0,0 +1,12 @@
+﻿
+-- Remove deprecated tables: DataPackage, DiscordChatQueue, User
+
+
+DROP TABLE [dbo].[DataPackage]
+GO
+DROP TABLE [dbo].[DiscordChatQueue]
+GO
+DROP TABLE [dbo].[User]
+GO
+
+

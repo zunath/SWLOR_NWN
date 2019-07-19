fec678f60bb59d775e7b7adc84b2c8f940454e6f
zunath <coolty3001@yahoo.com>
2019-07-19 14:49:39 -0400

    Fix AI for crystal spiders and give a claw to the Vellen Flesheaters.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-07-19.1.sql
@@ -0,0 +1,28 @@
+﻿
+INSERT INTO dbo.Spawn ( ID ,
+                        Name ,
+                        SpawnObjectTypeID )
+VALUES ( 30 ,   -- ID - int
+         N'Viscara - Crystal Cavern' , -- Name - nvarchar(64)
+         1     -- SpawnObjectTypeID - int
+    )
+
+INSERT INTO dbo.SpawnObject ( ID ,
+                              SpawnID ,
+                              Resref ,
+                              Weight ,
+                              SpawnRule ,
+                              NPCGroupID ,
+                              BehaviourScript ,
+                              DeathVFXID ,
+                              AIFlags )
+VALUES ( 5 ,   -- ID - int
+         30 ,   -- SpawnID - int
+         N'crystalspider' , -- Resref - nvarchar(16)
+         10 ,   -- Weight - int
+         N'' , -- SpawnRule - nvarchar(32)
+         16 ,   -- NPCGroupID - int
+         N'StandardBehaviour' , -- BehaviourScript - nvarchar(64)
+         0 ,   -- DeathVFXID - int
+         15     -- AIFlags - int
+    )

881bca0382bd0f49e2bf11d6dea2d37b6422e962
zunath <coolty3001@yahoo.com>
2019-07-21 10:38:31 -0400

    Increase belt base levels.

--- SWLOR.Game.Server/Data/Migrations/2019-07-21.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-07-21.1.sql
@@ -4,3 +4,22 @@ UPDATE dbo.PerkFeat
 SET ConcentrationTickInterval = 6,
 	ConcentrationFPCost = ConcentrationFPCost + 2
 WHERE PerkID IN (86, 179)
+
+
+
+-- Increase belt base levels.
+
+-- Force Belt 3
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 23
+WHERE ID = 271
+
+-- Heavy Belt 2
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 16
+WHERE ID = 273
+
+-- Heavy Belt 3
+UPDATE dbo.CraftBlueprint
+SET BaseLevel = 24
+WHERE ID = 274

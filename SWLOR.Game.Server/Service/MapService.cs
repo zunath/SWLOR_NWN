6c6f417f2e234995bf70c5c130c860f438be531e
zunath <coolty3001@yahoo.com>
2019-07-21 12:23:09 -0400

    Update NWNXPlayer. Use the NWNX Get/SetAreaExplorationState instead of our method since it's quicker.

--- SWLOR.Game.Server/Service/MapService.cs
+++ SWLOR.Game.Server/Service/MapService.cs
@@ -8,6 +8,7 @@ using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.Event.Area;
 using SWLOR.Game.Server.Event.Module;
 using SWLOR.Game.Server.Messaging;
+using SWLOR.Game.Server.NWNX;
 using SWLOR.Game.Server.ValueObject;
 using static NWN._;
 
@@ -65,49 +66,27 @@ namespace SWLOR.Game.Server.Service
         private static void SaveMapProgression(NWArea area, NWPlayer player)
         {
             var map = DataService.PCMapProgression.GetByPlayerIDAndAreaResrefOrDefault(player.GlobalID, area.Resref);
-            int areaSize = area.Width * area.Height;
             DatabaseActionType action = DatabaseActionType.Update;
 
-            if (map == null || areaSize != map.Progression.Length)
+            if (map == null)
             {
-                if (map != null)
-                {
-                    DataService.SubmitDataChange(map, DatabaseActionType.Delete);
-                }
-
                 map = new PCMapProgression
                 {
                     PlayerID = player.GlobalID,
                     AreaResref = area.Resref,
-                    Progression = string.Empty.PadLeft(areaSize, '0')
+                    Progression = string.Empty
                 };
 
                 action = DatabaseActionType.Insert;
             }
-            
-            string progression = string.Empty;
-            for (int x = 0; x < area.Width; x++)
-            {
-                for (int y = 0; y < area.Height; y++)
-                {
-                    bool visible = _.GetTileExplored(player, area, x, y) == TRUE;
-                    progression += visible ? '1' : '0';
-                }
-            }
-            
-            map.Progression = progression;
+
+            map.Progression = NWNXPlayer.GetAreaExplorationState(player, area);
             DataService.SubmitDataChange(map, action);
         }
 
         private static void LoadMapProgression(NWArea area, NWPlayer player)
         {
             var map = DataService.PCMapProgression.GetByPlayerIDAndAreaResrefOrDefault(player.GlobalID, area.Resref);
-            int areaSize = area.Width * area.Height;
-
-            // Only do the loading process once per player per area.
-            string variableName = "MAP_AREA_LOADED_" + area.Resref;
-            if (player.GetLocalInt(variableName) == TRUE) return;
-            player.SetLocalInt(variableName, TRUE);
 
             // No progression set - do a save which will create the record.
             if (map == null)
@@ -116,24 +95,7 @@ namespace SWLOR.Game.Server.Service
                 return;
             }
             
-            // Reset player's progression since the area size has changed.
-            if (map.Progression.Length != areaSize)
-            {
-                SaveMapProgression(area, player);
-                return;
-            }
-
-            // Otherwise everything checks out. Load the tile visibility.
-            int count = 0;
-            for (int x = 0; x < area.Width; x++)
-            {
-                for (int y = 0; y < area.Height; y++)
-                {
-                    bool visible = map.Progression[count] == '1';
-                    _.SetTileExplored(player, area, x, y, visible ? 1 : 0);
-                    count++;
-                }
-            }
+            NWNXPlayer.SetAreaExplorationState(player, area, map.Progression);
         }
 
 

8423350812aae61f23bbf9ef240a2a6b0da83808
zunath <coolty3001@yahoo.com>
2019-07-21 11:05:33 -0400

    Remove the heartbeat event from resources no matter what. Previously it was only getting removed if the resource had a prop (like a tree). Everything without a prop was continuing to call indefinitely.

--- SWLOR.Game.Server.Scripts/Placeable/Resource/OnHeartbeat.cs
+++ SWLOR.Game.Server.Scripts/Placeable/Resource/OnHeartbeat.cs
@@ -17,6 +17,10 @@ namespace SWLOR.Game.Server.Scripts.Placeable.Resource
         public void Main()
         {
             NWPlaceable objSelf = (NWGameObject.OBJECT_SELF);
+
+            // No matter what, we want to remove the heartbeat script from this resource.
+            _.SetEventScript(objSelf, _.EVENT_SCRIPT_PLACEABLE_ON_HEARTBEAT, string.Empty);
+
             bool hasSpawnedProp = objSelf.GetLocalInt("RESOURCE_PROP_SPAWNED") == 1;
             if (hasSpawnedProp) return;
 
@@ -27,8 +31,6 @@ namespace SWLOR.Game.Server.Scripts.Placeable.Resource
             NWPlaceable prop = (_.CreateObject(_.OBJECT_TYPE_PLACEABLE, propResref, location));
             objSelf.SetLocalObject("RESOURCE_PROP_OBJ", prop.Object);
             objSelf.SetLocalInt("RESOURCE_PROP_SPAWNED", 1);
-
-            _.SetEventScript(objSelf, _.EVENT_SCRIPT_PLACEABLE_ON_HEARTBEAT, string.Empty);
         }
     }
 }

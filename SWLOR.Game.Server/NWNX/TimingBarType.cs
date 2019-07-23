2df29003ef826000ca6cdc55a28c6bb8fc4ea553
Z <coolty3001@yahoo.com>
2019-07-23 16:09:05 -0400

    Update NWNX_Events event types. Add the OnItemDecrementStack event. Ignore any decrements for shuriken, dart, and throwing axe.

---
+++ SWLOR.Game.Server/NWNX/TimingBarType.cs
@@ -0,0 +1,15 @@
+﻿namespace SWLOR.Game.Server.NWNX
+{
+    public enum TimingBarType
+    {
+        Flag = 1,
+        Recover = 2, 
+        Disarm = 3,
+        Examine = 4,
+        Set = 5,
+        Rest = 6,
+        Unlock = 7,
+        Lock = 8,
+        Custom = 10
+    }
+}

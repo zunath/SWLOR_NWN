9cbff6a03ecb3032edd587672dc5ad95299e6054
Z <coolty3001@yahoo.com>
2019-08-06 11:18:16 -0400

    Add missing loot table for kath hound meat quest.

--- SWLOR.Game.Server/Data/Migrations/2019-08-03.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2019-08-03.1.sql
@@ -33,6 +33,26 @@ ALTER COLUMN QuestState INT NOT NULL
 GO
 
 
+-- Add missing loot table for kath hound meat quest.
+INSERT INTO dbo.LootTable ( ID ,
+                            Name )
+VALUES ( 44 , -- ID - int
+         N'Kath Hounds 2 - Viscara' -- Name - nvarchar(64)
+    )
+
+INSERT INTO dbo.LootTableItem ( LootTableID ,
+                                Resref ,
+                                MaxQuantity ,
+                                Weight ,
+                                IsActive ,
+                                SpawnRule )
+VALUES ( 44 ,    -- LootTableID - int
+         'kath_meat_1' ,   -- Resref - varchar(16)
+         1 ,    -- MaxQuantity - int
+         10 ,    -- Weight - tinyint
+         1 , -- IsActive - bit
+         N''    -- SpawnRule - nvarchar(64)
+    )
 
 -- Update the GetPlayerData proc. Run this after all other DDL changes have run.
 

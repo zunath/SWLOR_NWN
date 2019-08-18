e0c26fb89855f058553bedf29419d75549ca72d6
Cameron Valdez <cameronav23@gmail.com>
2019-08-18 14:48:06 -0500

    Data Migration for Throwing Axes and Two Tat Quests

---
+++ SWLOR.Game.Server/Data/Migrations/2019-08-18.1.sql
@@ -0,0 +1,25 @@
+﻿-- Throwing Axes
+INSERT INTO dbo.CraftBlueprint ( ID ,CraftCategoryID ,BaseLevel ,ItemName ,ItemResref ,Quantity ,SkillID ,CraftDeviceID ,PerkID ,RequiredPerkLevel ,IsActive ,
+MainComponentTypeID ,MainMinimum ,SecondaryComponentTypeID ,SecondaryMinimum ,TertiaryComponentTypeID ,TertiaryMinimum ,EnhancementSlots ,MainMaximum ,
+SecondaryMaximum ,TertiaryMaximum ,BaseStructureID )
+VALUES ( 683 ,12 ,2 , N'Basic Axe' , N'axe_b' ,  1 , 12 , 2 , NULL , 0 ,  1 , 4 ,  1 ,  8 ,  1 ,  0 ,  0 ,  1 ,  2 ,  2 ,  0 ,  NULL)
+
+INSERT INTO dbo.CraftBlueprint ( ID ,CraftCategoryID ,BaseLevel ,ItemName ,ItemResref ,Quantity ,SkillID ,CraftDeviceID ,PerkID ,RequiredPerkLevel ,IsActive ,
+MainComponentTypeID ,MainMinimum ,SecondaryComponentTypeID ,SecondaryMinimum ,TertiaryComponentTypeID ,TertiaryMinimum ,EnhancementSlots ,MainMaximum ,
+SecondaryMaximum ,TertiaryMaximum ,BaseStructureID )
+VALUES ( 684 ,12 ,5 , N'Axe I' , N'axe_1' ,  1 , 12 , 2 , NULL , 0 ,  1 , 4 ,  1 ,  8 ,  1 ,  0 ,  0 ,  2 ,  2 ,  2 ,  0 ,  NULL)
+
+INSERT INTO dbo.CraftBlueprint ( ID ,CraftCategoryID ,BaseLevel ,ItemName ,ItemResref ,Quantity ,SkillID ,CraftDeviceID ,PerkID ,RequiredPerkLevel ,IsActive ,
+MainComponentTypeID ,MainMinimum ,SecondaryComponentTypeID ,SecondaryMinimum ,TertiaryComponentTypeID ,TertiaryMinimum ,EnhancementSlots ,MainMaximum ,
+SecondaryMaximum ,TertiaryMaximum ,BaseStructureID )
+VALUES ( 685 ,12 ,10 , N'Axe II' , N'axe_2' ,  1 , 12 , 2 , NULL , 0 ,  1 , 4 ,  1 ,  8 ,  1 ,  0 ,  0 ,  3 ,  2 ,  2 ,  0 ,  NULL)
+
+INSERT INTO dbo.CraftBlueprint ( ID ,CraftCategoryID ,BaseLevel ,ItemName ,ItemResref ,Quantity ,SkillID ,CraftDeviceID ,PerkID ,RequiredPerkLevel ,IsActive ,
+MainComponentTypeID ,MainMinimum ,SecondaryComponentTypeID ,SecondaryMinimum ,TertiaryComponentTypeID ,TertiaryMinimum ,EnhancementSlots ,MainMaximum ,
+SecondaryMaximum ,TertiaryMaximum ,BaseStructureID )
+VALUES ( 686 ,12 ,15 , N'Axe III' , N'axe_3' ,  1 , 12 , 2 , NULL , 0 ,  1 , 4 ,  1 ,  8 ,  1 ,  0 ,  0 ,  4 ,  2 ,  2 ,  0 ,  NULL)
+
+INSERT INTO dbo.CraftBlueprint ( ID ,CraftCategoryID ,BaseLevel ,ItemName ,ItemResref ,Quantity ,SkillID ,CraftDeviceID ,PerkID ,RequiredPerkLevel ,IsActive ,
+MainComponentTypeID ,MainMinimum ,SecondaryComponentTypeID ,SecondaryMinimum ,TertiaryComponentTypeID ,TertiaryMinimum ,EnhancementSlots ,MainMaximum ,
+SecondaryMaximum ,TertiaryMaximum ,BaseStructureID )
+VALUES ( 687 ,12 ,20 , N'Dart IV' , N'axe_4' ,  1 , 12 , 2 , NULL , 0 ,  1 , 4 ,  1 ,  8 ,  1 ,  0 ,  0 ,  5 ,  2 ,  2 ,  0 ,  NULL)

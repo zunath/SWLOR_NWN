5b33b5a7c02f0d512d514031f7459cac932db35a
Cameron Valdez <cameronav23@gmail.com>
2019-08-18 16:20:41 -0500

    Data Migration for Throwing Axes and Two Tat Quests (#976)
    * Data migration for Tat Monsters
    
    * Update SWLOR.Game.Server.csproj
    
    * Update 2019-08-12.1.sql
    
    * Update SWLOR.Game.Server.csproj
    
    * Update SWLOR.Game.Server.csproj
    
    * Update SWLOR.Game.Server.csproj
    
    * Update SWLOR.Game.Server.csproj
    
    * Update SWLOR.Game.Server.csproj
    
    * Update SWLOR.Game.Server.csproj
    
    * Data Migration for Throwing Axes and Two Tat Quests
    
    * Update 2019-08-18.1.sql
    
    * Update WorkinForTheMan.cs

---
+++ SWLOR.Game.Server.Scripts/Quest/StinkyWomprats.cs
@@ -0,0 +1,16 @@
+﻿using SWLOR.Game.Server.Quest;
+
+namespace SWLOR.Game.Server.Scripts.Quest
+{
+    public class StinkyWomprats : AbstractQuest
+    {
+        public StinkyWomprats()
+        {
+            CreateQuest(32, "Stinky Womprats", "stinky_womprats")           
+                .AddObjectiveKillTarget(1, NPCGroupType.Womprat, 20)
+                .AddObjectiveTalkToNPC(2)
+                .AddRewardGold(1000)
+                .AddRewardItem("xp_tome_4", 1);
+        }
+    }
+}

e528708835108cd3707a2c4113fc4a9a77e4c6bc
Z <coolty3001@yahoo.com>
2019-08-19 09:50:51 -0400

    Add Tatooine npc group types to the NPCGroupType enum. Add the Tat quests to the project.

--- SWLOR.Game.Server.Scripts/Quest/StinkyWomprats.cs
+++ SWLOR.Game.Server.Scripts/Quest/StinkyWomprats.cs
@@ -7,7 +7,7 @@ namespace SWLOR.Game.Server.Scripts.Quest
         public StinkyWomprats()
         {
             CreateQuest(32, "Stinky Womprats", "stinky_womprats")           
-                .AddObjectiveKillTarget(1, NPCGroupType.Womprat, 20)
+                .AddObjectiveKillTarget(1, NPCGroupType.Tatooine_Womprat, 20)
                 .AddObjectiveTalkToNPC(2)
                 .AddRewardGold(1000)
                 .AddRewardItem("xp_tome_4", 1);

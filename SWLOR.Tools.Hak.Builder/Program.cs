3245241cb8323d0a2223e348f82d612b4166d0a1
Z <coolty3001@yahoo.com>
2019-08-20 23:07:09 -0400

    Optimize the compilation methods for haks.

--- SWLOR.Tools.Hak.Builder/Program.cs
+++ SWLOR.Tools.Hak.Builder/Program.cs
@@ -1,51 +1,68 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SWLOR.Tools.Hak.Builder
 {
     class Program
     {
         private static volatile int _processesComplete;
+        const string OutputFolder = "./output/";
 
         static void Main(string[] args)
         {
-            const string OutputFolder = "./output/";
 
-            var process = new Process
+            // Create the output folder.
+            if(!Directory.Exists(OutputFolder))
             {
-                StartInfo = new ProcessStartInfo("cmd.exe", "/K ")
-                {
-                    UseShellExecute = false, 
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }, 
-                EnableRaisingEvents = true
-            };
-            process.Exited += (sender, eventArgs) =>
-            {
-                _processesComplete++;
-                Console.WriteLine("Build complete. _processesComplete = " + _processesComplete);
-            };
-
-
-            var hakFolders = GetHakFolders();
-            foreach (var folder in hakFolders)
-            {
-                string command = $"nwn_erf -f \"{OutputFolder}{folder}.hak\" -e HAK -c ./{folder}";
-                Console.WriteLine($"Building hak: {folder}.hak");
-
-                process.StartInfo.Arguments = "/K /C " + command;
-                process.Start();
-
-                Console.WriteLine("Waiting for exit");
-                process.WaitForExit();
-                Console.WriteLine("Closing");
-                process.Close();
+                Directory.CreateDirectory(OutputFolder);
             }
 
-            // Model haks need to be compiled first (if the compilation flag is active).
+            // Copy the TLK to the output folder.
+            File.Copy("./swlor_tlk.tlk", $"{OutputFolder}/swlor_tlk.tlk");
 
+            // Build non-model haks.
+            var hakFolders = GetHakFolders();
+            Parallel.ForEach(hakFolders, (folder) =>
+            {
+                CompileHak(folder);
+            });
+
+            // Model files need to be compiled before being added to a hak file.
+            if(!Directory.Exists($"{OutputFolder}compiled_models"))
+            {
+                Directory.CreateDirectory($"{OutputFolder}compiled_models");
+            }
+
+            hakFolders = new string[] { "swlor_mdl", "swlor_mdl_p" };
+            Parallel.ForEach(hakFolders, (folder) =>
+            {
+                CompileModels(folder);
+            });
+
+            // Compilation is done. Now we insert the compiled models into the appropriate hak files.
+
+            // Clean up the compiled models folder.
+        }
+
+        private static Process CreateProcess(string command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd.exe", "/K " + command)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true, 
+                    CreateNoWindow = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            return process;
         }
 
         private static string[] GetHakFolders()
@@ -88,7 +105,43 @@ namespace SWLOR.Tools.Hak.Builder
                 "swlor_wav",
                 "swlor_wok"
             };
+        }
 
+        private static void CompileHak(string folder)
+        {
+            string command = $"nwn_erf -f \"{OutputFolder}{folder}.hak\" -e HAK -c ./{folder}";
+            Console.WriteLine($"Building hak: {folder}.hak");
+
+            using (var process = CreateProcess(command))
+            {
+                process.Start();
+
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+
+                string output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+            }
+        }
+
+        private static void CompileModels(string folder)
+        {
+            string command = $"nwnmdlcomp ./{folder}/* ./{OutputFolder}compiled_models/ -e";
+
+            Console.WriteLine($"Compiling models in folder: {folder}");
+
+            using (var process = CreateProcess(command))
+            {
+                process.Start();
+
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+
+                string output = process.StandardOutput.ReadToEnd();
+
+                process.WaitForExit();
+            }
         }
     }
 }

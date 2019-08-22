2ec481f52047f7f205b6a368f8d0f1c074957ce4
Z <coolty3001@yahoo.com>
2019-08-22 18:44:09 -0400

    Refund and remove the Seed Picker perk.

---
+++ SWLOR.Game.Server/Data/Migrations/2019-08-22.1.sql
@@ -0,0 +1,28 @@
+﻿
+EXEC dbo.ADM_RefundPlayerPerk @PerkID = 106 -- int
+
+DELETE FROM dbo.PCPerkRefund
+WHERE PerkID = 106
+
+DELETE FROM dbo.PerkFeat
+WHERE PerkID = 106
+
+DELETE FROM dbo.PerkLevelSkillRequirement
+WHERE PerkLevelID IN (
+	SELECT ID
+	FROM dbo.PerkLevel 
+	WHERE PerkID = 106
+)
+
+DELETE FROM dbo.PerkLevelQuestRequirement
+WHERE PerkLevelID IN (
+	SELECT ID
+	FROM dbo.PerkLevel 
+	WHERE PerkID = 106
+)
+
+DELETE FROM dbo.PerkLevel
+WHERE PerkID = 106
+
+DELETE FROM dbo.Perk
+WHERE ID = 106

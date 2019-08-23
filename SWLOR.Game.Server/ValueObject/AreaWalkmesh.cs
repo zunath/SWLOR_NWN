ae371b2c8a622e9f0eb2d3d60aa6fb3088200357
Z <coolty3001@yahoo.com>
2019-08-23 11:12:34 -0400

    Remove all database interaction from the walkmesh baking process. Store this data in memory since we have to check it at boot time every time.

---
+++ SWLOR.Game.Server/ValueObject/AreaWalkmesh.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SWLOR.Game.Server.ValueObject
+{
+    public class AreaWalkmesh
+    {
+        public AreaWalkmesh()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; set; }
+        public Guid AreaID { get; set; }
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        public double LocationZ { get; set; }
+    }
+}

9b796dd320aef2d0a41e1baf7db815bef357f164
Z <coolty3001@yahoo.com>
2019-08-25 17:46:38 -0400

    Stub out quest: Hutlar Power Investigation

---
+++ SWLOR.Game.Server.Scripts/Quest/Hutlar/HutlarPowerInvestigation.cs
@@ -0,0 +1,24 @@
+﻿using SWLOR.Game.Server.Quest;
+
+namespace SWLOR.Game.Server.Scripts.Quest.Hutlar
+{
+    public class HutlarPowerInvestigation: AbstractQuest
+    {
+        public HutlarPowerInvestigation()
+        {
+            CreateQuest(1003, "Hutlar Power Investigation", "hut_power_invest")
+                .AddPrerequisiteQuest(1000)
+                .AddPrerequisiteQuest(1001)
+                .AddPrerequisiteQuest(1002)
+                
+                .AddObjectiveUseObject(1)
+                .AddObjectiveUseObject(2)
+                .AddObjectiveUseObject(3)
+                .AddObjectiveUseObject(4)
+                .AddObjectiveUseObject(5)
+                .AddObjectiveTalkToNPC(6)
+                
+                .AddRewardGold(1200);
+        }
+    }
+}

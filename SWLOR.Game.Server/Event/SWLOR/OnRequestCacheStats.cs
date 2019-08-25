445c226ceec7b7115e740c930f03c21f2b02de9a
Z <coolty3001@yahoo.com>
2019-08-25 10:09:43 -0400

    Publish an OnRequestCacheStats when /getcachestats is called. Fix an issue where swapping concentration abilities wasn't removing old entries in the list. Display ConcentratingCreatures count in the /getcachestats output.

---
+++ SWLOR.Game.Server/Event/SWLOR/OnRequestCacheStats.cs
@@ -0,0 +1,14 @@
+﻿using SWLOR.Game.Server.GameObject;
+
+namespace SWLOR.Game.Server.Event.SWLOR
+{
+    public class OnRequestCacheStats
+    {
+        public NWPlayer Player { get; set; }
+
+        public OnRequestCacheStats(NWPlayer player)
+        {
+            Player = player;
+        }
+    }
+}

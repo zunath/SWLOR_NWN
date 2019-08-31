57d806243d9ccbb5d26bac15298eae7c40485db0
givemedeath <benco.moore@gmail.com>
2019-08-31 14:46:34 -0400

    Implement HoloCom into Rest Menu.

---
+++ SWLOR.Game.Server/ChatCommand/EndCall.cs
@@ -0,0 +1,51 @@
+﻿using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using SWLOR.Game.Server.Service;
+using static NWN._;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Ends your current HoloCom call.", CommandPermissionType.Player | CommandPermissionType.DM | CommandPermissionType.Admin)]
+    public class EndCall : IChatCommand
+    {
+        // not showing in chat menu and not a valid command.
+        public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
+        {
+            NWPlayer sourcePlayer = user;
+            NWPlayer destinationPlayer = GetLocalObject(sourcePlayer, "HOLOCOM_DESTINATION");
+
+            RemoveEffect(sourcePlayer, EffectCutsceneImmobilize());
+            RemoveEffect(destinationPlayer, EffectCutsceneImmobilize());
+
+            sourcePlayer.AssignCommand(() =>
+            {
+                PlaySound("hologram_off");
+            });
+            destinationPlayer.AssignCommand(() =>
+            {
+                PlaySound("hologram_off");
+            });
+            //AssignCommand(GetLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION"), () => PlaySound("hologram_off"));
+            //AssignCommand(GetLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION"), () => PlaySound("hologram_off"));
+
+            DestroyObject(GetLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION"));
+            DestroyObject(GetLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION"));
+
+            DeleteLocalObject(sourcePlayer, "HOLOCOM_DESTINATION");
+            DeleteLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION");
+            DeleteLocalInt(sourcePlayer, "HOLOCOM_ATTEMPT");
+            DeleteLocalInt(sourcePlayer, "HOLOCOM_CALL_CONNECTED");
+            DeleteLocalObject(destinationPlayer, "HOLOCOM_DESTINATION");
+            DeleteLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION");
+            DeleteLocalInt(destinationPlayer, "HOLOCOM_ATTEMPT");
+            DeleteLocalInt(destinationPlayer, "HOLOCOM_CALL_CONNECTED");
+        }
+
+        public bool RequiresTarget => false;
+        public string ValidateArguments(NWPlayer user, params string[] args)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ SWLOR.Game.Server/Service/HoloComService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SWLOR.Game.Server.GameObject;
+
+using NWN;
+using static NWN._;
+using SWLOR.Game.Server.Event.Module;
+using SWLOR.Game.Server.Messaging;
+
+
+namespace SWLOR.Game.Server.Service
+{
+    public static class HoloComService
+    {
+        public static void SubscribeEvents()
+        {
+            MessageHub.Instance.Subscribe<OnModuleChat>(message => OnModuleChat());
+        }
+
+        private static void OnModuleChat()
+        {
+            NWPlayer sender = (_.GetPCChatSpeaker());
+            if (sender.GetLocalInt("HOLOCOM_CALL_CONNECTED") != 1) return;
+
+            NWPlayer receiver = GetLocalObject(sender, "HOLOGRAM_DESTINATION");
+
+            string text = _.GetPCChatMessage().Trim();
+
+            if (text.StartsWith("/")) return;
+
+            receiver.AssignCommand(() =>
+            {
+                ActionPlayAnimation(ANIMATION_LOOPING_TALK_NORMAL);
+            });
+
+            receiver.AssignCommand(() =>
+            {
+                ActionSpeakString(text, TALKVOLUME_TALK);
+            });
+
+            //AssignCommand(receiver, () => ActionPlayAnimation(ANIMATION_LOOPING_TALK_NORMAL));
+            //AssignCommand(receiver, () => ActionSpeakString(text, TALKVOLUME_TALK));
+
+            // Might be nice to loop through nearby players that are in a holocom chat and send the string as background noise. 
+            // This could end up getting expensive though?
+        }
+
+    }
+}

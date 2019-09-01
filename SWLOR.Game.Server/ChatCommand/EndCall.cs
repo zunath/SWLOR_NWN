df56a30a6d88645aa8f5f5ee78e9098c2e751e38
givemedeath <benco.moore@gmail.com>
2019-09-01 19:05:43 -0400

    Refactor and implement HoloCom. Fixes from testing feedback. See top of HoloComService for details of outstanding issues.

--- SWLOR.Game.Server/ChatCommand/EndCall.cs
+++ SWLOR.Game.Server/ChatCommand/EndCall.cs
@@ -12,34 +12,7 @@ namespace SWLOR.Game.Server.ChatCommand
         // not showing in chat menu and not a valid command.
         public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
         {
-            NWPlayer sourcePlayer = user;
-            NWPlayer destinationPlayer = GetLocalObject(sourcePlayer, "HOLOCOM_DESTINATION");
-
-            RemoveEffect(sourcePlayer, EffectCutsceneImmobilize());
-            RemoveEffect(destinationPlayer, EffectCutsceneImmobilize());
-
-            sourcePlayer.AssignCommand(() =>
-            {
-                PlaySound("hologram_off");
-            });
-            destinationPlayer.AssignCommand(() =>
-            {
-                PlaySound("hologram_off");
-            });
-            //AssignCommand(GetLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION"), () => PlaySound("hologram_off"));
-            //AssignCommand(GetLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION"), () => PlaySound("hologram_off"));
-
-            DestroyObject(GetLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION"));
-            DestroyObject(GetLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION"));
-
-            DeleteLocalObject(sourcePlayer, "HOLOCOM_DESTINATION");
-            DeleteLocalObject(sourcePlayer, "HOLOGRAM_DESTINATION");
-            DeleteLocalInt(sourcePlayer, "HOLOCOM_ATTEMPT");
-            DeleteLocalInt(sourcePlayer, "HOLOCOM_CALL_CONNECTED");
-            DeleteLocalObject(destinationPlayer, "HOLOCOM_DESTINATION");
-            DeleteLocalObject(destinationPlayer, "HOLOGRAM_DESTINATION");
-            DeleteLocalInt(destinationPlayer, "HOLOCOM_ATTEMPT");
-            DeleteLocalInt(destinationPlayer, "HOLOCOM_CALL_CONNECTED");
+            HoloComService.SetIsInCall(user, HoloComService.GetTargetForActiveCall(user), false);
         }
 
         public bool RequiresTarget => false;

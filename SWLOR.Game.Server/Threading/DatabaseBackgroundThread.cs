d30801c7ab3cf243cca0bda5a9fabfa804c5a616
Zunath <coolty3001@yahoo.com>
2020-05-07 23:31:17 -0400

    Put background DB thread back in place.

--- SWLOR.Game.Server/Threading/DatabaseBackgroundThread.cs
+++ SWLOR.Game.Server/Threading/DatabaseBackgroundThread.cs
@@ -1,87 +1,85 @@
 ﻿
-//using SWLOR.Game.Server.Data;
-//using SWLOR.Game.Server.Enumeration;
-//using SWLOR.Game.Server.Service;
-//using SWLOR.Game.Server.ValueObject;
-//using System;
-//using System.Data.SqlClient;
-//using System.Threading;
-//using Microsoft.Data.SqlClient;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.Service;
+using SWLOR.Game.Server.ValueObject;
+using System;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
 
-//namespace SWLOR.Game.Server.Threading
-//{
-//    public class DatabaseBackgroundThread
-//    {
-//        private SqlConnection _connection;
-        
-//        public void Start()
-//        {
-//            _connection = new SqlConnection(DataService.SWLORConnectionString);
-//            _connection.Open();
-//        }
+namespace SWLOR.Game.Server.Threading
+{
+    public class DatabaseBackgroundThread
+    {
+        private MySqlConnection _connection;
 
-//        public void Stop()
-//        {
-//            ProcessQueue();
-//            _connection.Close();
-//        }
+        public void Start()
+        {
+            _connection = new MySqlConnection(DataService.SWLORConnectionString);
+            _connection.Open();
+        }
 
-//        private void ProcessQueue()
-//        {
-//            while (!DataService.DataQueue.IsEmpty)
-//            {
-//                if (!DataService.DataQueue.TryDequeue(out DatabaseAction request))
-//                {
-//                    Console.WriteLine("DATABASE WORKER: Was unable to process an object. Will try again...");
-//                    return;
-//                }
+        public void Stop()
+        {
+            ProcessQueue();
+            _connection.Close();
+        }
 
-//                try
-//                {
-//                    if (request.Action == DatabaseActionType.Insert)
-//                    {
-//                        foreach (var record in request.Data)
-//                        {
-//                            _connection.Insert(record.GetType(), record);
-//                        }
-//                    }
-//                    else if (request.Action == DatabaseActionType.Update)
-//                    {
-//                        foreach (var record in request.Data)
-//                        {
-//                            _connection.Update(record.GetType(), record);
-//                        }
-//                    }
-//                    else if (request.Action == DatabaseActionType.Delete)
-//                    {
-//                        foreach (var record in request.Data)
-//                        {
-//                            _connection.Delete(record.GetType(), record);
-//                        }
-//                    }
+        private void ProcessQueue()
+        {
+            while (!DataService.DataQueue.IsEmpty)
+            {
+                if (!DataService.DataQueue.TryDequeue(out DatabaseAction request))
+                {
+                    Console.WriteLine("DATABASE WORKER: Was unable to process an object. Will try again...");
+                    return;
+                }
 
-//                }
-//                catch (SqlException ex)
-//                {
-//                    Console.WriteLine("****EXCEPTION ON DATABASE BACKGROUND THREAD****");
-//                    Console.WriteLine("Data Type: " + request.DataType);
-//                    Console.WriteLine("Action: " + request.Action);
-//                    LoggingService.LogError(ex, request.Action.ToString());
-//                }
-//                catch (Exception ex)
-//                {
-//                    Console.WriteLine("****EXCEPTION ON DATABASE BACKGROUND THREAD****");
-//                    Console.WriteLine("Data Type: " + request.DataType);
-//                    Console.WriteLine("Action: " + request.Action);
-//                    LoggingService.LogError(ex, request.Action.ToString());
-//                }
-//            }
-//        }
+                try
+                {
+                    if (request.Action == DatabaseActionType.Insert)
+                    {
+                        foreach (var record in request.Data)
+                        {
+                            //_connection.Insert(record.GetType(), record); // todo fix
+                        }
+                    }
+                    else if (request.Action == DatabaseActionType.Update)
+                    {
+                        foreach (var record in request.Data)
+                        {
+                            //_connection.Update(record.GetType(), record); // todo fix
+                        }
+                    }
+                    else if (request.Action == DatabaseActionType.Delete)
+                    {
+                        foreach (var record in request.Data)
+                        {
+                            //_connection.Delete(record.GetType(), record); // todo fix
+                        }
+                    }
 
-//        public void Run()
-//        {
-//            ProcessQueue();
-//        }
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("****EXCEPTION ON DATABASE BACKGROUND THREAD****");
+                    Console.WriteLine("Data Type: " + request.DataType);
+                    Console.WriteLine("Action: " + request.Action);
+                    LoggingService.LogError(ex, request.Action.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("****EXCEPTION ON DATABASE BACKGROUND THREAD****");
+                    Console.WriteLine("Data Type: " + request.DataType);
+                    Console.WriteLine("Action: " + request.Action);
+                    LoggingService.LogError(ex, request.Action.ToString());
+                }
+            }
+        }
 
-//    }
-//}
+        public void Run()
+        {
+            ProcessQueue();
+        }
+
+    }
+}

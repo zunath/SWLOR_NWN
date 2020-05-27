59bd35f5469d8e561966c22e400c956a8aec556a
Zunath <coolty3001@yahoo.com>
2020-05-27 12:14:36 -0400

    Put some guards around the damage plugin's event data to ensure values don't fall outside acceptable ranges.

--- SWLOR.Game.Server/NWNX/DamageEventData.cs
+++ SWLOR.Game.Server/NWNX/DamageEventData.cs
@@ -5,19 +5,177 @@ namespace SWLOR.Game.Server.NWNX
     public class DamageEventData
     {
         public NWObject Damager { get; set; }
-        public int Bludgeoning { get; set; }
-        public int Pierce { get; set; }
-        public int Slash { get; set; }
-        public int Magical { get; set; }
-        public int Acid { get; set; }
-        public int Cold { get; set; }
-        public int Divine { get; set; }
-        public int Electrical { get; set; }
-        public int Fire { get; set; }
-        public int Negative { get; set; }
-        public int Positive { get; set; }
-        public int Sonic { get; set; }
-        public int Base { get; set; }
+        private int _bludgeoning;
+        public int Bludgeoning
+        {
+            get => _bludgeoning;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _bludgeoning = value;
+            }
+        }
+
+        private int _pierce;
+
+        public int Pierce
+        {
+            get => _pierce;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _pierce = value;
+            }
+        }
+
+        private int _slash;
+
+        public int Slash
+        {
+            get => _slash;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _slash = value;
+            }
+        }
+
+        private int _magical;
+        public int Magical
+        {
+            get => _magical;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _magical = value;
+            }
+        }
+
+        private int _acid;
+        public int Acid
+        {
+            get => _acid;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _acid = value;
+            }
+        }
+
+        private int _cold;
+        public int Cold
+        {
+            get => _cold;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _cold = value;
+            }
+        }
+
+        private int _divine;
+        public int Divine
+        {
+            get => _divine;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _divine = value;
+            }
+        }
+
+        private int _electrical;
+        public int Electrical
+        {
+            get => _electrical;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _electrical = value;
+            }
+        }
+
+        private int _fire;
+        public int Fire
+        {
+            get => _fire;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _fire = value;
+            }
+        }
+
+        private int _negative;
+        public int Negative
+        {
+            get => _negative;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _negative = value;
+            }
+        }
+
+        private int _positive;
+        public int Positive
+        {
+            get => _positive;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _positive = value;
+            }
+        }
+
+        private int _sonic;
+        public int Sonic
+        {
+            get => _sonic;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _sonic = value;
+            }
+        }
+
+        private int _base;
+        public int Base
+        {
+            get => _base;
+            set
+            {
+                if (value < -1)
+                    value = -1;
+
+                _base = value;
+            }
+        }
+
 
 
         public int Total => (Bludgeoning < 0 ? 0 : Bludgeoning) +

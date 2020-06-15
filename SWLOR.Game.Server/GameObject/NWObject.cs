9a156a5e40e77dc0f7f1bad5ebd2d5ee8fd2da5a
Z <coolty3001@yahoo.com>
2020-06-15 17:21:09 -0400

    Remove the PC/DM check on global IDs... duh. Also remove the exception that's unnecessary.

--- SWLOR.Game.Server/GameObject/NWObject.cs
+++ SWLOR.Game.Server/GameObject/NWObject.cs
@@ -41,11 +41,6 @@ namespace SWLOR.Game.Server.GameObject
 
         public virtual Guid GetOrAssignGlobalID()
         {
-            if (!GetIsPC(Object) || GetIsDM(Object)) return Guid.Empty;
-
-            if (!GetIsObjectValid(Object))
-                throw new Exception("NWN object has not been set for this wrapper.");
-
             string globalID;
             if (IsPlayer)
             {

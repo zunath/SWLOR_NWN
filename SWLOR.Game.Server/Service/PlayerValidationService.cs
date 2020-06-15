02897d2a598fec08f6d32a1c3289aab577fd27c1
Z <coolty3001@yahoo.com>
2020-06-14 22:56:21 -0400

    Temporarily disable the character deletion process when a character with bad attributes enters the server.

--- SWLOR.Game.Server/Service/PlayerValidationService.cs
+++ SWLOR.Game.Server/Service/PlayerValidationService.cs
@@ -22,7 +22,8 @@ namespace SWLOR.Game.Server.Service
             
             if (!string.IsNullOrWhiteSpace(error))
             {
-                NWNXAdmin.DeletePlayerCharacter(player, true);
+                // todo: Temporarily removed until NWNX gets fixed.
+                //NWNXAdmin.DeletePlayerCharacter(player, true);
             }
         }
 

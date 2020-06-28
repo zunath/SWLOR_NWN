a19bb39e674c037112a19ccac97f2c1f810c2fbd
givemedeath <benco.moore@gmail.com>
2020-06-28 19:16:05 -0400

    Add DM PlayVFX chat command. Give access to entire visualeffects.2da if the effect can be played at a location. Fix Dual Blaster counting as Throwing.

---
+++ SWLOR.Game.Server/ChatCommand/PlayVFX.cs
@@ -0,0 +1,44 @@
+﻿using SWLOR.Game.Server.ChatCommand.Contracts;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.GameObject;
+using System;
+using SWLOR.Game.Server.NWN;
+using SWLOR.Game.Server.NWN.Enum;
+using SWLOR.Game.Server.NWN.Enum.VisualEffect;
+using static SWLOR.Game.Server.NWN._;
+using SWLOR.Game.Server.Service;
+
+namespace SWLOR.Game.Server.ChatCommand
+{
+    [CommandDetails("Switches the active language. Use /language help for more information.", CommandPermissionType.DM | CommandPermissionType.Admin)]
+    public class PlayVFX : IChatCommand
+    {
+        public void DoAction(NWPlayer user, NWObject target, NWLocation targetLocation, params string[] args)
+        {
+            string command = args[0].ToLower();
+            Effect effect = EffectVisualEffect((VisualEffect)Int32.Parse(args[0]));
+            _.ApplyEffectAtLocation(DurationType.Instant, effect, targetLocation, 6.0f);
+        }
+
+        public string ValidateArguments(NWPlayer user, params string[] args)
+        {
+            if (args.Length < 1)
+            {
+                return ColorTokenService.Red("Enter the ID from visualeffects.2da");
+            }
+
+            try
+            {
+                int result = Int32.Parse(args[0]);
+            }
+            catch (FormatException)
+            {
+                return ColorTokenService.Red("Enter the ID from visualeffects.2da");
+            }
+
+            return string.Empty;
+        }
+
+        public bool RequiresTarget => true;
+    }
+}

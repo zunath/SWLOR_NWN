c58227d5ab999656e228ec7476d64554faad2d31
taelon875 <kivash0081@yahoo.com>
2020-10-28 15:23:48 -0400

    Balanced After Some Testing
    My Buffed Cell Changes were a little high.... Think this is a little more balanced.
    
    After Testing Yeah Premonition was really favorable... Lowered the Value Drastically but added a Concealment effect to the second and a higher concealement to the third.

---
+++ SWLOR.Game.Server/Data/Migrations/2020-10-28.1.sql
@@ -0,0 +1,6 @@
+﻿Update PerkLevel Set Description = 'The caster gains 3 Temporary Hit Points Per Tick' Where ID = 2281;
+Update PerkLevel Set Description = 'The caster gains 3 Temporary Hit Points and 5% Concealment Per Tick' Where ID = 2282;
+Update PerkLevel Set Description = 'The caster gains 5 Temporary Hit Points and 10% Concealment Per Tick' Where ID = 2283;
+Update PerkFeat Set ConcentrationFPCost = 3 Where ID = 97;
+Update PerkFeat Set ConcentrationFPCost = 4 Where ID = 98;
+Update PerkFeat Set ConcentrationFPCost = 5 Where ID = 99;

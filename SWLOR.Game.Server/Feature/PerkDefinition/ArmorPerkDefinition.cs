36cc2c78a6584cf2275f39a387e9cf6a4cd7b339
Z <coolty3001@yahoo.com>
2020-11-07 14:09:18 -0500

    Force and Armor configs

--- SWLOR.Game.Server/Feature/PerkDefinition/ArmorPerkDefinition.cs
+++ SWLOR.Game.Server/Feature/PerkDefinition/ArmorPerkDefinition.cs
@@ -9,8 +9,429 @@ namespace SWLOR.Game.Server.Feature.PerkDefinition
         public Dictionary<PerkType, PerkDetail> BuildPerks()
         {
             var builder = new PerkBuilder();
+            Cloaks(builder);
+            Belts(builder);
+            Rings(builder);
+            Necklaces(builder);
+            Breastplates(builder);
+            Helmets(builder);
+            Bracers(builder);
+            Leggings(builder);
+            HeavyShields(builder);
+            Tunics(builder);
+            Caps(builder);
+            Gloves(builder);
+            Boots(builder);
+            LightShields(builder);
 
             return builder.Build();
+        }
+
+        private void Cloaks(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorGeneral, PerkType.Cloaks)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Cloaks")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Cloaks")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Cloaks")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Cloaks")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Cloaks")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+
+        }
+
+        private void Belts(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorGeneral, PerkType.Belts)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Belts")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Belts")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Belts")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Belts")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Belts")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Rings(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorGeneral, PerkType.Rings)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Rings")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Rings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Rings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Rings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Rings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Necklaces(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorGeneral, PerkType.Necklaces)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Necklaces")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Necklaces")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Necklaces")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Necklaces")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Necklaces")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Breastplates(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorHeavy, PerkType.Breastplates)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Breastplates")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Breastplates")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Breastplates")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Breastplates")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Breastplates")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Helmets(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorHeavy, PerkType.Helmets)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Helmets")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Helmets")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Helmets")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Helmets")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Helmets")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Bracers(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorHeavy, PerkType.Bracers)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Bracers")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Bracers")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Bracers")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Bracers")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Bracers")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Leggings(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorHeavy, PerkType.Leggings)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Leggings")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Leggings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Leggings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Leggings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Leggings")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void HeavyShields(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorHeavy, PerkType.HeavyShields)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Heavy Shields")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Heavy Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Heavy Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Heavy Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Heavy Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Tunics(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorLight, PerkType.Tunics)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Tunics")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Tunics")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Tunics")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Tunics")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Tunics")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Caps(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorLight, PerkType.Caps)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Caps")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Caps")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Caps")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Caps")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Caps")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Gloves(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorLight, PerkType.Gloves)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Gloves")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Gloves")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Gloves")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Gloves")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Gloves")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void Boots(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorLight, PerkType.Boots)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Boots")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Boots")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Boots")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Boots")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Boots")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
+        }
+
+        private void LightShields(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ArmorLight, PerkType.LightShields)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 1 Light Shields")
+                .Price(2)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 2 Light Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 10)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 3 Light Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 20)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 4 Light Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 30)
+
+                .AddPerkLevel()
+                .Description("Grants the ability to equip tier 5 Light Shields")
+                .Price(2)
+                .RequirementSkill(SkillType.Armor, 40);
         }
 
     }
--- SWLOR.Game.Server/Feature/PerkDefinition/ForcePerkDefinition.cs
+++ SWLOR.Game.Server/Feature/PerkDefinition/ForcePerkDefinition.cs
@@ -10,7 +10,207 @@ namespace SWLOR.Game.Server.Feature.PerkDefinition
         {
             var builder = new PerkBuilder();
 
+            ForcePush(builder);
+            BurstOfSpeed(builder);
+            ThrowLightsaber(builder);
+            ForceStun(builder);
+            ComprehendSpeech(builder);
+            BattleInsight(builder);
+            MindTrick(builder);
+
             return builder.Build();
+        }
+
+        private void ForcePush(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.ForcePush)
+                .Name("Force Push")
+
+                .AddPerkLevel()
+                .Description("Knockdown a small target. If resisted, target is slowed for 6 seconds.")
+                .Price(2)
+                .RequirementSkill(SkillType.Force, 5)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Knockdown a medium or smaller target. If resisted, target is slowed for 6 seconds.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 10)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Knockdown a large or smaller target. If resisted, target is slowed for 6 seconds.")
+                .Price(4)
+                .RequirementSkill(SkillType.Force, 20)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Knockdown any size target. If resisted, target is slowed for 6 seconds.")
+                .Price(5)
+                .RequirementSkill(SkillType.Force, 30)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void BurstOfSpeed(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.BurstOfSpeed)
+                .Name("Burst of Speed")
+
+                .AddPerkLevel()
+                .Description("Increases your speed by 20% while concentrating.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 5)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Increases your speed by 30% while concentrating.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 15)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Increases your speed by 40% while concentrating.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 25)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Increases your speed by 50% while concentrating.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 35)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Increases your speed by 60% while concentrating.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 45)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void ThrowLightsaber(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.ThrowLightsaber)
+                .Name("Throw Lightsaber")
+
+                .AddPerkLevel()
+                .Description("Throw your equipped lightsaber up to 15m for (saber damage + INT modifier) * 100%")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 10)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Throw your equipped lightsaber up to 15m for (saber damage + INT modifier) * 125%")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 20)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Throw your equipped lightsaber up to 15m for (saber damage + INT modifier) * 160%")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 30)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Throw your equipped lightsaber up to 15m for (saber damage +INT modifier) *200 %. This will chain to a second target within 5m of the first.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 40)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Throw your equipped lightsaber up to 15m for (saber damage +INT modifier) *250 %. This will chain to a second and third target within 5m of the first.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 50)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void ForceStun(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.ForceStun)
+                .Name("Force Stun")
+
+                .AddPerkLevel()
+                .Description("Single target is Tranquilized while the caster concentrates or, if resisted, gets -5 to AB and AC.")
+                .Price(4)
+                .RequirementSkill(SkillType.Force, 10)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Target and nearest other enemy within 10m is Tranquilized while the caster concentrates or, if resisted, get -5 to AB and AC.")
+                .Price(7)
+                .RequirementSkill(SkillType.Force, 25)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Target and all other enemies within 10 are Tranquilized while the caster concentrates or, if resisted, get -5 to AB and AC.")
+                .Price(10)
+                .RequirementSkill(SkillType.Force, 40)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void ComprehendSpeech(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.ComprehendSpeech)
+                .Name("Comprehend Speech")
+
+                .AddPerkLevel()
+                .Description("The caster counts has having 5 extra ranks in all languages for the purpose of understanding others speaking, so long as they concentrate.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 5)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("The caster counts has having 10 extra ranks in all languages for the purpose of understanding others speaking, so long as they concentrate.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 15)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("The caster counts has having 15 extra ranks in all languages for the purpose of understanding others speaking, so long as they concentrate.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 25)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("The caster counts has having 20 extra ranks in all languages for the purpose of understanding others speaking, so long as they concentrate.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 35)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void BattleInsight(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.BattleInsight)
+                .Name("Battle Insight")
+
+                .AddPerkLevel()
+                .Description("The caster gets -5 AB & AC but nearby party members get +3 AB & AC.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 20)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("The caster gets -8 AB & AC but nearby party members get +6 AB & AC.")
+                .Price(3)
+                .RequirementSkill(SkillType.Force, 40)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
+        }
+
+        private void MindTrick(PerkBuilder builder)
+        {
+            builder.Create(PerkCategoryType.ForceUniversal, PerkType.MindTrick)
+                .Name("Mind Trick")
+
+                .AddPerkLevel()
+                .Description("Applies Confusion effect to a single non-mechanical target with lower WIS than the caster, while the caster concentrates.")
+                .Price(7)
+                .RequirementSkill(SkillType.Force, 20)
+                .RequirementCharacterType(CharacterType.ForceSensitive)
+
+                .AddPerkLevel()
+                .Description("Applies Confusion effect to all hostile non-mechanical targets within 10m with lower WIS than the caster, while the caster concentrates.")
+                .Price(7)
+                .RequirementSkill(SkillType.Force, 40)
+                .RequirementCharacterType(CharacterType.ForceSensitive);
         }
 
     }

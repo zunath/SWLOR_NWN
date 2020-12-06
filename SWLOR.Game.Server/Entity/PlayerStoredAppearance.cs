d13eac22aea60ad2942c0ffc5a2c0252d2bb209a
Z <coolty3001@yahoo.com>
2020-12-05 22:21:39 -0500

    Outfit saving/loading/deleting

---
+++ SWLOR.Game.Server/Entity/PlayerStoredAppearance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SWLOR.Game.Server.Entity
+{
+    public class PlayerStoredAppearance : EntityBase
+    {
+        public PlayerStoredAppearance()
+        {
+            SavedOutfits = new Dictionary<int, string>();
+        }
+
+        public override string KeyPrefix => "PlayerStoredAppearance";
+
+        public Dictionary<int, string> SavedOutfits { get; set; }
+    }
+}

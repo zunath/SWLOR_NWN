04ddd3fd58de4cb9e3160bd0744981be57a4ff5a
taelon875 <kivash0081@yahoo.com>
2020-12-06 19:02:27 -0500

    More Force Stuff
    Fixed Rock Damage and Removed Specs.

--- SWLOR.Game.Server/Data/Migrations/2020-12-02.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2020-12-02.1.sql
@@ -28,6 +28,24 @@ Update PerkFeat Set ConcentrationFPCost = 5 WHERE PerkID = 181 AND FeatID = 1210
 Update PerkFeat Set ConcentrationFPCost = 6 WHERE PerkID = 181 AND FeatID = 1211;
 Update PerkFeat Set ConcentrationFPCost = 7 WHERE PerkID = 181 AND FeatID = 1212;
 
-Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 41
-Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 42
-Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 43
+Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 41;
+Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 42;
+Update CooldownCategory Set BaseCooldownTime = 14 WHERE ID  = 43;
+
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 3;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 13;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 19;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 174;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 181;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 182;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 184;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 185;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 4;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 76;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 126;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 173;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 183;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 175;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 176;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 177;
+Update PerkLevel Set SpecializationID = 0  WHERE PerkID = 180;

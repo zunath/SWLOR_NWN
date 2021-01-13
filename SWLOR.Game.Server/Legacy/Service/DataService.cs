315c4d7e8b33a66f6bbb07fd5509fbc35f4db834
zunath <coolty3001@yahoo.com>
2021-01-12 22:01:53 -0500

    Kill legacy caches

--- SWLOR.Game.Server/Legacy/Service/DataService.cs
+++ SWLOR.Game.Server/Legacy/Service/DataService.cs
@@ -20,86 +20,22 @@ namespace SWLOR.Game.Server.Legacy.Service
         public static string SWLORConnectionString { get; }
         public static MySqlConnection Connection { get; private set; }
 
-        public static ApartmentBuildingCache ApartmentBuilding { get; } = new ApartmentBuildingCache();
         public static AreaCache Area { get; } = new AreaCache();
-        public static AssociationCache Association { get; } = new AssociationCache();
-        public static AttributeCache Attribute { get; } = new AttributeCache();
-        public static AuthorizedDMCache AuthorizedDM { get; } = new AuthorizedDMCache();
-        public static BankCache Bank { get; } = new BankCache();
-        public static BankItemCache BankItem { get; } = new BankItemCache();
-        public static BaseItemTypeCache BaseItemType { get; } = new BaseItemTypeCache();
-        public static BaseStructureCache BaseStructure { get; } = new BaseStructureCache();
-        public static BaseStructureTypeCache BaseStructureType { get; } = new BaseStructureTypeCache();
-        public static BugReportCache BugReport { get; } = new BugReportCache();
-        public static ChatChannelCache ChatChannel { get; } = new ChatChannelCache();
-        public static ChatLogCache ChatLog { get; } = new ChatLogCache();
-        public static ClientLogEventCache ClientLogEvent { get; } = new ClientLogEventCache();
-        public static ClientLogEventTypeCache ClientLogEventType { get; } = new ClientLogEventTypeCache();
-        public static ComponentTypeCache ComponentType { get; } = new ComponentTypeCache();
-        public static CooldownCategoryCache CooldownCategory { get; } = new CooldownCategoryCache();
-        public static CraftBlueprintCache CraftBlueprint { get; } = new CraftBlueprintCache();
-        public static CraftBlueprintCategoryCache CraftBlueprintCategory { get; } = new CraftBlueprintCategoryCache();
-        public static CraftDeviceCache CraftDevice { get; } = new CraftDeviceCache();
-        public static CustomEffectCache CustomEffect { get; } = new CustomEffectCache();
-        public static CustomEffectCategoryCache CustomEffectCategory { get; } = new CustomEffectCategoryCache();
-        public static DatabaseVersionCache DatabaseVersion { get; } = new DatabaseVersionCache();
-        public static DMActionCache DMAction { get; } = new DMActionCache();
-        public static DMActionTypeCache DMActionType { get; } = new DMActionTypeCache();
-        public static DMRoleCache DMRole { get; } = new DMRoleCache();
-        public static DownloadCache Download { get; } = new DownloadCache();
-        public static EnmityAdjustmentRuleCache EnmityAdjustmentRule { get; } = new EnmityAdjustmentRuleCache();
-        public static ErrorCache Error { get; } = new ErrorCache();
-        public static FameRegionCache FameRegion { get; } = new FameRegionCache();
-        public static GameTopicCache GameTopic { get; } = new GameTopicCache();
-        public static GameTopicCategoryCache GameTopicCategory { get; } = new GameTopicCategoryCache();
-        public static GuildCache Guild { get; } = new GuildCache();
-        public static GuildTaskCache GuildTask { get; } = new GuildTaskCache();
-        public static ItemTypeCache ItemType { get; } = new ItemTypeCache();
-        public static JukeboxSongCache JukeboxSong { get; } = new JukeboxSongCache();
-        public static KeyItemCache KeyItem { get; } = new KeyItemCache();
-        public static KeyItemCategoryCache KeyItemCategory { get; } = new KeyItemCategoryCache();
         public static LootTableCache LootTable { get; } = new LootTableCache();
-        public static LootTableItemCache LootTableItem { get; } = new LootTableItemCache();
-        public static MarketCategoryCache MarketCategory { get; } = new MarketCategoryCache();
         public static MessageCache Message { get; } = new MessageCache();
-        public static NPCGroupCache NPCGroup { get; } = new NPCGroupCache();
-        public static PCCooldownCache PCCooldown { get; } = new PCCooldownCache();
-        public static PCCraftedBlueprintCache PCCraftedBlueprint { get; } = new PCCraftedBlueprintCache();
-        public static PCCustomEffectCache PCCustomEffect { get; } = new PCCustomEffectCache();
-        public static PCGuildPointCache PCGuildPoint { get; } = new PCGuildPointCache();
-        public static PCKeyItemCache PCKeyItem { get; } = new PCKeyItemCache();
-        public static PCMapPinCache PCMapPin { get; } = new PCMapPinCache();
-        public static PCMapProgressionCache PCMapProgression { get; } = new PCMapProgressionCache();
-        public static PCMarketListingCache PCMarketListing { get; } = new PCMarketListingCache();
-        public static PCObjectVisibilityCache PCObjectVisibility { get; } = new PCObjectVisibilityCache();
-        public static PCOutfitCache PCOutfit { get; } = new PCOutfitCache();
-        public static PCHelmetCache PCHelmet { get; } = new PCHelmetCache();
-        public static PCWeaponCache PCWeapon { get; } = new PCWeaponCache();
-        public static PCOverflowItemCache PCOverflowItem { get; } = new PCOverflowItemCache();
         public static PCPerkCache PCPerk { get; } = new PCPerkCache();
-        public static PCPerkRefundCache PCPerkRefund { get; } = new PCPerkRefundCache();
-        public static PCQuestItemProgressCache PCQuestItemProgress { get; } = new PCQuestItemProgressCache();
-        public static PCQuestKillTargetProgressCache PCQuestKillTargetProgress { get; } = new PCQuestKillTargetProgressCache();
         public static PCQuestStatusCache PCQuestStatus { get; } = new PCQuestStatusCache();
-        public static PCRegionalFameCache PCRegionalFame { get; } = new PCRegionalFameCache();
         public static PCSkillCache PCSkill { get; } = new PCSkillCache();
         public static PCSkillPoolCache PCSkillPool { get; } = new PCSkillPoolCache();
         public static PerkCache Perk { get; } = new PerkCache();
-        public static PerkCategoryCache PerkCategory { get; } = new PerkCategoryCache();
         public static PerkFeatCache PerkFeat { get; } = new PerkFeatCache();
         public static PerkLevelCache PerkLevel { get; } = new PerkLevelCache();
         public static PerkLevelQuestRequirementCache PerkLevelQuestRequirement { get; } = new PerkLevelQuestRequirementCache();
         public static PerkLevelSkillRequirementCache PerkLevelSkillRequirement { get; } = new PerkLevelSkillRequirementCache();
         public static PlayerCache Player { get; } = new PlayerCache();
-        public static ServerConfigurationCache ServerConfiguration { get; } = new ServerConfigurationCache();
         public static SkillCache Skill { get; } = new SkillCache();
         public static SkillCategoryCache SkillCategory { get; } = new SkillCategoryCache();
-        public static SpaceEncounterCache SpaceEncounter { get; } = new SpaceEncounterCache();
-        public static StarportCache Starport { get; } = new StarportCache();
-        public static SpawnCache Spawn { get; } = new SpawnCache();
         public static SpawnObjectCache SpawnObject { get; } = new SpawnObjectCache();
-        public static SpawnObjectTypeCache SpawnObjectType { get; } = new SpawnObjectTypeCache();
-        public static StructureModeCache StructureMode { get; } = new StructureModeCache();
 
 
         static DataService()
@@ -128,9 +64,6 @@ namespace SWLOR.Game.Server.Legacy.Service
         public static void Initialize(bool initializeCache)
         {
             Connection = new MySqlConnection(SWLORConnectionString);
-
-            if (initializeCache)
-                InitializeCache();
         }
 
         private static void LoadCache<T>()
@@ -161,100 +94,7 @@ namespace SWLOR.Game.Server.Legacy.Service
             MessageHub.Instance.Publish(new OnCacheObjectDeleted<T>(entity));
         }
 
-
-        /// <summary>
-        /// Retrieves all objects in frequently accessed data from the database and stores them into the cache.
-        /// This should only be called one time at initial load.
-        /// </summary>
-        private static void InitializeCache()
-        {
-            Console.WriteLine("Initializing the cache...");
-            LoadCache<Area>();
-
-            LoadCache<ApartmentBuilding>();
-            LoadCache<Association>();
-            LoadCache<Data.Entity.Attribute>();
-            LoadCache<AuthorizedDM>();
-            LoadCache<Bank>();
-            LoadCache<BankItem>();
-            LoadCache<BaseItemType>();
-            LoadCache<BaseStructure>();
-            LoadCache<Data.Entity.BaseStructureType>();
-            LoadCache<BuildingStyle>();
-            LoadCache<Data.Entity.BuildingType>();
-            LoadCache<ChatChannel>();
-            LoadCache<ModuleEventType>();
-            LoadCache<Data.Entity.ComponentType>();
-            LoadCache<CooldownCategory>();
-            LoadCache<CraftBlueprint>();
-            LoadCache<CraftBlueprintCategory>();
-            LoadCache<CraftDevice>();
-            LoadCache<Data.Entity.CustomEffect>();
-            LoadCache<CustomEffectCategory>();
-            LoadCache<DMRole>();
-            LoadCache<EnmityAdjustmentRule>();
-            LoadCache<FameRegion>();
-            LoadCache<Guild>();
-            LoadCache<GuildTask>();
-            LoadCache<ItemType>();
-            LoadCache<JukeboxSong>();
-            LoadCache<KeyItem>();
-            LoadCache<KeyItemCategory>();
-            LoadCache<LootTable>();
-            LoadCache<LootTableItem>();
-            LoadCache<MarketCategory>();
-            LoadCache<Message>();
-            LoadCache<NPCGroup>();
-            LoadCache<PCBase>();
-            LoadCache<PCBasePermission>();
-            LoadCache<PCBaseStructure>();
-            LoadCache<PCBaseStructureItem>();
-            LoadCache<PCBaseStructurePermission>();
-            LoadCache<Data.Entity.PCBaseType>();
-            LoadPCMarketListingCache();
-            LoadCache<Starport>();
-            LoadCache<SpaceEncounter>();
-
-
-            LoadCache<PCCooldown>();
-            LoadCache<PCCraftedBlueprint>();
-            LoadCache<PCCustomEffect>();
-
-            LoadCache<PCGuildPoint>();
-            LoadCache<PCKeyItem>();
-            LoadCache<PCMapPin>();
-            LoadCache<PCMapProgression>();
-            LoadCache<PCObjectVisibility>();
-            LoadCache<PCOutfit>();
-            LoadCache<PCHelmet>();
-            LoadCache<PCWeapon>();
-            LoadCache<PCOverflowItem>();
-            LoadCache<PCPerk>();
-            LoadCache<PCQuestItemProgress>();
-            LoadCache<PCQuestKillTargetProgress>();
-            LoadCache<PCQuestStatus>();
-            LoadCache<PCRegionalFame>();
-            LoadCache<PCSkill>();
-            LoadCache<PCSkillPool>();
-            LoadCache<PCPerkRefund>();
-
-            LoadCache<Data.Entity.Perk>();
-            LoadCache<PerkFeat>();
-            LoadCache<PerkCategory>();
-            LoadCache<PerkLevel>();
-            LoadCache<PerkLevelQuestRequirement>();
-            LoadCache<PerkLevelSkillRequirement>();
-            LoadCache<Player>();
-            LoadCache<ServerConfiguration>();
-            LoadCache<Skill>();
-            LoadCache<SkillCategory>();
-            LoadCache<Spawn>();
-            LoadCache<SpawnObject>();
-            LoadCache<SpawnObjectType>();
-            LoadCache<StructureMode>();
-            Console.WriteLine("Cache initialized!");
-        }
-
+        
         /// <summary>
         /// PC Market Listings should only be loaded into the cache if they:
         /// 1.) Haven't been sold already

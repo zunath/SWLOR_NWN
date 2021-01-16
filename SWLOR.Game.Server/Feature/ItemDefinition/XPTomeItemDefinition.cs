eb595c7790b38f6387340af59824dcf2ba0cf7e0
zunath <coolty3001@yahoo.com>
2021-01-15 23:37:37 -0500

    Perk refund item.

--- SWLOR.Game.Server/Feature/ItemDefinition/XPTomeItemDefinition.cs
+++ SWLOR.Game.Server/Feature/ItemDefinition/XPTomeItemDefinition.cs
@@ -10,18 +10,36 @@ namespace SWLOR.Game.Server.Feature.ItemDefinition
     {
         public Dictionary<string, ItemDetail> BuildItems()
         {
-            var builder = new ItemBuilder()
-                .Create("xp_tome_1", "xp_tome_2", "xp_tome_3", "xp_tome_4")
+            var builder = new ItemBuilder();
+            XPTomes(builder);
+            PerkRefundTome(builder);
+
+            return builder.Build();
+        }
+
+        private static void XPTomes(ItemBuilder builder)
+        {
+            builder.Create("xp_tome_1", "xp_tome_2", "xp_tome_3", "xp_tome_4")
                 .ApplyAction((user, item, target, location) =>
                 {
                     SetLocalObject(user, "XP_TOME_OBJECT", item);
                     AssignCommand(user, () => ClearAllActions());
-                    
+
                     Dialog.StartConversation(user, user, nameof(XPTomeDialog));
                 });
-
-
-            return builder.Build();
         }
+
+        private static void PerkRefundTome(ItemBuilder builder)
+        {
+            builder.Create("refund_tome")
+                .ApplyAction((user, item, target, location) =>
+                {
+                    SetLocalObject(user, "PERK_REFUND_OBJECT", item);
+                    AssignCommand(user, () => ClearAllActions());
+
+                    Dialog.StartConversation(user, user, nameof(PerkRefundDialog));
+                });
+        }
+        
     }
 }

7c25da6292784c7c1061de8447fe1ce1eaaf825c
zunath <coolty3001@yahoo.com>
2021-01-24 20:56:04 -0500

    Add action-transport snippet.

---
+++ SWLOR.Game.Server/Feature/SnippetDefinition/TransportationSnippetDefinition.cs
@@ -0,0 +1,39 @@
+﻿using SWLOR.Game.Server.Service;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+
+namespace SWLOR.Game.Server.Feature.SnippetDefinition
+{
+    public class TransportationSnippetDefinition
+    {
+        /// <summary>
+        /// Snippet which teleports a player to the waypoint with the specified tag.
+        /// </summary>
+        /// <param name="player">The player to teleport</param>
+        /// <param name="args">Arguments provided by the conversation builder.</param>
+        [Snippet("action-teleport")]
+        public static void ActionTeleport(uint player, string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                const string Error = "'action-teleport' requires a waypoint tag argument.";
+                SendMessageToPC(player, Error);
+                Log.Write(LogGroup.Error, Error);
+                return;
+            }
+
+            var waypointTag = args[0];
+            var waypoint = GetWaypointByTag(waypointTag);
+
+            if (!GetIsObjectValid(waypoint))
+            {
+                var error = $"Could not locate waypoint with tag '{waypointTag}' for snippet 'action-teleport'";
+                SendMessageToPC(player, error);
+                Log.Write(LogGroup.Error, error);
+                return;
+            }
+
+            var location = GetLocation(waypoint);
+            AssignCommand(player, () => ActionJumpToLocation(location));
+        }
+    }
+}

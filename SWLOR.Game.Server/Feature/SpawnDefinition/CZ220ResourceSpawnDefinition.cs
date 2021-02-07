ec562f50fa10e98abfbc7928e62b48e50b0eca9f
zunath <coolty3001@yahoo.com>
2021-02-07 12:57:40 -0500

    Update hand-placed spawn points for new system. Add spawn tables for search sites.

--- SWLOR.Game.Server/Feature/SpawnDefinition/CZ220ResourceSpawnDefinition.cs
+++ SWLOR.Game.Server/Feature/SpawnDefinition/CZ220ResourceSpawnDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Service.SpawnService;
 
 namespace SWLOR.Game.Server.Feature.SpawnDefinition
@@ -8,9 +9,30 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
         public Dictionary<string, SpawnTable> BuildSpawnTables()
         {
             var builder = new SpawnTableBuilder();
-
+            JunkPiles(builder);
+            Caches(builder);
+            OreVeins(builder);
 
             return builder.Build();
         }
+
+        private void JunkPiles(SpawnTableBuilder builder)
+        {
+            builder.Create("CZ220_JUNKPILES")
+                .AddSpawn(ObjectType.Placeable, "cz220_junk");
+        }
+
+        private void Caches(SpawnTableBuilder builder)
+        {
+            builder.Create("CZ220_CACHES")
+                .AddSpawn(ObjectType.Placeable, "cz220_cache");
+        }
+
+        private void OreVeins(SpawnTableBuilder builder)
+        {
+            builder.Create("CZ220_VELDITE")
+                .AddSpawn(ObjectType.Placeable, "veldite_vein");
+        }
+
     }
 }
--- SWLOR.Game.Server/Feature/SpawnDefinition/ViscaraResourceSpawnDefinition.cs
+++ SWLOR.Game.Server/Feature/SpawnDefinition/ViscaraResourceSpawnDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Service.SpawnService;
 
 namespace SWLOR.Game.Server.Feature.SpawnDefinition
@@ -8,9 +9,15 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
         public Dictionary<string, SpawnTable> BuildSpawnTables()
         {
             var builder = new SpawnTableBuilder();
-
+            CoxxionBase(builder);
 
             return builder.Build();
+        }
+
+        private void CoxxionBase(SpawnTableBuilder builder)
+        {
+            builder.Create("COXXION_BASE_SCAVENGE")
+                .AddSpawn(ObjectType.Placeable, "v_cox_scav");
         }
     }
 }

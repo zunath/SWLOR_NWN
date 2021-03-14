746363ee7f64684d54920296bca072abfeb0f8c2
zunath <coolty3001@yahoo.com>
2021-03-14 11:08:44 -0400

    Simplify mod item definition for both weapons and armor.

--- SWLOR.Game.Server/Feature/ItemDefinition/ModItemDefinition.cs
+++ SWLOR.Game.Server/Feature/ItemDefinition/ModItemDefinition.cs
@@ -9,18 +9,18 @@ namespace SWLOR.Game.Server.Feature.ItemDefinition
 {
     public class ModItemDefinition: IItemListDefinition
     {
+        private readonly ItemBuilder _builder = new ItemBuilder();
+
         public Dictionary<string, ItemDetail> BuildItems()
         {
-            var builder = new ItemBuilder();
-            WeaponMod(builder);
-            ArmorMod(builder);
-
-            return builder.Build();
+            CreateModItem("MOD_WEAPON", "weapons", ItemPropertyType.WeaponSocket, Item.WeaponBaseItemTypes);
+            CreateModItem("MOD_ARMOR", "armor", ItemPropertyType.ArmorSocket, Item.ArmorBaseItemTypes);
+            return _builder.Build();
         }
 
-        private void WeaponMod(ItemBuilder builder)
+        private void CreateModItem(string key, string itemClassification, ItemPropertyType propertyType, List<BaseItem> validItemTypes)
         {
-            builder.Create("MOD_WEAPON")
+            _builder.Create(key)
                 .Delay(6.0f)
                 .MaxDistance(0.0f)
                 .PlaysAnimation(Animation.LoopingGetMid)
@@ -44,22 +44,22 @@ namespace SWLOR.Game.Server.Feature.ItemDefinition
 
                     // Ensure target is an item.
                     if (GetObjectType(target) != ObjectType.Item)
-                        return "Only weapons may be targeted.";
+                        return "Only items may be targeted.";
 
-                    // Ensure target is a weapon.
+                    // Ensure target is a valid type.
                     var itemType = GetBaseItemType(target);
-                    if (!Item.WeaponBaseItemTypes.Contains(itemType))
-                        return "Only weapons may be targeted.";
+                    if (!validItemTypes.Contains(itemType))
+                        return $"Only {itemClassification} may be targeted.";
 
-                    // Look for a Weapon Socket property. If one is found, this is a valid target.
+                    // Look for a Socket property. If one is found, this is a valid target.
                     for (var ip = GetFirstItemProperty(target); GetIsItemPropertyValid(ip); ip = GetNextItemProperty(target))
                     {
-                        if (GetItemPropertyType(ip) == ItemPropertyType.WeaponSocket)
+                        if (GetItemPropertyType(ip) == propertyType)
                             return string.Empty;
                     }
 
                     // Couldn't find any open sockets. Return an error message.
-                    return "No open sockets could be found on the targeted weapon.";
+                    return "No open sockets could be found on the targeted item.";
                 })
                 .ApplyAction((user, item, target, location) =>
                 {
@@ -69,7 +69,7 @@ namespace SWLOR.Game.Server.Feature.ItemDefinition
                     // Remove the empty socket property.
                     for (var ip = GetFirstItemProperty(target); GetIsItemPropertyValid(ip); ip = GetNextItemProperty(target))
                     {
-                        if (GetItemPropertyType(ip) == ItemPropertyType.WeaponSocket)
+                        if (GetItemPropertyType(ip) == propertyType)
                         {
                             RemoveItemProperty(target, ip);
                             break;
@@ -78,66 +78,7 @@ namespace SWLOR.Game.Server.Feature.ItemDefinition
 
                     mod.ApplyItemModAction?.Invoke(user, item, target);
                 });
-        }
 
-        private void ArmorMod(ItemBuilder builder)
-        {
-            builder.Create("MOD_ARMOR")
-                .Delay(6.0f)
-                .MaxDistance(0.0f)
-                .ValidationAction((user, item, target, location) =>
-                {
-                    // Ensure the mod type is set on the item.
-                    var modType = GetLocalString(item, "MOD_TYPE");
-                    if (string.IsNullOrWhiteSpace(modType))
-                    {
-                        Log.Write(LogGroup.Error, $"Mod type is unspecified on item '{GetName(item)}'. Set the local variable 'MOD_TYPE' on the item in the toolset.");
-                        return "Mod is not registered in the system. Notify an admin this item is bugged.";
-                    }
-
-                    // Ensure the mod type is registered in code.
-                    if (!ItemMod.IsRegistered(modType))
-                    {
-                        Log.Write(LogGroup.Error, $"Mod type '{modType}' is not programmed. Add it to the ModItemDefinition.cs class.");
-                        return "Mod is not registered in the system. Notify an admin this mod type is bugged.";
-                    }
-
-                    // Ensure target is an item.
-                    if (GetObjectType(target) != ObjectType.Item)
-                        return "Only armor may be targeted.";
-
-                    // Ensure target is an armor.
-                    var itemType = GetBaseItemType(target);
-                    if (!Item.ArmorBaseItemTypes.Contains(itemType))
-                        return "Only armor may be targeted.";
-
-                    // Look for an Armor Socket property. If one is found, this is a valid target.
-                    for (var ip = GetFirstItemProperty(target); GetIsItemPropertyValid(ip); ip = GetNextItemProperty(target))
-                    {
-                        if (GetItemPropertyType(ip) == ItemPropertyType.ArmorSocket)
-                            return string.Empty;
-                    }
-
-                    // Couldn't find any open sockets. Return an error message.
-                    return "No open sockets could be found on the targeted armor.";
-                })
-                .ApplyAction((user, item, target, location) =>
-                {
-                    var modType = GetLocalString(item, "MOD_TYPE");
-                    var mod = ItemMod.GetByKey(modType);
-
-                    // Remove the empty socket property.
-                    for (var ip = GetFirstItemProperty(target); GetIsItemPropertyValid(ip); ip = GetNextItemProperty(target))
-                    {
-                        if (GetItemPropertyType(ip) == ItemPropertyType.ArmorSocket)
-                        {
-                            RemoveItemProperty(target, ip);
-                            break;
-                        }
-                    }
-
-                    mod.ApplyItemModAction?.Invoke(user, item, target);
-                });
         }
     }
 }

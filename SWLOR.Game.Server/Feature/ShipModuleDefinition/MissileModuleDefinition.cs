7401fd167a4ab2d5603c94a2c483886646820452
zunath <coolty3001@yahoo.com>
2021-03-24 21:46:00 -0400

    Missile launcher modules

--- SWLOR.Game.Server/Feature/ShipModuleDefinition/MissileModuleDefinition.cs
+++ SWLOR.Game.Server/Feature/ShipModuleDefinition/MissileModuleDefinition.cs
@@ -1,5 +1,11 @@
 ﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Core.NWScript.Enum.VisualEffect;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.SpaceService;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+using Random = SWLOR.Game.Server.Service.Random;
 
 namespace SWLOR.Game.Server.Feature.ShipModuleDefinition
 {
@@ -9,8 +15,108 @@ namespace SWLOR.Game.Server.Feature.ShipModuleDefinition
 
         public Dictionary<string, ShipModuleDetail> BuildShipModules()
         {
+            MissileLauncher("msl_launch_b", "Basic Missile Launcher", "B. Msl Launch", "Deals explosive damage to your target and nearby ships.", 1, 8f, 10, 7);
+            MissileLauncher("msl_launch_1", "Missile Launcher I", "Msl Launch I", "Deals explosive damage to your target and nearby ships.", 2, 11f, 14, 10);
+            MissileLauncher("msl_launch_2", "Missile Launcher II", "Msl Launch II", "Deals explosive damage to your target and nearby ships.", 3, 14f, 18, 13);
+            MissileLauncher("msl_launch_3", "Missile Launcher III", "Msl Launch III", "Deals explosive damage to your target and nearby ships.", 4, 17f, 22, 16);
+            MissileLauncher("msl_launch_4", "Missile Launcher IV", "Msl Launch IV", "Deals explosive damage to your target and nearby ships.", 5, 20f, 26, 19);
 
             return _builder.Build();
+        }
+
+        private void MissileLauncher(string itemTag, string name, string shortName, string description, int requiredLevel, float recast, int capacitor, int baseDamage)
+        {
+            _builder.Create(itemTag)
+                .Name(name)
+                .ShortName(shortName)
+                .Description(description)
+                .IsActiveModule()
+                .PowerType(ShipModulePowerType.High)
+                .RequirePerk(PerkType.OffensiveModules, requiredLevel)
+                .Recast(recast)
+                .Capacitor(capacitor)
+                .ActivatedAction((activator, target) =>
+                {
+                    var chanceToHit = Space.CalculateChanceToHit(activator.Creature, target.Creature);
+                    var roll = Random.D100(1);
+                    var isHit = roll <= chanceToHit;
+                    var targetDistance = GetDistanceBetween(activator.Creature, target.Creature);
+                    var targetLocation = GetLocation(target.Creature);
+                    var targetDefense = target.ExplosiveDefense;
+                    var attackerDamage = baseDamage + activator.ExplosiveDamage;
+                    var damage = attackerDamage - targetDefense;
+                    if (damage < 0) damage = 0;
+                    var delay = (float)(targetDistance / (3.0 * log(targetDistance) + 2.0));
+
+                    // Shoot some missiles out to the target.
+                    var isHitCopy = isHit;
+                    AssignCommand(activator.Creature, () =>
+                    {
+                        ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffect.Vfx_Imp_Mirv, !isHitCopy), target.Creature);
+                    });
+                    
+                    // Display an explosion at the target location in a few seconds (based on travel distance of the initial missile graphic)
+                    // Then apply damage on target and those nearby.
+                    DelayCommand(delay, () =>
+                    {
+                        ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffect.Fnf_Fireball, !isHitCopy), target.Creature);
+
+                        if (isHit)
+                        {
+                            Space.ApplyShipDamage(activator.Creature, target.Creature, damage);
+                        }
+                        else
+                        {
+                            Messaging.SendMessageNearbyToPlayers(activator.Creature, $"{GetName(activator.Creature)} misses their intended target.");
+                        }
+
+                        // Iterate over nearby targets, rolling to apply damage to each.
+                        var nearby = GetFirstObjectInShape(Shape.Sphere, 3f, targetLocation, false, ObjectType.Creature);
+                        while (GetIsObjectValid(nearby))
+                        {
+                            // Picked up the same creature as our initial target. Move to the next one since we've already processed it.
+                            if (nearby == target.Creature)
+                            {
+                                nearby = GetNextObjectInShape(Shape.Sphere, 3f, targetLocation, false, ObjectType.Creature);
+                                continue;
+                            }
+
+                            // Only targets within 5 meters may be hit by this missile.
+                            var nearbyLocation = GetLocation(nearby);
+                            if (GetDistanceBetweenLocations(targetLocation, nearbyLocation) > 5f) break;
+
+                            var shipTarget = Space.GetShipStatus(nearby);
+                            if (!GetIsObjectValid(shipTarget.Creature))
+                            {
+                                nearby = GetNextObjectInShape(Shape.Sphere, 3f, targetLocation, false, ObjectType.Creature);
+                                continue;
+                            }
+
+                            if (shipTarget.Shield <= 0 && shipTarget.Hull <= 0)
+                            {
+                                nearby = GetNextObjectInShape(Shape.Sphere, 3f, targetLocation, false, ObjectType.Creature);
+                                continue;
+                            }
+
+                            targetDefense = shipTarget.ExplosiveDefense;
+                            attackerDamage = baseDamage + activator.ExplosiveDamage;
+
+                            damage = attackerDamage - targetDefense;
+                            if (damage < 0) damage = 0;
+                            chanceToHit = Space.CalculateChanceToHit(activator.Creature, shipTarget.Creature);
+                            roll = Random.D100(1);
+                            isHit = roll <= chanceToHit;
+
+                            if (isHit)
+                            {
+                                Space.ApplyShipDamage(activator.Creature, shipTarget.Creature, damage / 2);
+                            }
+
+                            nearby = GetNextObjectInShape(Shape.Sphere, 3f, targetLocation, false, ObjectType.Creature);
+                        }
+                    });
+
+                });
         }
     }
 }

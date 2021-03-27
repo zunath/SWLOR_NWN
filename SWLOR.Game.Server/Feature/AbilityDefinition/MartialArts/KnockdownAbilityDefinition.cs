cfade9fe0e4cf669afe74b0fccffe238c70420b9
zunath <coolty3001@yahoo.com>
2021-03-27 15:59:19 -0400

    Add knockdown ability.

---
+++ SWLOR.Game.Server/Feature/AbilityDefinition/MartialArts/KnockdownAbilityDefinition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.Service;
+using SWLOR.Game.Server.Service.AbilityService;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+using Random = SWLOR.Game.Server.Service.Random;
+
+namespace SWLOR.Game.Server.Feature.AbilityDefinition.MartialArts
+{
+    public class KnockdownAbilityDefinition: IAbilityListDefinition
+    {
+        private readonly AbilityBuilder _builder = new AbilityBuilder();
+
+        public Dictionary<Feat, AbilityDetail> BuildAbilities()
+        {
+            Knockdown();
+
+            return _builder.Build();
+        }
+
+        private void Knockdown()
+        {
+            _builder.Create(Feat.Knockdown, PerkType.Knockdown)
+                .Name("Knockdown")
+                .HasRecastDelay(RecastGroup.Knockdown, 60f)
+                .IsWeaponAbility()
+                .RequirementStamina(6)
+                .HasImpactAction((activator, target, level) =>
+                {
+                    var isHit = Random.D100(1) <= 60;
+                    if (!isHit) return;
+
+                    ApplyEffectToObject(DurationType.Temporary, EffectKnockdown(), target, 12f);
+                });
+        }
+    }
+}

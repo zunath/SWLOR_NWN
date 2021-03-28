8eba6fc9f6ff778605d146381bc114c6447d0149
zunath <coolty3001@yahoo.com>
2021-03-27 23:54:32 -0400

    Fix standard / force player status window

--- SWLOR.Game.Server/Feature/PlayerStatusWindow.cs
+++ SWLOR.Game.Server/Feature/PlayerStatusWindow.cs
@@ -44,7 +44,20 @@ namespace SWLOR.Game.Server.Feature
             // Character UI elements
             else
             {
-                DrawCharacterStatusComponent(player);
+                var playerId = GetObjectUUID(player);
+                var dbPlayer = DB.Get<Player>(playerId);
+                var isStandard = dbPlayer.CharacterType == CharacterType.Standard;
+
+                // Standard Characters
+                if (isStandard)
+                {
+                    DrawStandardCharacterStatusComponent(player);
+                }
+                // Force Characters
+                else
+                {
+                    DrawForceCharacterStatusComponent(player);
+                }
             }
         }
 
@@ -52,7 +65,54 @@ namespace SWLOR.Game.Server.Feature
         /// Draws the HP, FP, and STM status information on the player's screen.
         /// </summary>
         /// <param name="player">The player to draw the component for.</param>
-        private static void DrawCharacterStatusComponent(uint player)
+        private static void DrawStandardCharacterStatusComponent(uint player)
+        {
+            var playerId = GetObjectUUID(player);
+            var dbPlayer = DB.Get<Player>(playerId) ?? new Player();
+
+            var currentHP = GetCurrentHitPoints(player);
+            var maxHP = GetMaxHitPoints(player);
+            var currentSTM = dbPlayer.Stamina;
+            var maxSTM = Stat.GetMaxStamina(player, dbPlayer);
+
+            var backgroundBar = BuildBar(1, 1, 22);
+            var hpBar = BuildBar(currentHP, maxHP, 22);
+            var stmBar = BuildBar(currentSTM, maxSTM, 22);
+
+            const int WindowX = 1;
+            const int WindowY = 5;
+            const int WindowWidth = 25;
+            const ScreenAnchor Anchor = ScreenAnchor.BottomRight;
+
+            // Draw order is backwards. The top-most layer needs to be drawn first.
+            var centerWindowX = Gui.CenterStringInWindow(backgroundBar, WindowX, WindowWidth);
+
+            // Draw the text
+            var hpText = "HP:".PadRight(5, ' ') + $"{currentHP.ToString().PadLeft(4, ' ')} / {maxHP.ToString().PadLeft(4, ' ')}";
+            var stmText = "STM:".PadRight(5, ' ') + $"{currentSTM.ToString().PadLeft(4, ' ')} / {maxSTM.ToString().PadLeft(4, ' ')}";
+
+            PostString(player, hpText, centerWindowX + 8, WindowY + 2, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId + 2, Gui.TextName);
+            PostString(player, stmText, centerWindowX + 8, WindowY + 1, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId, Gui.TextName);
+
+            // Draw the bars
+            PostString(player, hpBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorHealthBar, Gui.ColorHealthBar, _characterIdReservation.StartId + 3, Gui.FontName);
+            PostString(player, stmBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorStaminaBar, Gui.ColorStaminaBar, _characterIdReservation.StartId + 5, Gui.FontName);
+
+            // Draw the backgrounds
+            if (!GetLocalBool(player, "PLAYERSTATUSWINDOW_BACKGROUND_DRAWN"))
+            {
+                PostString(player, backgroundBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 6, Gui.FontName);
+                PostString(player, backgroundBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 8, Gui.FontName);
+
+                Gui.DrawWindow(player, _characterIdReservation.StartId + 9, Anchor, WindowX, WindowY, WindowWidth - 2, 2);
+            }
+        }
+
+        /// <summary>
+        /// Draws the HP, FP, and STM status information on a force player's screen.
+        /// </summary>
+        /// <param name="player">The player to draw the component for.</param>
+        private static void DrawForceCharacterStatusComponent(uint player)
         {
             var playerId = GetObjectUUID(player);
             var dbPlayer = DB.Get<Player>(playerId) ?? new Player();
@@ -68,7 +128,6 @@ namespace SWLOR.Game.Server.Feature
             var hpBar = BuildBar(currentHP, maxHP, 22);
             var fpBar = BuildBar(currentFP, maxFP, 22);
             var stmBar = BuildBar(currentSTM, maxSTM, 22);
-            var isStandard = dbPlayer.CharacterType == CharacterType.Standard;
 
             const int WindowX = 1;
             const int WindowY = 5;
@@ -84,44 +143,20 @@ namespace SWLOR.Game.Server.Feature
             var stmText = "STM:".PadRight(5, ' ') + $"{currentSTM.ToString().PadLeft(4, ' ')} / {maxSTM.ToString().PadLeft(4, ' ')}";
 
             PostString(player, hpText, centerWindowX + 8, WindowY + 3, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId + 2, Gui.TextName);
-
-            if (isStandard)
-            {
-                PostString(player, stmText, centerWindowX + 8, WindowY + 2, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId, Gui.TextName);
-            }
-            else
-            {
-                PostString(player, fpText, centerWindowX + 8, WindowY + 2, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId + 1, Gui.TextName);
-                PostString(player, stmText, centerWindowX + 8, WindowY + 1, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId, Gui.TextName);
-            }
+            PostString(player, fpText, centerWindowX + 8, WindowY + 2, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId + 1, Gui.TextName);
+            PostString(player, stmText, centerWindowX + 8, WindowY + 1, Anchor, 0.0f, Gui.ColorWhite, Gui.ColorWhite, _characterIdReservation.StartId, Gui.TextName);
 
             // Draw the bars
             PostString(player, hpBar, centerWindowX + 2, WindowY + 3, Anchor, 0.0f, Gui.ColorHealthBar, Gui.ColorHealthBar, _characterIdReservation.StartId + 3, Gui.FontName);
-
-            if (isStandard)
-            {
-                PostString(player, stmBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorStaminaBar, Gui.ColorStaminaBar, _characterIdReservation.StartId + 5, Gui.FontName);
-            }
-            else
-            {
-                PostString(player, fpBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorManaBar, Gui.ColorManaBar, _characterIdReservation.StartId + 4, Gui.FontName);
-                PostString(player, stmBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorStaminaBar, Gui.ColorStaminaBar, _characterIdReservation.StartId + 5, Gui.FontName);
-            }
+            PostString(player, fpBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorManaBar, Gui.ColorManaBar, _characterIdReservation.StartId + 4, Gui.FontName);
+            PostString(player, stmBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorStaminaBar, Gui.ColorStaminaBar, _characterIdReservation.StartId + 5, Gui.FontName);
 
             // Draw the backgrounds
             if (!GetLocalBool(player, "PLAYERSTATUSWINDOW_BACKGROUND_DRAWN"))
             {
                 PostString(player, backgroundBar, centerWindowX + 2, WindowY + 3, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 6, Gui.FontName);
-
-                if (isStandard)
-                {
-                    PostString(player, backgroundBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 7, Gui.FontName);
-                }
-                else
-                {
-                    PostString(player, backgroundBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 7, Gui.FontName);
-                    PostString(player, backgroundBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 8, Gui.FontName);
-                }
+                PostString(player, backgroundBar, centerWindowX + 2, WindowY + 2, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 7, Gui.FontName);
+                PostString(player, backgroundBar, centerWindowX + 2, WindowY + 1, Anchor, 0.0f, Gui.ColorBlack, Gui.ColorBlack, _characterIdReservation.StartId + 8, Gui.FontName);
                 
                 Gui.DrawWindow(player, _characterIdReservation.StartId + 9, Anchor, WindowX, WindowY, WindowWidth - 2, 3);
             }

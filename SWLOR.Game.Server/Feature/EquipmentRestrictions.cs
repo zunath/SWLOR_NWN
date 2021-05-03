623b02d2130c3450b3a9bbd0e6a14a4c1f23c69b
Scott Milliorn <scottmilliorn@gmail.com>
2021-05-03 15:48:13 -0700

    OnEquipNoGearChange

--- SWLOR.Game.Server/Feature/EquipmentRestrictions.cs
+++ SWLOR.Game.Server/Feature/EquipmentRestrictions.cs
@@ -54,6 +54,16 @@ namespace SWLOR.Game.Server.Feature
             Events.SkipEvent();
         }
 
+        [NWNEventHandler("item_eqpval_bef")]
+        public static void OnEquipNoGearChange()
+        {
+            if (GetIsInCombat(OBJECT_SELF) && GetIsPC(OBJECT_SELF))
+            {
+                Events.SkipEvent();
+                SendMessageToPC(OBJECT_SELF, "You may not equip an item while in combat.");
+            }
+        }
+
         /// <summary>
         /// When an item is equipped, check if the item is going to be dual wielded. If it is, ensure player has
         /// at least level 1 of the Dual Wield perk. If they don't, skip the equip event with an error message.

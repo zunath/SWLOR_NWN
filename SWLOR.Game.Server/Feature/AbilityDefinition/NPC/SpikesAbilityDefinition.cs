267c822b74396b452aff16ea0e0072704c3a0c31
zunath <none>
2021-08-29 12:30:56 -0400

    Add Spikes, Talon, and Venom NPC abilities.

--- SWLOR.Game.Server/Feature/AbilityDefinition/NPC/SpikesAbilityDefinition.cs
+++ SWLOR.Game.Server/Feature/AbilityDefinition/NPC/SpikesAbilityDefinition.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Core.NWScript.Enum.VisualEffect;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.AbilityService;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
 
 namespace SWLOR.Game.Server.Feature.AbilityDefinition.NPC
 {
@@ -10,11 +14,32 @@ namespace SWLOR.Game.Server.Feature.AbilityDefinition.NPC
 
         public Dictionary<FeatType, AbilityDetail> BuildAbilities()
         {
+            Spikes();
 
             return _builder.Build();
         }
 
+        private void Spikes()
+        {
+            _builder.Create(FeatType.Spikes, PerkType.Invalid)
+                .Name("Spikes")
+                .HasActivationDelay(3.5f)
+                .HasRecastDelay(RecastGroup.Spikes, 20f)
+                .IsCastedAbility()
+                .RequirementStamina(8)
+                .HasImpactAction((activator, target, level, location) =>
+                {
+                    var might = GetAbilityModifier(AbilityType.Might, activator);
+                    var dmg = 2.5f;
+                    var defense = Combat.CalculateDefense(target);
+                    var vitality = GetAbilityModifier(AbilityType.Vitality, target);
+                    var damage = Combat.CalculateDamage(dmg, might, defense, vitality, false);
 
+                    ApplyEffectToObject(DurationType.Instant, EffectDamage(damage, DamageType.Piercing), target);
+                    ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffect.Vfx_Imp_Wallspike), target);
+                    StatusEffect.Apply(activator, target, StatusEffectType.Bleed, 45f);
+                });
+        }
 
     }
 }
--- SWLOR.Game.Server/Feature/AbilityDefinition/NPC/TalonAbilityDefinition.cs
+++ SWLOR.Game.Server/Feature/AbilityDefinition/NPC/TalonAbilityDefinition.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Core.NWScript.Enum.VisualEffect;
+using SWLOR.Game.Server.Enumeration;
+using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.AbilityService;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
 
 namespace SWLOR.Game.Server.Feature.AbilityDefinition.NPC
 {
@@ -10,8 +14,30 @@ namespace SWLOR.Game.Server.Feature.AbilityDefinition.NPC
 
         public Dictionary<FeatType, AbilityDetail> BuildAbilities()
         {
+            Talon();
 
             return _builder.Build();
+        }
+
+        private void Talon()
+        {
+            _builder.Create(FeatType.Talon, PerkType.Invalid)
+                .Name("Talon")
+                .HasActivationDelay(2.0f)
+                .HasRecastDelay(RecastGroup.Talon, 40f)
+                .IsCastedAbility()
+                .RequirementStamina(3)
+                .HasImpactAction((activator, target, level, location) =>
+                {
+                    var might = GetAbilityModifier(AbilityType.Might, activator);
+                    var dmg = 1.0f;
+                    var defense = Combat.CalculateDefense(target);
+                    var vitality = GetAbilityModifier(AbilityType.Vitality, target);
+                    var damage = Combat.CalculateDamage(dmg, might, defense, vitality, false);
+
+                    ApplyEffectToObject(DurationType.Instant, EffectDamage(damage, DamageType.Piercing), target);
+                    ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffect.Vfx_Com_Blood_Spark_Medium), target);
+                });
         }
     }
 }

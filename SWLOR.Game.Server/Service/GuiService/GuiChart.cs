ec19e5cb8bf80d932428c31150cfea763aecad02
zunath <none>
2021-09-18 13:18:31 -0400

    Gui chart + slots

---
+++ SWLOR.Game.Server/Service/GuiService/GuiChart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.Beamdog;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+
+namespace SWLOR.Game.Server.Service.GuiService
+{
+    public class GuiChart: GuiWidget
+    {
+        public List<GuiChartSlot> Slots { get; set; }
+
+        public GuiChart()
+        {
+            Slots = new List<GuiChartSlot>();
+        }
+
+        public override Json BuildElement()
+        {
+            var slots = JsonArray();
+
+            foreach (var slot in Slots)
+            {
+                slots = JsonArrayInsert(slots, slot.ToJson());
+            }
+
+            return Nui.Chart(slots);
+        }
+    }
+}
---
+++ SWLOR.Game.Server/Service/GuiService/GuiChartSlot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.Beamdog;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+
+namespace SWLOR.Game.Server.Service.GuiService
+{
+    public class GuiChartSlot
+    {
+        public NuiChartType Type { get; set; }
+
+        public string Legend { get; set; }
+        public string LegendBindName { get; set; }
+        public bool IsLegendBound => !string.IsNullOrWhiteSpace(LegendBindName);
+
+        public GuiColor Color { get; set; }
+        public string ColorBindName { get; set; }
+        public bool IsColorBound => !string.IsNullOrWhiteSpace(ColorBindName);
+
+        public List<float> Data { get; set; }
+        public string DataBindName { get; set; }
+        public bool IsDataBound => !string.IsNullOrWhiteSpace(DataBindName);
+
+        public GuiChartSlot()
+        {
+            Data = new List<float>();
+        }
+
+        public Json ToJson()
+        {
+            var legend = IsLegendBound ? Nui.Bind(LegendBindName) : JsonString(Legend);
+            var color = IsColorBound ? Nui.Bind(ColorBindName) : Color.ToJson();
+            var data = JsonArray();
+
+            if (IsDataBound)
+            {
+                data = Nui.Bind(DataBindName);
+            }
+            else
+            {
+                foreach (var d in Data)
+                {
+                    data = JsonArrayInsert(data, JsonFloat(d));
+                }
+            }
+
+            return Nui.ChartSlot(Type, legend, color, data);
+        }
+    }
+}

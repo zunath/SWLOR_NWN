824188aaeb94f179b22593b2eb0753511697484b
zunath <none>
2021-09-18 12:20:46 -0400

    Combo box, group, and lists

---
+++ SWLOR.Game.Server/Service/GuiService/GuiComboBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.Beamdog;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+
+namespace SWLOR.Game.Server.Service.GuiService
+{
+    public class GuiComboBox: GuiWidget
+    {
+        public List<GuiComboEntry> Options { get; set; }
+        public string OptionsBindName { get; set; }
+        public bool IsOptionsBound => !string.IsNullOrWhiteSpace(OptionsBindName);
+
+        public int SelectedIndex { get; set; }
+        public string SelectedIndexBindName { get; set; }
+        public bool IsSelectedIndexBound => !string.IsNullOrWhiteSpace(SelectedIndexBindName);
+
+        public GuiComboBox()
+        {
+            Options = new List<GuiComboEntry>();
+        }
+
+        public override Json BuildElement()
+        {
+            var selectedIndex = IsSelectedIndexBound ? Nui.Bind(SelectedIndexBindName) : JsonInt(SelectedIndex);
+            var options = JsonArray();
+
+            foreach (var option in Options)
+            {
+                options = JsonArrayInsert(options, option.ToJson());
+            }
+
+            return Nui.Combo(options, selectedIndex);
+        }
+    }
+}
---
+++ SWLOR.Game.Server/Service/GuiService/GuiComboEntry.cs
@@ -0,0 +1,16 @@
+﻿using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.Beamdog;
+
+namespace SWLOR.Game.Server.Service.GuiService
+{
+    public class GuiComboEntry: GuiWidget
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+
+        public override Json BuildElement()
+        {
+            return Nui.ComboEntry(Label, Value);
+        }
+    }
+}

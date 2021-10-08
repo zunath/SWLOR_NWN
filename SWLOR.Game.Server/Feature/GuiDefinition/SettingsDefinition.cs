bfc9cf7e63485810cc829a9039014033cf72c8ee
zunath <none>
2021-10-07 20:42:15 -0400

    Player settings window

---
+++ SWLOR.Game.Server/Feature/GuiDefinition/SettingsDefinition.cs
@@ -0,0 +1,51 @@
+﻿using SWLOR.Game.Server.Feature.GuiDefinition.ViewModel;
+using SWLOR.Game.Server.Service.GuiService;
+
+namespace SWLOR.Game.Server.Feature.GuiDefinition
+{
+    public class SettingsDefinition: IGuiWindowDefinition
+    {
+        private readonly GuiWindowBuilder<SettingsViewModel> _builder = new();
+
+        public GuiConstructedWindow BuildWindow()
+        {
+            _builder.CreateWindow(GuiWindowType.Settings)
+                .BindOnOpened(model => model.OnLoadWindow())
+                .SetIsResizable(true)
+                .SetInitialGeometry(0, 0, 545f, 295.5f)
+                .SetTitle("Settings")
+                
+                .AddColumn(col =>
+                {
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddCheckBox()
+                            .SetText("Show Achievements")
+                            .SetTooltip("Shows or hides achievement notification window. You will still continue to acquire achievements even if this setting is disabled.")
+                            .BindIsChecked(model => model.DisplayAchievementNotification);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddButton()
+                            .SetText("Save")
+                            .BindOnClicked(model => model.OnSave());
+
+                        row.AddButton()
+                            .SetText("Cancel")
+                            .BindOnClicked(model => model.OnSave());
+
+                        row.AddSpacer();
+                    });
+                });
+
+            return _builder.Build();
+        }
+    }
+}
---
+++ SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using SWLOR.Game.Server.Entity;
+using SWLOR.Game.Server.Service;
+using SWLOR.Game.Server.Service.GuiService;
+using static SWLOR.Game.Server.Core.NWScript.NWScript;
+
+namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
+{
+    public class SettingsViewModel: GuiViewModelBase<SettingsViewModel>
+    {
+        public bool DisplayAchievementNotification
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public Action OnLoadWindow() => () =>
+        {
+            var playerId = GetObjectUUID(Player);
+            var dbPlayer = DB.Get<Player>(playerId);
+
+            DisplayAchievementNotification = dbPlayer.Settings.DisplayAchievementNotification;
+            WatchOnClient(model => model.DisplayAchievementNotification);
+        };
+
+        public Action OnSave() => () =>
+        {
+            var playerId = GetObjectUUID(Player);
+            var dbPlayer = DB.Get<Player>(playerId);
+
+            dbPlayer.Settings.DisplayAchievementNotification = DisplayAchievementNotification;
+            DB.Set(playerId, dbPlayer);
+
+            Gui.TogglePlayerWindow(Player, GuiWindowType.Settings);
+        };
+
+        public Action OnCancel() => () =>
+        {
+            Gui.TogglePlayerWindow(Player, GuiWindowType.Settings);
+        };
+
+    }
+}

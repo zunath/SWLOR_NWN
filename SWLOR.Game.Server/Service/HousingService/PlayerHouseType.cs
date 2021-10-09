5cb31f9f6bbbbfac8c141e04e0eece747c8da4a7
zunath <none>
2021-10-09 11:31:25 -0400

    Hook up apartment types.

--- SWLOR.Game.Server/Service/HousingService/PlayerHouseType.cs
+++ SWLOR.Game.Server/Service/HousingService/PlayerHouseType.cs
@@ -4,16 +4,26 @@ namespace SWLOR.Game.Server.Service.HousingService
 {
     public enum PlayerHouseType
     {
-        [PlayerHouse("Invalid", 0, 100, 0, "", false)]
+        [PlayerHouse("Invalid", 0, 999999,  "", false)]
         Invalid = 0,
-        [PlayerHouse("Basic - Wooden/White (4x4)", 30, 1, 5000, "player_layout_1", true)]
-        Basic = 1,
-        [PlayerHouse("Advanced - Wooden/White (6x6)", 50, 3, 15000, "player_layout_2", true)]
-        Advanced = 2,
-        [PlayerHouse("Superior - Wooden/White (8x8)", 70, 6, 30000, "player_layout_3", true)]
-        Superior = 3,
-        [PlayerHouse("Master - Wooden/White (10x10)", 100, 10, 50000, "player_layout_4", true)]
-        Master = 4
+        [PlayerHouse("Small Apartment - Style 1", 30, 5000, "apartment_002", true)]
+        SmallStyle1 = 1,
+        [PlayerHouse("Small Apartment - Style 2", 30, 5000, "playerap_s_unf", true)]
+        SmallStyle2 = 2,
+        [PlayerHouse("Small Apartment - Style 2 (Furnished)", 30, 7000, "playerap_s_fur", true)]
+        SmallStyle2Furnished = 3,
+        [PlayerHouse("Medium Apartment - Style 1", 50, 10000, "apartment_2", true)]
+        MediumStyle1 = 4,
+        [PlayerHouse("Medium Apartment - Style 2", 50, 10000, "playerap_m_unf", true)]
+        MediumStyle2 = 5,
+        [PlayerHouse("Medium Apartment - Style 2 (Furnished)", 50, 13000, "playerap_m_fur", true)]
+        MediumStyle2Furnished = 6,
+        [PlayerHouse("Large Apartment - Style 1", 70, 25000, "apartment_3", true)]
+        LargeStyle1 = 7,
+        [PlayerHouse("Large Apartment - Style 2", 70, 25000, "playerap_l_unf", true)]
+        LargeStyle2 = 8,
+        [PlayerHouse("Large Apartment - Style 2 (Furnished)", 70, 30000, "playerap_l_fur", true)]
+        LargeStyle2Furnished = 9,
     }
 
     public class PlayerHouseAttribute : Attribute
@@ -21,21 +31,18 @@ namespace SWLOR.Game.Server.Service.HousingService
         public string Name { get; set; }
         public int FurnitureLimit { get; set; }
         public bool IsActive { get; set; }
-        public int RequiredSeedRank { get; set; }
         public int Price { get; set; }
         public string AreaInstanceResref { get; set; }
 
         public PlayerHouseAttribute(
             string name, 
             int furnitureLimit, 
-            int requiredSeedRank,
             int price,
             string areaInstanceResref,
             bool isActive)
         {
             Name = name;
             FurnitureLimit = furnitureLimit;
-            RequiredSeedRank = requiredSeedRank;
             Price = price;
             AreaInstanceResref = areaInstanceResref;
             IsActive = isActive;

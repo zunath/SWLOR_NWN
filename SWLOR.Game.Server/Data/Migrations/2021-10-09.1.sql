d938dee9f4a1194571e18e4262e2446744397524
Taelon875 <kivash0081@yahoo.com>
2021-10-15 21:38:51 -0400

    Its only logical!
    There i even did the work!

--- SWLOR.Game.Server/Data/Migrations/2021-10-09.1.sql
+++ SWLOR.Game.Server/Data/Migrations/2021-10-09.1.sql
@@ -3,16 +3,16 @@
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
 values (24, 'Small Apartment Style 2', 'playerap_s_unf', null, 0, '', 1, 3, 1000, 100, 30);
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
-values (25, 'Small Apartment Style 2 - Furnished', 'playerap_s_fur', null, 0, '', 1, 3, 1500, 150, 30);
+values (25, 'Small Apartment Style 2 - Furnished', 'playerap_s_fur', null, 0, '', 1, 3, 1500, 150, 15);
 
 -- Medium
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
 values (26, 'Medium Apartment Style 2', 'playerap_m_unf', null, 0, '', 1, 3, 1500, 200, 40);
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
-values (27, 'Medium Apartment Style 2 - Furnished', 'playerap_m_fur', null, 0, '', 1, 3, 2000, 250, 40);
+values (27, 'Medium Apartment Style 2 - Furnished', 'playerap_m_fur', null, 0, '', 1, 3, 2000, 250, 20);
 
 -- Large
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
 values (28, 'Large Apartment Style 2', 'playerap_l_unf', null, 0, '', 1, 3, 2000, 300, 50);
 insert into BuildingStyle(ID, Name, Resref, BaseStructureID, IsDefault, DoorRule, IsActive, BuildingTypeID, PurchasePrice, DailyUpkeep, FurnitureLimit)
-values (29, 'Large Apartment Style 2 - Furnished', 'playerap_l_fur', null, 0, '', 1, 3, 2500, 350, 50);
+values (29, 'Large Apartment Style 2 - Furnished', 'playerap_l_fur', null, 0, '', 1, 3, 2500, 350, 25);

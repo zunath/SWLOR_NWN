770629afc8c5e64b20aa72937c1dbb22631c7df3
Cameron Valdez <cameronav23@gmail.com>
2021-10-27 12:42:17 -0500

    Move Speeder functionality to Item Def
    Moved speeder functionality to an item definition. All speeder logic is handled in SpeederItemDefinition.

--- SWLOR.Game.Server/Feature/ItemDefinition/SpeederItemDefinition.cs
+++ SWLOR.Game.Server/Feature/ItemDefinition/SpeederItemDefinition.cs
@@ -1,52 +1,57 @@
-﻿using System;
-using SWLOR.Game.Server.Core;
+﻿using System.Collections.Generic;
 using SWLOR.Game.Server.Core.NWNX;
 using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Service.ItemService;
+using SWLOR.Game.Server.Core;
 using static SWLOR.Game.Server.Core.NWScript.NWScript;
 
-
-namespace SWLOR.Game.Server.Feature
+namespace SWLOR.Game.Server.Feature.ItemDefinition
 {
-    public class HandleSpeeder
+    public class SpeederItemDefinition: IItemListDefinition
     {
+        private readonly ItemBuilder _builder = new ItemBuilder();
+
+        public Dictionary<string, ItemDetail> BuildItems()
+        {
+            Speeder();
+            return _builder.Build();
+        }
         /// <summary>
-        /// Before an item is used, check if the area is an interior and return if it is. 
         /// Check player's pheno: 
         /// Pheno = normal: change tail to speederbike, set pheno to speederbike and movement rate to DMfast.
         /// Pheno = SpeederBike: change tail to none, set pheno and speed to normal.
         /// </summary>
-        [NWNEventHandler("item_use_bef")]
-        public static void SpeederMount()
+        private void Speeder()
         {
-            var player = OBJECT_SELF;
-            var area = GetArea(player);
-            var item = StringToObject(EventsPlugin.GetEventData("ITEM_OBJECT_ID"));
-            string itemTag = GetTag(item);
-
-           if (GetIsAreaInterior(area))
-           {
-               return;
-           }
-           EventsPlugin.SkipEvent();
-
-            if (itemTag == "sped_model1")
-            {
-                if (GetPhenoType(player) == PhenoType.SpeederBike)
+            _builder.Create("speeder")
+                .Delay(2f)
+                .PlaysAnimation(Animation.LoopingGetMid)
+                .ApplyAction((user, item, target, location) =>
                 {
-                    SetPhenoType(PhenoType.Normal, player);
-                    SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.None,player);
-                    CreaturePlugin.SetMovementRate(player, MovementRate.Normal);
-                    SendMessageToPC(player,"You dismount your speeder.");
-                }
-                else
-                {
-                    SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.SpeederBike,player);
-                    SetPhenoType(PhenoType.SpeederBike, player);
-                    CreaturePlugin.SetMovementRate(player, MovementRate.DMFast);
-                    SendMessageToPC(player, "You mount your speeder.");
-                }
-            }
-          
+                    var area = GetArea(user);
+                    var itemTag = GetTag(item);
+
+                    if (GetIsAreaInterior(area))
+                    {
+                        return;
+                    }
+
+                    if (GetPhenoType(user) == PhenoType.SpeederBike)
+                    {
+                        SetPhenoType(PhenoType.Normal, user);
+                        SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.None, user);
+                        CreaturePlugin.SetMovementRate(user, MovementRate.Normal);
+                        SendMessageToPC(user, "You dismount your speeder.");
+                    }
+                    else
+                    {
+                        SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.SpeederBike, user);
+                        SetPhenoType(PhenoType.SpeederBike, user);
+                        CreaturePlugin.SetMovementRate(user, MovementRate.DMFast);
+                        SendMessageToPC(user, "You mount your speeder.");
+                    }
+                    
+                });
         }
         /// <summary>
         /// On creature damaged if mounted, 25% chance for player to be dazed while getting knocked off the bike. 
@@ -56,27 +61,27 @@ namespace SWLOR.Game.Server.Feature
         [NWNEventHandler("crea_damaged")]
         public static void AttackedDismount()
         {
-            var player = OBJECT_SELF;;
+            var player = OBJECT_SELF; ;
 
-                if (GetPhenoType(player) == PhenoType.SpeederBike)
+            if (GetPhenoType(player) == PhenoType.SpeederBike)
+            {
+                Effect stun = EffectStunned();
+                int dazeChance = Random(100);
+                if (dazeChance < 25)
                 {
-                    Effect stun = EffectStunned();
-                    int dazeChance = Random(100);
-                    if (dazeChance < 25)
-                    {
-                        ApplyEffectToObject(DurationType.Temporary, stun, player, 2.0f);
-                    }
-                    AssignCommand(player, () =>
-                    {
-                        ActionPlayAnimation(Animation.LoopingDeadBack, 1, 2.0f);
-                    });
-
-                    FloatingTextStringOnCreature("You have been dismounted.", player, false);
-                    SetPhenoType(PhenoType.Normal, player);
-                    SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.None,player);
-                    CreaturePlugin.SetMovementRate(player, MovementRate.Normal);
+                    ApplyEffectToObject(DurationType.Temporary, stun, player, 2.0f);
                 }
+                AssignCommand(player, () =>
+                {
+                    ActionPlayAnimation(Animation.LoopingDeadBack, 1, 2.0f);
+                });
+
+                FloatingTextStringOnCreature("You have been dismounted.", player, false);
+                SetPhenoType(PhenoType.Normal, player);
+                SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.None, player);
+                CreaturePlugin.SetMovementRate(player, MovementRate.Normal);
             }
+        }
         /// <summary>
         /// When a creature acquires emnity and is mounted, dismount. 
         /// Set pheno to normal, tail to none and movement rate to normal.
@@ -111,12 +116,12 @@ namespace SWLOR.Game.Server.Feature
             if (GetPhenoType(player) == PhenoType.SpeederBike && GetIsAreaInterior(targetArea))
             {
                 FloatingTextStringOnCreature("You have been dismounted for entering an area with a speeder.", player, false);
-                ExecuteScript("mounted_enter", player);
                 SetPhenoType(PhenoType.Normal, player);
                 SetCreatureTailType(Core.NWScript.Enum.Creature.TailType.None, player);
                 CreaturePlugin.SetMovementRate(player, MovementRate.Normal);
             }
         }
+
 
     }
 }

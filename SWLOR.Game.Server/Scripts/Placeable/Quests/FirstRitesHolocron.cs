54f1e5de2a14cb538e6ce67a3e6d2d26b7a1d428
Cameron Valdez <cameronav23@gmail.com>
2021-11-10 11:16:41 -0600

    Delete force crystal class, add cutscene service and first rites holocron

---
+++ SWLOR.Game.Server/Scripts/Placeable/Quests/FirstRitesHolocron.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SWLOR.Game.Server.NWN;
+using SWLOR.Game.Server.GameObject;
+using SWLOR.Game.Server.Service;
+
+namespace SWLOR.Game.Server.Scripts.Placeable.Quests
+{
+    public class FirstRitesHolocron : IScript
+    {
+        public void SubscribeEvents()
+        {
+        }
+
+        public void UnsubscribeEvents()
+        {
+        }
+
+        public void Main()
+        {
+            const int QuestID = 30;
+            NWPlaceable crystal = _.OBJECT_SELF;
+            NWPlayer player = _.GetLastUsedBy();
+
+            player.SendMessage("You pick up the strange looking cube which seemingly opens itself at your touch. A stream of swirling blue light" +
+                    " shifts into what seems to be a recording of a robed humanoid assembling a weapon. With time, you think you may be able to do this yourself.");
+
+            var quest = QuestService.GetQuestByID(QuestID);
+            quest.Advance(player, crystal);
+
+            // Hide the "Source of Power?" placeable so the player can't use it again.
+            ObjectVisibilityService.AdjustVisibility(player, "81533EBB-2084-4C97-B004-8E1D8C395F56", false);
+
+            _.PlaySound("dt_ordi_11");
+            // Notify the player that new lightsaber perks have unlocked.
+            player.FloatingText("You have unlocked the Lightsaber Blueprints perk. Find this under the Engineering category in your perks menu.");
+        }
+    }
+}

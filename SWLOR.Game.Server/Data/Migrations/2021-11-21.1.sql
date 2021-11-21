79de4d7c8b3e305eb5b0b62516d49ae238656d11
Mithreas <jdt@metaswitch.com>
2021-11-21 20:07:47 +1000

    Fix the description on Shield Oath.

---
+++ SWLOR.Game.Server/Data/Migrations/2021-11-21.1.sql
@@ -0,0 +1,4 @@
+﻿-- Fix the description on Shield Oath, Perk 132
+UPDATE Perk
+SET Description = 'Increases Damage Immunity and enmity generation but reduces damage while active. Only one stance may be active at a time.'
+WHERE ID = 132;

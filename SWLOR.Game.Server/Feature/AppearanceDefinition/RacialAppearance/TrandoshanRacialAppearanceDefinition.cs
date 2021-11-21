552d9869a2e78eb81e457de5af779cb4ab2890c4
Scott Finlay <scottfinlay@protonmail.com>
2021-11-21 11:00:44 -0600

    Head Pack 3 ID changes
    ID changes for Head Pack 3.
    
    One minor note is that Human ID 142 is already listed as a PC, but I appear to have overwritten it with one of my heads.
    
    Personally I think the new asset looks better anyway, and the impact is only one specific head out of dozens, so the likelihood of it affecting a live player is low.

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TrandoshanRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TrandoshanRacialAppearanceDefinition.cs
@@ -2,8 +2,8 @@
 {
     public class TrandoshanRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
-        public override int[] MaleHeads { get; } = { 2, 101, 111, 123, 124, 125, 143, 162 };
-        public override int[] FemaleHeads { get; } = { 24, 126, 128, 135, 150, 157 };
+        public override int[] MaleHeads { get; } = { 2, 97, 98, 101, 111, 123, 124, 125, 143, 147, 148, 162 };
+        public override int[] FemaleHeads { get; } = { 24, 50, 51, 126, 128, 129, 131, 135, 150, 157 };
 
     }
 }

9fa85bee23f92e613cff93ab237574e209f59b41
zunath <none>
2021-11-22 14:55:35 -0500

    Fix Trandoshan parts.

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TrandoshanRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TrandoshanRacialAppearanceDefinition.cs
@@ -5,5 +5,17 @@
         public override int[] MaleHeads { get; } = { 2, 101, 111, 123, 124, 125, 143, 162 };
         public override int[] FemaleHeads { get; } = { 24, 126, 128, 135, 150, 157 };
 
+        public override int[] Torsos { get; } = { 201 };
+        public override int[] Pelvis { get; } = { 201 };
+        public override int[] RightBicep { get; } = { 201 };
+        public override int[] RightForearm { get; } = { 201 };
+        public override int[] RightHand { get; } = { 201 };
+        public override int[] RightThigh { get; } = { 201 };
+        public override int[] RightShin { get; } = { 201 };
+        public override int[] LeftBicep { get; } = { 201 };
+        public override int[] LeftForearm { get; } = { 201 };
+        public override int[] LeftHand { get; } = { 201 };
+        public override int[] LeftThigh { get; } = { 201 };
+        public override int[] LeftShin { get; } = { 201 };
     }
 }

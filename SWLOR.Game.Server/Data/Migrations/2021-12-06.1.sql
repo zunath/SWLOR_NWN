adfd048d9179de8b9b2dc2314732c8e1e1294746
Mithreas <38130103+Mithreas@users.noreply.github.com>
2021-12-06 22:57:35 +1000

    Light cannon (#1239)
    * Minor fix to the recipe for Starship Auxiliary Light Cannons, which was
    originally written in an era where perks allowed PCs to reduce the
    number of components needed for a recipe.

---
+++ SWLOR.Game.Server/Data/Migrations/2021-12-06.1.sql
@@ -0,0 +1,4 @@
+﻿-- Make the Starship Auxiliary Light Cannon craftable by reducing the required components numbers.
+UPDATE CraftBlueprint 
+SET MainMinimum = '2', SecondaryMinimum = '2'
+WHERE ID = 667;

607ae0f14283c2a57e8e678ca66c88628f5d5524
Scott Finlay <scottfinlay@protonmail.com>
2022-01-25 19:31:16 -0600

    Added SQL injection for new skills
    per Zunath instruction

---
+++ SWLOR.Game.Server/Data/2022-01-25.sql
@@ -0,0 +1,9 @@
+INSERT INTO `Skill` (`ID`, `SkillCategoryID`, `Name`, `MaxRank`, `IsActive`, `Description`, `Primary`, `Secondary`, `Tertiary`, `ContributesToSkillCap`) 
+VALUES ('39', '8', 'Togruti', '20', '1', 'Ability to speak the Togruti language.', '0', '0', '0', '0');
+
+INSERT INTO `Skill` (`ID`, `SkillCategoryID`, `Name`, `MaxRank`, `IsActive`, `Description`, `Primary`, `Secondary`, `Tertiary`, `ContributesToSkillCap`) 
+VALUES ('40', '8', 'Rodese', '20', '1', 'Ability to speak the Rodese language.', '0', '0', '0', '0');
+
+INSERT INTO `Skill` (`ID`, `SkillCategoryID`, `Name`, `MaxRank`, `IsActive`, `Description`, `Primary`, `Secondary`, `Tertiary`, `ContributesToSkillCap`) 
+VALUES ('41', '8', 'Kel Dor', '20', '1', 'Ability to speak the Kel Dor language.', '0', '0', '0', '0');
+

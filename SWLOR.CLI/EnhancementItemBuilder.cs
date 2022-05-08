aa5aa9c174d820c0b34c99fe58bf8e2e60cd3163
zunath <none>
2022-05-08 11:38:23 -0400

    Add code gen app for building recipes from spreadsheet to CLI project.

--- SWLOR.CLI/EnhancementItemBuilder.cs
+++ SWLOR.CLI/EnhancementItemBuilder.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Random = SWLOR.Game.Server.Service.Random;
 
 namespace SWLOR.CLI

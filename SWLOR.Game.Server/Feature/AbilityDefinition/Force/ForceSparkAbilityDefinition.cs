06a2ead03b271b9e1ddb9f410fe9c63ce1c03386
zunath <none>
2022-05-11 19:41:40 -0400

    Force Spark ability

--- SWLOR.Game.Server/Feature/AbilityDefinition/Force/ForceSparkAbilityDefinition.cs
+++ SWLOR.Game.Server/Feature/AbilityDefinition/Force/ForceSparkAbilityDefinition.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
 using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Core.NWScript.Enum.VisualEffect;
+using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.AbilityService;
+using SWLOR.Game.Server.Service.CombatService;
+using SWLOR.Game.Server.Service.PerkService;
+using SWLOR.Game.Server.Service.SkillService;
 
 namespace SWLOR.Game.Server.Feature.AbilityDefinition.Force
 {
@@ -10,8 +15,71 @@ namespace SWLOR.Game.Server.Feature.AbilityDefinition.Force
 
         public Dictionary<FeatType, AbilityDetail> BuildAbilities()
         {
+            ForceSpark1();
+            ForceSpark2();
+            ForceSpark3();
 
             return _builder.Build();
+        }
+        private void Impact(uint activator, uint target, int dmg, int evaDecrease)
+        {
+            var attackerStat = GetAbilityScore(activator, AbilityType.Willpower);
+            var defenderStat = GetAbilityScore(target, AbilityType.Willpower);
+            var attack = Stat.GetAttack(activator, AbilityType.Willpower, SkillType.Force);
+            var defense = Stat.GetDefense(target, CombatDamageType.Force, AbilityType.Willpower);
+            var damage = Combat.CalculateDamage(attack, dmg, attackerStat, defense, defenderStat, 0);
+
+            ApplyEffectToObject(DurationType.Instant, EffectDamage(damage), target);
+            ApplyEffectToObject(DurationType.Temporary, EffectACDecrease(evaDecrease), target, 60f);
+            ApplyEffectToObject(DurationType.Instant, EffectVisualEffect(VisualEffect.Vfx_Imp_Starburst_Red), target);
+
+            Enmity.ModifyEnmity(activator, target, damage + 10);
+            CombatPoint.AddCombatPoint(activator, target, SkillType.Force, 3);
+        }
+
+        private void ForceSpark1()
+        {
+            _builder.Create(FeatType.ForceSpark1, PerkType.ForceSpark)
+                .Name("Force Spark I")
+                .HasRecastDelay(RecastGroup.ForceSpark, 20f)
+                .RequirementFP(1)
+                .IsCastedAbility()
+                .UsesAnimation(Animation.LoopingConjure1)
+                .DisplaysVisualEffectWhenActivating()
+                .HasImpactAction((activator, target, level, location) =>
+                {
+                    Impact(activator, target, 9, 2);
+                });
+        }
+
+        private void ForceSpark2()
+        {
+            _builder.Create(FeatType.ForceSpark2, PerkType.ForceSpark)
+                .Name("Force Spark II")
+                .HasRecastDelay(RecastGroup.ForceSpark, 20f)
+                .RequirementFP(2)
+                .IsCastedAbility()
+                .UsesAnimation(Animation.LoopingConjure1)
+                .DisplaysVisualEffectWhenActivating()
+                .HasImpactAction((activator, target, level, location) =>
+                {
+                    Impact(activator, target, 14, 4);
+                });
+        }
+
+        private void ForceSpark3()
+        {
+            _builder.Create(FeatType.ForceSpark3, PerkType.ForceSpark)
+                .Name("Force Spark III")
+                .HasRecastDelay(RecastGroup.ForceSpark, 20f)
+                .RequirementFP(3)
+                .IsCastedAbility()
+                .UsesAnimation(Animation.LoopingConjure1)
+                .DisplaysVisualEffectWhenActivating()
+                .HasImpactAction((activator, target, level, location) =>
+                {
+                    Impact(activator, target, 32, 6);
+                });
         }
     }
 }

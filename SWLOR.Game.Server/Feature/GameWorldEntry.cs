86295ef369c3b4504e01c7b24fe5c99a2686f827
zunath <none>
2022-05-21 20:02:13 -0400

    Replace the OOC area with Scorchy's new area.

---
+++ SWLOR.Game.Server/Feature/GameWorldEntry.cs
@@ -0,0 +1,20 @@
+﻿using SWLOR.Game.Server.Core;
+
+namespace SWLOR.Game.Server.Feature
+{
+    public class GameWorldEntry
+    {
+        [NWNEventHandler("enter_world")]
+        public static void EnterGameWorld()
+        {
+            var player = GetPCSpeaker();
+            var waypoint = GetObjectByTag("ENTRY_STARTING_WP");
+            var location = GetLocation(waypoint);
+
+            AssignCommand(player, () =>
+            {
+                ActionJumpToLocation(location);
+            });
+        }
+    }
+}

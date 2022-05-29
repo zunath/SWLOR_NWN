bc7703d0da57b1ebca7425c48ed079f58a1dfbe1
zunath <none>
2022-05-29 09:54:07 -0400

    Add loot table for Sith Crypt Crates.

--- SWLOR.Game.Server/Feature/LootTableDefinition/KorribanLootTableDefinition.cs
+++ SWLOR.Game.Server/Feature/LootTableDefinition/KorribanLootTableDefinition.cs
@@ -52,11 +52,43 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
 
         private void SithCryptCrates()
         {
-            _builder.Create("KORRIBAN_SITH_CRATE_1");
+            _builder.Create("KORRIBAN_SITH_CRATE_1")
+                .AddItem("elec_flawed", 20)
+                .AddItem("ref_scordspar", 5)
+                .AddItem("fine_wood", 5)
+                .AddItem("fiberp_flawed", 5)
+                .AddItem("distilled_water", 10)
+                .AddItem("b_flour", 2)
+                .AddItem("sweet_butter", 5)
+                .AddItem("v_apple", 3)
+                .AddItem("choco_cookies", 4)
+                .AddGold(24, 15)
+                .AddItem("jade", 1, 1, true);
 
-            _builder.Create("KORRIBAN_SITH_CRATE_2");
+            _builder.Create("KORRIBAN_SITH_CRATE_2")
+                .AddItem("elec_flawed", 20)
+                .AddItem("ref_scordspar", 5)
+                .AddItem("fine_wood", 5)
+                .AddItem("fiberp_flawed", 5)
+                .AddItem("distilled_water", 10)
+                .AddItem("b_flour", 10)
+                .AddItem("sweet_butter", 5)
+                .AddItem("v_apple", 3)
+                .AddItem("cairn_sandwich", 3)
+                .AddGold(22, 15);
 
-            _builder.Create("KORRIBAN_SITH_CRATE_3");
+            _builder.Create("KORRIBAN_SITH_CRATE_3")
+                .AddItem("elec_flawed", 20)
+                .AddItem("ref_scordspar", 12)
+                .AddItem("fine_wood", 12)
+                .AddItem("fiberp_flawed", 12)
+                .AddItem("b_flour", 5)
+                .AddItem("sweet_butter", 5)
+                .AddItem("v_apple", 3)
+                .AddItem("green_curry", 5)
+                .AddGold(40, 10)
+                .AddItem("agate", 1, 1, true)
+                .AddItem("map_56", 1, 1, true);
         }
     }
 }

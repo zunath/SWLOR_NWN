c5e4b23e14eab3f82872eb60286a9ebb79a581c0
zunath <none>
2022-05-29 09:13:24 -0400

    Add Sith Crypt spawn table

--- SWLOR.Game.Server/Feature/SpawnDefinition/KorribanSpawnDefinition.cs
+++ SWLOR.Game.Server/Feature/SpawnDefinition/KorribanSpawnDefinition.cs
@@ -15,6 +15,7 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
             Ravine();
             Caverns();
             Dunes();
+            SithCrypt();
 
             return _builder.Build();
         }
@@ -73,7 +74,7 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
 
         private void Caverns()
         {
-            _builder.Create("KORRIBAN_CAVERNS", "Ravine")
+            _builder.Create("KORRIBAN_CAVERNS", "Caverns")
                 .AddSpawn(ObjectType.Creature, "shyrack")
                 .WithFrequency(30)
                 .RandomlyWalks()
@@ -100,6 +101,20 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
 
                 .AddSpawn(ObjectType.Creature, "korr_terentatek")
                 .WithFrequency(5)
+                .RandomlyWalks()
+                .ReturnsHome();
+        }
+
+        private void SithCrypt()
+        {
+            _builder.Create("KORRIBAN_SITH_CRYPT", "Sith Crypt")
+                .AddSpawn(ObjectType.Creature, "sith_ghost")
+                .WithFrequency(50)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "sith_ghost_male")
+                .WithFrequency(50)
                 .RandomlyWalks()
                 .ReturnsHome();
         }

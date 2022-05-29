4de78ea13ec5117a7aa64239d76875aacf385ded
zunath <none>
2022-05-29 12:24:23 -0400

    Update recipe builder based on new spreadsheet layout.

--- SWLOR.CLI/RecipeCodeBuilder.cs
+++ SWLOR.CLI/RecipeCodeBuilder.cs
@@ -17,6 +17,8 @@ namespace SWLOR.CLI
         {
             ClearOutputDirectory();
 
+            var componentTemplate = $"{Environment.NewLine}\t.Component(\"%%COMPONENTRESREF%%\", %%COMPONENTQUANTITY%%)";
+
             var recipeTemplate = File.ReadAllText(Template);
             var inputLines = File.ReadAllLines(InputData);
             var output = string.Empty;
@@ -27,39 +29,60 @@ namespace SWLOR.CLI
                 if (string.IsNullOrWhiteSpace(data[0]))
                     continue;
 
-
-                var recipeName = data[6].Trim();
-                var categoryEnumName = data[4].Trim();
+                var skill = data[0].Trim();
+                var recipeName = data[5].Trim();
+                var categoryEnumName = data[2].Trim().Replace("-", "").Replace(" ", "");
                 var recipeEnumName = data[3].Trim();
-                var resref = data[9].Trim();
-                var level = data[7].Trim();
-                var perkLevel = data[5].Trim();
-                var modSlots = data[11].Trim();
-                var component1Resref = data[12].Trim();
-                var component1Quantity = data[13].Trim();
-                var component2Resref = data[14].Trim();
-                var component2Quantity = data[15].Trim();
-                var component3Resref = data[16].Trim();
-                var component3Quantity = data[17].Trim();
-                var component4Resref = data[18].Trim();
-                var component4Quantity = data[19].Trim();
+                var resref = data[8].Trim();
+                var level = data[6].Trim();
+                var perkLevel = data[4].Trim();
+                var modSlots = data[10].Trim();
+                var component1Resref = data[11].Trim();
+                var component1Quantity = data[12].Trim();
+                var component2Resref = data[13].Trim();
+                var component2Quantity = data[14].Trim();
+                var component3Resref = data[15].Trim();
+                var component3Quantity = data[16].Trim();
+                var component4Resref = data[17].Trim();
+                var component4Quantity = data[18].Trim();
 
                 var recipeCode = recipeTemplate
+                    .Replace("%%RECIPESKILL%%", skill)
                     .Replace("%%RECIPENAME%%", recipeName)
                     .Replace("%%CATEGORYENUMNAME%%", categoryEnumName)
                     .Replace("%%RECIPEENUMNAME%%", recipeEnumName)
                     .Replace("%%RESREF%%", resref)
                     .Replace("%%LEVEL%%", level)
                     .Replace("%%PERKLEVEL%%", perkLevel)
-                    .Replace("%%MODSLOTS%%", modSlots)
-                    .Replace("%%COMPONENT1RESREF%%", component1Resref)
-                    .Replace("%%COMPONENT1QUANTITY%%", component1Quantity)
-                    .Replace("%%COMPONENT2RESREF%%", component2Resref)
-                    .Replace("%%COMPONENT2QUANTITY%%", component2Quantity)
-                    .Replace("%%COMPONENT3RESREF%%", component3Resref)
-                    .Replace("%%COMPONENT3QUANTITY%%", component3Quantity)
-                    .Replace("%%COMPONENT4RESREF%%", component4Resref)
-                    .Replace("%%COMPONENT4QUANTITY%%", component4Quantity);
+                    .Replace("%%MODSLOTS%%", modSlots);
+
+                var components = string.Empty;
+                if (!string.IsNullOrWhiteSpace(component1Resref) && !string.IsNullOrWhiteSpace(component1Quantity))
+                {
+                    components += componentTemplate
+                        .Replace("%%COMPONENTRESREF%%", component1Resref)
+                        .Replace("%%COMPONENTQUANTITY%%", component1Quantity);
+                }
+                if (!string.IsNullOrWhiteSpace(component2Resref) && !string.IsNullOrWhiteSpace(component2Quantity))
+                {
+                    components += componentTemplate
+                        .Replace("%%COMPONENTRESREF%%", component2Resref)
+                        .Replace("%%COMPONENTQUANTITY%%", component2Quantity);
+                }
+                if (!string.IsNullOrWhiteSpace(component3Resref) && !string.IsNullOrWhiteSpace(component3Quantity))
+                {
+                    components += componentTemplate
+                        .Replace("%%COMPONENTRESREF%%", component3Resref)
+                        .Replace("%%COMPONENTQUANTITY%%", component3Quantity);
+                }
+                if (!string.IsNullOrWhiteSpace(component4Resref) && !string.IsNullOrWhiteSpace(component4Quantity))
+                {
+                    components += componentTemplate
+                        .Replace("%%COMPONENTRESREF%%", component4Resref)
+                        .Replace("%%COMPONENTQUANTITY%%", component4Quantity);
+                }
+
+                recipeCode = recipeCode.Replace("%%COMPONENTS%%", components);
 
                 recipeCode += Environment.NewLine;
                 recipeCode += Environment.NewLine;

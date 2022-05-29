ecb87cb9ae0dfcd9b187316d5c8d6caf7659a7d6
zunath <none>
2022-05-28 20:21:39 -0400

    Stub out Korriban and Dathomir. Move several creatures into their respective categories.

--- SWLOR.Game.Server/Service/WeatherService/WeatherPlanetDefinitions.cs
+++ SWLOR.Game.Server/Service/WeatherService/WeatherPlanetDefinitions.cs
@@ -62,6 +62,16 @@ namespace SWLOR.Game.Server.Service.WeatherService
                     CloudyText = "Clouds build over head, and there is a occasional strong gust of wind.",
                     ColdCloudyText = "The clouds over head build, a cold wind stings exposed flesh. Looks like it is going to snow.",
                     MildText = "It is cold, the sky is clear, and there is a gentle breeze.",
+                },
+                [PlanetType.Korriban] = new WeatherClimate
+                {
+                    HeatModifier = +3,
+                    HumidityModifier = -5
+                },
+                [PlanetType.Dathomir] = new WeatherClimate
+                {
+                    HeatModifier = -1,
+                    HumidityModifier = +1
                 }
             };
         }

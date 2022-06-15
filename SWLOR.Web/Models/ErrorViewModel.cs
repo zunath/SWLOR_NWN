6ad97c65feb75634ff225358081b6a1a1696c84f
Zunath <coolty3001@yahoo.com>
2022-06-14 21:06:19 -0400

    Convert web app to ASP.NET MVC site

---
+++ SWLOR.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWLOR.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

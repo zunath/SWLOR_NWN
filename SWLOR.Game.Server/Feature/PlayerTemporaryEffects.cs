40c5b68be0583c9f26c882b3423149437d939475
Zunath <coolty3001@yahoo.com>
2022-06-21 20:32:34 -0400

    Reapply BAB upon login into the server. Fixes an issue where the number of attacks isn't correct after a relog.

--- SWLOR.Game.Server/Feature/PlayerTemporaryEffects.cs
+++ SWLOR.Game.Server/Feature/PlayerTemporaryEffects.cs
@@ -1,4 +1,5 @@
 ﻿using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.NWNX;
 using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Entity;
 using SWLOR.Game.Server.Service;
@@ -14,6 +15,7 @@ namespace SWLOR.Game.Server.Feature
             ApplyCutsceneGhostToPlayer(player);
             ApplyHeight(player);
             RemoveImmobility(player);
+            ReapplyBAB(player);
         }
 
         private static void ApplyCutsceneGhostToPlayer(uint player)
@@ -42,6 +44,11 @@ namespace SWLOR.Game.Server.Feature
                     RemoveEffect(player, effect);
                 }
             }
+        }
+
+        private static void ReapplyBAB(uint player)
+        {
+            CreaturePlugin.SetBaseAttackBonus(player, GetBaseAttackBonus(player));
         }
 
         /// <summary>

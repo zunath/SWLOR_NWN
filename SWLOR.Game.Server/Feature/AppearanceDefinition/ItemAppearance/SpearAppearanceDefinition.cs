34268e07c4ab578abb16d8456dd93509ae740e53
Zunath <coolty3001@yahoo.com>
2022-06-26 22:21:08 -0400

    Stub out appearances for other weapon types. Set up Dagger, Electroblade, Great Sworrd, Katar, Pistol, Quarterstaff, Rifle.

---
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/ItemAppearance/SpearAppearanceDefinition.cs
@@ -0,0 +1,44 @@
+﻿namespace SWLOR.Game.Server.Feature.AppearanceDefinition.ItemAppearance
+{
+    public class SpearAppearanceDefinition : WeaponAppearanceBaseDefinition
+    {
+        public override bool IsSimple => false;
+
+        public override int[] TopParts { get; } =
+        {
+            // Color #1
+            // Color #2
+            // Color #3
+            // Color #4
+            // Color #5
+            // Color #6
+            // Color #7
+            // Color #8
+            // Color #9
+        };
+        public override int[] MiddleParts { get; } =
+        {
+            // Color #1
+            // Color #2
+            // Color #3
+            // Color #4
+            // Color #5
+            // Color #6
+            // Color #7
+            // Color #8
+            // Color #9
+        };
+        public override int[] BottomParts { get; } =
+        {
+            // Color #1
+            // Color #2
+            // Color #3
+            // Color #4
+            // Color #5
+            // Color #6
+            // Color #7
+            // Color #8
+            // Color #9
+        };
+    }
+}

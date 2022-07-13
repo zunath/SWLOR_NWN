57946178352342730814f93e49f7cd3aab91beff
Zunath <coolty3001@yahoo.com>
2022-07-13 19:45:09 -0400

    Migration fix, legacy item cleanup

--- SWLOR.Game.Server/Feature/MigrationDefinition/ServerMigration/_2_LegacyServerMigration.cs
+++ SWLOR.Game.Server/Feature/MigrationDefinition/ServerMigration/_2_LegacyServerMigration.cs
@@ -1,4 +1,6 @@
-﻿using SWLOR.Game.Server.Core.NWNX;
+﻿using System;
+using System.Linq;
+using SWLOR.Game.Server.Core.NWNX;
 using SWLOR.Game.Server.Entity;
 using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.DBService;
@@ -6,9 +8,9 @@ using SWLOR.Game.Server.Service.MigrationService;
 
 namespace SWLOR.Game.Server.Feature.MigrationDefinition.ServerMigration
 {
-    public class _1_LegacyServerMigration: LegacyMigrationBase, IServerMigration
+    public class _2_LegacyServerMigration: LegacyMigrationBase, IServerMigration
     {
-        public int Version => 1;
+        public int Version => 2;
         public void Migrate()
         {
             MigratePersistentStorageItems();
@@ -16,11 +18,24 @@ namespace SWLOR.Game.Server.Feature.MigrationDefinition.ServerMigration
 
         private void MigratePersistentStorageItems()
         {
-            var items = DB.Search(new DBQuery<InventoryItem>());
+            var query = new DBQuery<InventoryItem>();
+            var itemCount = (int)DB.SearchCount(query);
+            var items = DB.Search(query.AddPaging(itemCount, 0)).ToList();
+            var tempStorage = GetObjectByTag("MIGRATION_STORAGE");
 
             foreach (var item in items)
             {
+                if (item.StorageId != "BANK_CZ220" && item.StorageId != "BANK_VELES" && item.StorageId != "BANK_MONCALA")
+                    continue;
+
+                if (item.IconResref != "unknown_item")
+                    continue;
+
                 var deserialized = ObjectPlugin.Deserialize(item.Data);
+                if (!GetIsObjectValid(deserialized))
+                    continue;
+
+                ObjectPlugin.AcquireItem(tempStorage, deserialized);
 
                 WipeItemProperties(deserialized);
                 Item.MarkLegacyItem(deserialized);

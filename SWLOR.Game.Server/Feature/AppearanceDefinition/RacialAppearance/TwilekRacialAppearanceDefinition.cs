286ded0aac03a7e63eaa54a871f76e87a7b734c8
Scott Finlay <scottfinlay@protonmail.com>
2022-07-20 18:33:25 -0500

    Xis headpack 4 (#1432)
    * Head Pack 4
    
    New IDs:
    - Male Human/Mirialan/Echani/Chiss: 50, 51, 52, 53, 54, 63, 64, 75
    - Male Trandoshan: 96
    - Female Human/Mirialan/Echani/Chiss: 54, 55, 56, 57, 58
    - New Helmets: 61, 62
    
    * Head Tweak
    
    Added Twilek Head 216

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TwilekRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/TwilekRacialAppearanceDefinition.cs
@@ -3,6 +3,6 @@
     public class TwilekRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
         public override int[] MaleHeads { get; } = { 65, 66, 67, 115 };
-        public override int[] FemaleHeads { get; } = { 79, 80, 81, 82, 139, 144, 145, 205, 206, 207, 208, 209 };
+        public override int[] FemaleHeads { get; } = { 79, 80, 81, 82, 139, 144, 145, 205, 206, 207, 208, 209, 216 };
     }
 }

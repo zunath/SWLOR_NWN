ab6ef4cbb94faa72827210b21540dc0af45e3202
Zunath <coolty3001@yahoo.com>
2022-07-20 22:07:13 -0400

    Place a Force Sensitive restriction on the Strong Style perks.

--- SWLOR.Game.Server/Feature/PerkDefinition/TwoHandedPerkDefinition.cs
+++ SWLOR.Game.Server/Feature/PerkDefinition/TwoHandedPerkDefinition.cs
@@ -863,6 +863,7 @@ namespace SWLOR.Game.Server.Feature.PerkDefinition
                 })
 
                 .AddPerkLevel()
+                .RequirementCharacterType(CharacterType.ForceSensitive)
                 .Description("Saberstaff attacks use your Perception stat for accuracy and Might stat for damage while active.")
                 .Price(1)
                 .GrantsFeat(FeatType.StrongStyleSaberstaff);

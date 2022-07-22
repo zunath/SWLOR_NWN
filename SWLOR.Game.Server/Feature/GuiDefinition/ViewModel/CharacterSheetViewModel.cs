a0f444b0336efe5b7aff2446ff7071c42edc38fa
Zunath <coolty3001@yahoo.com>
2022-07-21 22:23:39 -0400

    Hook Holocom up to the character sheet.

--- SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/CharacterSheetViewModel.cs
+++ SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/CharacterSheetViewModel.cs
@@ -2,6 +2,7 @@
 using SWLOR.Game.Server.Core.NWNX;
 using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Entity;
+using SWLOR.Game.Server.Feature.DialogDefinition;
 using SWLOR.Game.Server.Feature.GuiDefinition.RefreshEvent;
 using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.AbilityService;
@@ -242,6 +243,11 @@ namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
         public Action OnClickRecipes() => () =>
         {
             Gui.TogglePlayerWindow(Player, GuiWindowType.Recipes);
+        };
+
+        public Action OnClickHoloCom() => () =>
+        {
+            Dialog.StartConversation(Player, Player, nameof(HoloComDialog));
         };
 
         public Action OnClickKeyItems() => () =>

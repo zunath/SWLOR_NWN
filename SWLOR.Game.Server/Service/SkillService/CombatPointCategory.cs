c3f035323c553d9a38d206df7c30d0199cf3c251
Wildkins <john.wildkins@gmail.com>
2022-07-28 22:30:39 -0400

    Implement XP/Combat Point changes (#1456)
    * Categorize XP sharing across skills, exempt First Aid from affecting XP gain
    
    * Address comments
    
    * Remove errant comment

---
+++ SWLOR.Game.Server/Service/SkillService/CombatPointCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWLOR.Game.Server.Service.SkillService
+{
+    public enum CombatPointCategoryType
+    {
+        Exempt = 0,
+        Utility = 1,
+        Weapon = 2
+    }
+
+}

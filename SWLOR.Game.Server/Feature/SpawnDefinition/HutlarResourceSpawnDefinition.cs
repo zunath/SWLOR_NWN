d7451ec96bb35ec5eb461d15ccec6fc511298597
Wildkins <john.wildkins@gmail.com>
2022-07-29 04:01:32 -0400

    Add placeholder resource/creature spawns to Hutlar Frozen Wastes/Caves (#1463)

--- SWLOR.Game.Server/Feature/SpawnDefinition/HutlarResourceSpawnDefinition.cs
+++ SWLOR.Game.Server/Feature/SpawnDefinition/HutlarResourceSpawnDefinition.cs
@@ -11,6 +11,8 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
         {
             QionTundra();
             Valley();
+            Wastes();
+            FrozenCave();
 
             return _builder.Build();
         }
@@ -58,6 +60,48 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
                 .WithFrequency(40)
                 .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
                 .WithFrequency(10);
+        }
+
+        private void Wastes()
+        {
+            _builder.Create("RESOURCES_HUTLAR_WASTES")
+                .AddSpawn(ObjectType.Placeable, "plagionite_vein")
+                .WithFrequency(40)
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(10)
+
+                .AddSpawn(ObjectType.Placeable, "ancient_tree")
+                .WithFrequency(20)
+                .AddSpawn(ObjectType.Placeable, "aracia_tree")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies3")
+                .WithFrequency(20)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_3")
+                .WithFrequency(20)
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5);
+        }
+
+        private void FrozenCave()
+        {
+            _builder.Create("RESOURCES_FROZEN_CAVE")
+                .AddSpawn(ObjectType.Placeable, "plagionite_vein")
+                .WithFrequency(20)
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies3")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(2)
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_3")
+                .WithFrequency(40)
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(10);
+
         }
     }
 }

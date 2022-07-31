6ec1457b83ea6a9dfe31137ffa379ca46fb6ec76
Zunath <coolty3001@yahoo.com>
2022-07-31 18:21:11 -0400

    Stubbing out refinery changes

---
+++ SWLOR.Game.Server/Feature/GuiDefinition/RefineryDefinition.cs
@@ -0,0 +1,26 @@
+﻿using SWLOR.Game.Server.Core.Beamdog;
+using SWLOR.Game.Server.Feature.GuiDefinition.ViewModel;
+using SWLOR.Game.Server.Service.GuiService;
+
+namespace SWLOR.Game.Server.Feature.GuiDefinition
+{
+    public class RefineryDefinition : IGuiWindowDefinition
+    {
+        private readonly GuiWindowBuilder<CharacterSheetViewModel> _builder = new();
+
+        public GuiConstructedWindow BuildWindow()
+        {
+            _builder.CreateWindow(GuiWindowType.Refinery)
+                .SetInitialGeometry(0, 0, 800f, 400f)
+                .SetTitle("Refinery")
+                .SetIsResizable(true)
+                .SetIsCollapsible(true)
+                .AddColumn(col =>
+                {
+
+                });
+
+            return _builder.Build();
+        }
+    }
+}
---
+++ SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/RefineryViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SWLOR.Game.Server.Service.GuiService;
+
+namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
+{
+    public class RefineryViewModel: GuiViewModelBase<RefineryViewModel, GuiPayloadBase>
+    {
+        private const int BaseItemsRefinedPerCore = 3;
+        private const string PowerCoreIconResref = "iit_midmisc_008";
+
+        public GuiBindingList<string> InputItemNames
+        {
+            get => Get<GuiBindingList<string>>();
+            set => Set(value);
+        }
+
+        public GuiBindingList<string> OutputItemNames
+        {
+            get => Get<GuiBindingList<string>>();
+            set => Set(value);
+        }
+
+        public string RequiredPowerCores
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+
+        protected override void Initialize(GuiPayloadBase initialPayload)
+        {
+
+        }
+
+        public Action OnClickAddItem() => () =>
+        {
+
+        };
+
+        public Action OnClickRemoveItems() => () =>
+        {
+
+        };
+
+        public Action OnClickRefine() => () =>
+        {
+
+        };
+    }
+}

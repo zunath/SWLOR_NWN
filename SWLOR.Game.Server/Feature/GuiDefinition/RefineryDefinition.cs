2b968fe914cf30a74d16325334bc53761d80570a
Zunath <coolty3001@yahoo.com>
2022-07-31 21:25:04 -0400

    Main refinery functionality.

--- SWLOR.Game.Server/Feature/GuiDefinition/RefineryDefinition.cs
+++ SWLOR.Game.Server/Feature/GuiDefinition/RefineryDefinition.cs
@@ -6,7 +6,7 @@ namespace SWLOR.Game.Server.Feature.GuiDefinition
 {
     public class RefineryDefinition : IGuiWindowDefinition
     {
-        private readonly GuiWindowBuilder<CharacterSheetViewModel> _builder = new();
+        private readonly GuiWindowBuilder<RefineryViewModel> _builder = new();
 
         public GuiConstructedWindow BuildWindow()
         {
@@ -15,10 +15,99 @@ namespace SWLOR.Game.Server.Feature.GuiDefinition
                 .SetTitle("Refinery")
                 .SetIsResizable(true)
                 .SetIsCollapsible(true)
+                .BindIsClosable(model => model.IsCloseEnabled)
+                .AddColumn(col =>
+                {
+                    col.AddRow(row =>
+                    {
+                        row.AddLabel()
+                            .SetHeight(20f)
+                            .BindText(model => model.Instructions)
+                            .SetHorizontalAlign(NuiHorizontalAlign.Left)
+                            .SetVerticalAlign(NuiVerticalAlign.Middle)
+                            .BindColor(model => model.InstructionsColor);
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddLabel()
+                            .SetText("Input Items:")
+                            .SetHeight(20f);
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddList(template =>
+                        {
+                            template.AddCell(cell =>
+                                {
+                                    cell.AddToggleButton()
+                                        .BindText(model => model.InputItemNames)
+                                        .BindIsToggled(model => model.InputItemToggles)
+                                        .BindOnClicked(model => model.OnClickInputItem());
+                                });
+                        })
+                            .BindRowCount(model => model.InputItemNames);
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddButton()
+                            .SetText("Add Item")
+                            .BindOnClicked(model => model.OnClickAddItem());
+
+                        row.AddButton()
+                            .SetText("Remove Items")
+                            .BindOnClicked(model => model.OnClickRemoveItems());
+                    });
+
+
+                    col.AddRow(row =>
+                    {
+                        row.AddImage()
+                            .SetResref(RefineryViewModel.PowerCoreIconResref)
+                            .SetHeight(64f)
+                            .SetWidth(64f);
+
+                        row.AddLabel()
+                            .BindText(model => model.RequiredPowerCores);
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+                        row.AddButton()
+                            .SetText("Refine Items")
+                            .BindOnClicked(model => model.OnClickRefine());
+                        row.AddSpacer();
+                    });
+
+                })
+                
                 .AddColumn(col =>
                 {
 
-                });
+                    col.AddRow(row =>
+                    {
+                        row.AddLabel()
+                            .SetText("Output Items:")
+                            .SetHeight(20f);
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddList(template =>
+                            {
+                                template.AddCell(cell =>
+                                {
+                                    cell.AddToggleButton()
+                                        .BindText(model => model.OutputItemNames);
+                                });
+                            })
+                            .BindRowCount(model => model.OutputItemNames);
+                    });
+                })
+                ;
 
             return _builder.Build();
         }
--- SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/RefineryViewModel.cs
+++ SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/RefineryViewModel.cs
@@ -1,16 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SWLOR.Game.Server.Core;
+using SWLOR.Game.Server.Core.NWNX;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.GuiService;
+using SWLOR.Game.Server.Service.GuiService.Component;
+using SWLOR.Game.Server.Service.PerkService;
+using SWLOR.Game.Server.Service.SkillService;
 
 namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
 {
     public class RefineryViewModel: GuiViewModelBase<RefineryViewModel, GuiPayloadBase>
     {
+        private static GuiColor _green = new GuiColor(0, 255, 0);
+        private static GuiColor _red = new GuiColor(255, 0, 0);
+
+        private class OreDetail
+        {
+            public int RequiredLevel { get; }
+            public string RefinedItemResref { get; }
+            public int XPGranted { get; }
+
+
+            public OreDetail(int requiredLevel, string refinedItemResref, int xpGranted)
+            {
+                RequiredLevel = requiredLevel;
+                RefinedItemResref = refinedItemResref;
+                XPGranted = xpGranted;
+            }
+        }
+
+        private static readonly Dictionary<string, OreDetail> _ores = new Dictionary<string, OreDetail>
+        {
+            {"raw_veldite", new OreDetail(1, "ref_veldite", 25)},
+            {"ore_tilarium", new OreDetail(1, "ref_tilarium", 25)},
+
+            {"raw_scordspar", new OreDetail(2, "ref_scordspar", 50)},
+            {"ore_currian", new OreDetail(2, "ref_currian", 50)},
+
+
+            {"raw_plagionite", new OreDetail(3, "ref_plagionite", 75)},
+            {"ore_idailia", new OreDetail(3, "ref_idailia", 75)},
+
+            {"raw_keromber", new OreDetail(4, "ref_keromber", 100)},
+            {"ore_barinium", new OreDetail(4, "ref_barinium", 100)},
+
+            {"raw_jasioclase", new OreDetail(5, "ref_jasioclase", 125)},
+            {"ore_gostian", new OreDetail(5, "ref_gostian", 125)},
+
+            // Options below are for further expansion down the road.
+            {"raw_hemorgite", new OreDetail(99, "ref_hemorgite", 150)},
+            {"raw_ochne", new OreDetail(99, "ref_ochne", 175)},
+            {"raw_croknor", new OreDetail(99, "ref_croknor", 200)},
+            {"raw_arkoxit", new OreDetail(99, "ref_arkoxit", 225)},
+            {"raw_bisteiss", new OreDetail(99, "ref_bisteiss", 250)},
+        };
+
         private const int BaseItemsRefinedPerCore = 3;
-        private const string PowerCoreIconResref = "iit_midmisc_008";
+        public const string PowerCoreIconResref = "iit_midmisc_008";
+        private const float RefiningDelaySeconds = 6f;
+        private bool _isRefining;
+
+        private List<string> _inputItems;
+        private List<int> _inputStackSizes;
+        private List<string> _inputItemResrefs;
+
+        private int ItemCount => _inputStackSizes.Sum();
 
         public GuiBindingList<string> InputItemNames
         {
             get => Get<GuiBindingList<string>>();
+            set => Set(value);
+        }
+
+        public GuiBindingList<bool> InputItemToggles
+        {
+            get => Get<GuiBindingList<bool>>();
             set => Set(value);
         }
 
@@ -26,25 +93,201 @@ namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
             set => Set(value);
         }
 
+        public bool IsCloseEnabled
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public string Instructions
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public GuiColor InstructionsColor
+        {
+            get => Get<GuiColor>();
+            set => Set(value);
+        }
 
         protected override void Initialize(GuiPayloadBase initialPayload)
         {
+            IsCloseEnabled = true;
+            _isRefining = false;
+            _inputItems = new List<string>();
+            _inputStackSizes = new List<int>();
+            _inputItemResrefs = new List<string>();
+            InputItemNames = new GuiBindingList<string>();
+            InputItemToggles = new GuiBindingList<bool>();
+            OutputItemNames = new GuiBindingList<string>();
 
+            CalculateCoresRequired();
+        }
+
+        private void CalculateCoresRequired()
+        {
+            var refineryManagement = Perk.GetEffectivePerkLevel(Player, PerkType.RefineryManagement);
+            var itemsPerCore = BaseItemsRefinedPerCore + refineryManagement;
+            var quantity = (int)Math.Ceiling(ItemCount / (float)itemsPerCore);
+            
+            if(quantity == 1)
+                RequiredPowerCores = $"{quantity}x Power Core Required";
+            else
+                RequiredPowerCores = $"{quantity}x Power Cores Required";
         }
 
         public Action OnClickAddItem() => () =>
         {
+            bool ValidateItem(uint item)
+            {
+                if (_isRefining)
+                    return false;
 
+                if (GetItemPossessor(item) != Player)
+                {
+                    SendMessageToPC(Player, "Item must be in your inventory.");
+                    return false;
+                }
+
+                var resref = GetResRef(item);
+
+                // Not a valid item.
+                if (!_ores.ContainsKey(resref))
+                {
+                    SendMessageToPC(Player, "Only raw materials may be added to the refining list.");
+                    return false;
+                }
+
+                // Player doesn't have prerequisite level.
+                var perkLevel = Perk.GetEffectivePerkLevel(Player, PerkType.Refining);
+                if (perkLevel < _ores[resref].RequiredLevel)
+                {
+                    SendMessageToPC(Player, $"Your Refining perk level must be at least {_ores[resref].RequiredLevel} to refine that item.");
+                    return false;
+                }
+
+                return true;
+            }
+
+            Targeting.EnterTargetingMode(Player, ObjectType.Item, "Please select a resource to refine.", (item) =>
+            {
+                if (!ValidateItem(item))
+                    return;
+
+                var resref = GetResRef(item);
+                var itemName = Cache.GetItemNameByResref(resref);
+                var stackSize = GetItemStackSize(item);
+                var serialized = ObjectPlugin.Serialize(item);
+                var outputOre = _ores[resref];
+                var outputItemName = Cache.GetItemNameByResref(outputOre.RefinedItemResref);
+
+                InputItemNames.Add($"{stackSize}x {itemName}");
+                InputItemToggles.Add(false);
+                OutputItemNames.Add($"{stackSize}x {outputItemName}");
+                _inputItems.Add(serialized);
+                _inputStackSizes.Add(stackSize);
+                _inputItemResrefs.Add(resref);
+
+                CalculateCoresRequired();
+
+                DestroyObject(item);
+            });
         };
 
         public Action OnClickRemoveItems() => () =>
         {
+            if (_isRefining)
+                return;
 
+            for (var index = InputItemToggles.Count-1; index >= 0; index--)
+            {
+                var isToggled = InputItemToggles[index];
+
+                if (!isToggled)
+                    continue;
+
+                var serialized = _inputItems[index];
+                var item = ObjectPlugin.Deserialize(serialized);
+                ObjectPlugin.AcquireItem(Player, item);
+
+                _inputItems.RemoveAt(index);
+                _inputStackSizes.RemoveAt(index);
+                _inputItemResrefs.RemoveAt(index);
+                InputItemToggles.RemoveAt(index);
+                InputItemNames.RemoveAt(index);
+                OutputItemNames.RemoveAt(index);
+
+                CalculateCoresRequired();
+            }
         };
 
         public Action OnClickRefine() => () =>
         {
+            if (_isRefining)
+                return;
 
+            if (ItemCount <= 0)
+            {
+                Instructions = "Please add items.";
+                InstructionsColor = _red;
+                return;
+            }
+
+            Instructions = string.Empty;
+            IsCloseEnabled = false;
+
+            // Flag player as refining so that they can't queue up another item at the same time.
+            SetLocalBool(Player, "IS_REFINING", true);
+
+            // Apply immobilization
+            var effect = EffectCutsceneImmobilize();
+            effect = TagEffect(effect, "REFINING_EFFECT");
+            ApplyEffectToObject(DurationType.Temporary, effect, Player, RefiningDelaySeconds);
+
+            // Play an animation
+            AssignCommand(Player, () => ActionPlayAnimation(Animation.LoopingGetMid, 1.0f, RefiningDelaySeconds));
+
+            // Display the timing bar and finish the process when the delay elapses.
+            PlayerPlugin.StartGuiTimingBar(Player, RefiningDelaySeconds);
+            DelayCommand(RefiningDelaySeconds, () =>
+            {
+                var xp = 0;
+
+                for (var index = _inputItems.Count - 1; index >= 0; index--)
+                {
+                    var oreResref = _inputItemResrefs[index];
+                    var refinedItem = _ores[oreResref];
+                    var stackSize = _inputStackSizes[index];
+
+                    // Spawn the refined item onto the player.
+                    CreateItemOnObject(refinedItem.RefinedItemResref, Player, stackSize);
+
+                    xp += refinedItem.XPGranted;
+                }
+
+                DeleteLocalBool(Player, "IS_REFINING");
+                Skill.GiveSkillXP(Player, SkillType.Gathering, xp);
+
+                _inputItemResrefs.Clear();
+                _inputItems.Clear();
+                _inputStackSizes.Clear();
+                InputItemToggles.Clear();
+                InputItemNames.Clear();
+                OutputItemNames.Clear();
+
+                Instructions = "Success!";
+                InstructionsColor = _green;
+
+                CalculateCoresRequired();
+            });
         };
+
+        public Action OnClickInputItem() => () =>
+        {
+            var index = NuiGetEventArrayIndex();
+            InputItemToggles[index] = !InputItemToggles[index];
+        };
+
     }
 }

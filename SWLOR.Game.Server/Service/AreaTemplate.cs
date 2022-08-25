8d0f18a470b31ea6e52f358e4d970060029963c9
givemedeath <benco.moore@gmail.com>
2022-08-25 15:36:55 -0400

    Add paging support.

--- SWLOR.Game.Server/Service/AreaTemplate.cs
+++ SWLOR.Game.Server/Service/AreaTemplate.cs
@@ -6,6 +6,7 @@ using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Core;
 using SWLOR.Game.Server.Entity;
 using SWLOR.Game.Server.Service.DBService;
+using SWLOR.Game.Server.Service.GuiService;
 
 namespace SWLOR.Game.Server.Service
 {
@@ -13,7 +14,99 @@ namespace SWLOR.Game.Server.Service
     {
         private static Dictionary<string, uint> TemplateAreasByResref { get; } = new();
         private static Dictionary<uint, List<uint>> TemplateAreaCustomObjectsByArea { get; } = new();
+        private static Dictionary<int, Dictionary<int, string>> PlaceableAppearanceByPage { get; } = new();
+        private static Dictionary<int, Dictionary<int, string>> CreatureAppearanceByPage { get; } = new();
 
+        public static int TotalCountAppearancePlaceables { get; set; }
+        public static int TotalCountAppearanceCreatures { get; set; }
+
+        public const int PAGESIZE = 50;
+
+        public static int GetPlaceableAppearancePageCount()
+        {
+            return PlaceableAppearanceByPage.Count;
+        }
+
+        public static int GetCreatureAppearancePageCount()
+        {
+            return CreatureAppearanceByPage.Count;
+        }
+
+        [NWNEventHandler("mod_cache")]
+        public static void CacheAppearances()
+        {
+            TotalCountAppearancePlaceables = 0;
+            TotalCountAppearanceCreatures = 0;            
+
+            var rowCount = UtilPlugin.Get2DARowCount("placeables");
+            var pageNumber = 0;
+            var actualRowCount = 0;
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                var entry = Get2DAString("placeables", "Label", row);
+
+                if (entry != "****" && !string.IsNullOrWhiteSpace(entry))
+                {
+                    if (actualRowCount > 0 && actualRowCount % PAGESIZE == 0) pageNumber++;
+
+                    if (!PlaceableAppearanceByPage.ContainsKey(pageNumber))
+                        PlaceableAppearanceByPage[pageNumber] = new Dictionary<int, string>();
+
+                    PlaceableAppearanceByPage[pageNumber].Add(row, entry);
+                    actualRowCount++;                 
+                }
+            }
+            TotalCountAppearancePlaceables = actualRowCount;
+            Console.WriteLine($"Loaded {PlaceableAppearanceByPage.Count} placeable appearance pages ({AreaTemplate.PAGESIZE} per page)  into cache.");
+
+            rowCount = UtilPlugin.Get2DARowCount("appearance");
+            pageNumber = 0;
+            actualRowCount = 0;
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                var entry = Get2DAString("appearance", "Label", row);
+
+                if (entry != "****" && !string.IsNullOrWhiteSpace(entry))
+                {
+                    if (actualRowCount > 0 && actualRowCount % PAGESIZE == 0) pageNumber++;
+
+                    if (!CreatureAppearanceByPage.ContainsKey(pageNumber))
+                        CreatureAppearanceByPage[pageNumber] = new Dictionary<int, string>();
+
+                    CreatureAppearanceByPage[pageNumber].Add(row, entry);
+                    actualRowCount++;
+                }
+            }
+            TotalCountAppearanceCreatures = actualRowCount;
+            Console.WriteLine($"Loaded {CreatureAppearanceByPage.Count} creature appearance pages ({AreaTemplate.PAGESIZE} per page)  into cache.");
+        }
+
+        [NWNEventHandler("mod_exit")]
+        public static void OnExit()
+        {
+            //return;
+
+            // For some reason I can't determine, the below is not actually working.
+            // I.E. The visual effect is not removed.
+            // The intent is to just have the OnClose GUI binding for this window called
+            // so that selected Object Highlight effect is cleaned up.
+            var player = GetExitingObject();
+            Console.WriteLine("Toggling Area Manager window before client disconnect. Name = " + GetName(player));
+            Gui.TogglePlayerWindow(player, GuiWindowType.AreaManager);
+            Gui.CloseWindow(player, GuiWindowType.AreaManager, player);
+            Console.WriteLine("Area Manager window closed.");                     
+        }
+
+        public static Dictionary<int, string> GetPlaceableAppearances(int pageNumber)
+        {
+            return PlaceableAppearanceByPage[pageNumber];
+        }
+        public static Dictionary<int, string> GetCreatureAppearances(int pageNumber)
+        {
+            return CreatureAppearanceByPage[pageNumber];
+        }
         public static void CacheTemplateArea(string resref, uint area)
         {
             TemplateAreasByResref[resref] = area;
@@ -32,7 +125,8 @@ namespace SWLOR.Game.Server.Service
                 // store transition targets to restore after CreateArea is called.
                 for (var x = GetFirstObjectInArea(area.Value); GetIsObjectValid(x); x = GetNextObjectInArea(area.Value))
                 {
-                    SetLocalObject(x, "ORIGINAL_TRANSITION_TARGET", GetTransitionTarget(x));
+                    if (GetTransitionTarget(x) != OBJECT_INVALID)
+                        SetLocalObject(x, "ORIGINAL_TRANSITION_TARGET", GetTransitionTarget(x));
                 }
 
                 var targetArea = CreateArea(GetResRef(area.Value), "template_" + GetTag(area.Value), "[Template] " + GetName(area.Value));
@@ -58,14 +152,18 @@ namespace SWLOR.Game.Server.Service
                 // restore transition targets
                 for (var x = GetFirstObjectInArea(area.Value); GetIsObjectValid(x); x = GetNextObjectInArea(area.Value))
                 {
-                    SetTransitionTarget(GetLocalObject(x, "ORIGINAL_TRANSITION_TARGET"), x);
-                    DeleteLocalObject(x, "ORIGINAL_TRANSITION_TARGET");
+                    var transitionTarget = GetLocalObject(x, "ORIGINAL_TRANSITION_TARGET");
+                    if (transitionTarget != OBJECT_INVALID)
+                    {
+                        SetTransitionTarget(transitionTarget, x);
+                        DeleteLocalObject(x, "ORIGINAL_TRANSITION_TARGET");
+                    }
                 }
 
                 // restore all save DM spawned objects
                 var query = new DBQuery<AreaTemplateObject>()
                     .AddFieldSearch(nameof(AreaTemplateObject.AreaResref), GetResRef(targetArea), false)
-                    .OrderBy(nameof(AreaTemplateObject.AreaResref));
+                    .OrderBy(nameof(AreaTemplateObject.ObjectName));
                 var areaTemplates = DB.Search(query)
                     .ToList();
 
@@ -180,5 +278,7 @@ namespace SWLOR.Game.Server.Service
                 DB.Set<AreaTemplateObject>(dbObject);
             }
         }
+
+
     }
 }

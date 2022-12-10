ca2d89aa1b060ba717022828ee0a6ba59383f969
Zunath <coolty3001@yahoo.com>
2022-12-10 10:37:26 -0500

    Add resource spawns to Dathomir areas.

--- SWLOR.Game.Server/Feature/SpawnDefinition/DathomirResourceSpawnDefinition.cs
+++ SWLOR.Game.Server/Feature/SpawnDefinition/DathomirResourceSpawnDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
 using SWLOR.Game.Server.Service.SpawnService;
 
 namespace SWLOR.Game.Server.Feature.SpawnDefinition
@@ -9,74 +10,214 @@ namespace SWLOR.Game.Server.Feature.SpawnDefinition
 
         public Dictionary<string, SpawnTable> BuildSpawnTables()
         {
-            CaveRuins();
             Desert();
             DesertWestSide();
             GrottoCaverns();
             Grottos();
-            MountainCaves();
             Mountains();
-            RuinBase();
             TarnishedJungles();
             TarnishedJunglesNorth();
             TribeVillage();
 
             return _builder.Build();
         }
-
-        private void CaveRuins()
-        {
-            _builder.Create("DATHOMIR_CAVE_RUINS_RESOURCES");
-        }
-
+        
         private void Desert()
         {
-            _builder.Create("DATHOMIR_DESERT_RESOURCES");
+            _builder.Create("DATHOMIR_DESERT_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(40)
+
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(20)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(10)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(10);
         }
 
         private void DesertWestSide()
         {
-            _builder.Create("DATHOMIR_DESERT_WEST_SIDE_RESOURCES");
+            _builder.Create("DATHOMIR_DESERT_WEST_SIDE_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(40)
+
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(15)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(15);
         }
 
         private void GrottoCaverns()
         {
-            _builder.Create("DATHOMIR_GROTTO_CAVERNS_RESOURCES");
+            _builder.Create("DATHOMIR_GROTTO_CAVERNS_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(20)
+
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(40)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(15)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(15);
         }
 
         private void Grottos()
         {
-            _builder.Create("DATHOMIR_GROTTOS_RESOURCES");
-        }
+            _builder.Create("DATHOMIR_GROTTOS_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(20)
 
-        private void MountainCaves()
-        {
-            _builder.Create("DATHOMIR_MOUNTAIN_CAVES_RESOURCES");
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(50)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(15)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(15);
         }
 
         private void Mountains()
         {
-            _builder.Create("DATHOMIR_MOUNTAINS_RESOURCES");
-        }
+            _builder.Create("DATHOMIR_MOUNTAINS_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(30)
 
-        private void RuinBase()
-        {
-            _builder.Create("DATHOMIR_RUIN_BASE_RESOURCES");
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(50)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(10)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(5);
         }
 
         private void TarnishedJungles()
         {
-            _builder.Create("DATHOMIR_TARNISHED_JUNGLES_RESOURCES");
+            _builder.Create("DATHOMIR_TARNISHED_JUNGLES_RESOURCES")
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(15)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(15);
         }
 
         private void TarnishedJunglesNorth()
         {
-            _builder.Create("DATHOMIR_TARNISHED_JUNGLES_NORTH_RESOURCES");
+            _builder.Create("DATHOMIR_TARNISHED_JUNGLES_NORTH_RESOURCES")
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(15)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(15);
         }
 
         private void TribeVillage()
         {
-            _builder.Create("DATHOMIR_TRIBE_VILLAGE_RESOURCES");
+            _builder.Create("DATHOMIR_TRIBE_VILLAGE_RESOURCES")
+                .AddSpawn(ObjectType.Placeable, "keromber_vein")
+                .WithFrequency(40)
+
+                .AddSpawn(ObjectType.Placeable, "jasioclase_vein")
+                .WithFrequency(20)
+
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_4")
+                .WithFrequency(10)
+                .AddSpawn(ObjectType.Placeable, "herbs_patch_5")
+                .WithFrequency(30)
+
+                .AddSpawn(ObjectType.Placeable, "patch_veggies4")
+                .WithFrequency(5)
+                .AddSpawn(ObjectType.Placeable, "patch_veggies5")
+                .WithFrequency(10)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_4")
+                .WithFrequency(5)
+
+                .AddSpawn(ObjectType.Placeable, "fiberp_bush_5")
+                .WithFrequency(10);
         }
 
     }

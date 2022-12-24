1f772416e6adcd2d7a3ee773f9e19986c9ac8fba
Valo56 <106187784+Valo56@users.noreply.github.com>
2022-12-24 09:38:18 -0800

    Korriban Dungeon, high level enemies, extremely difficult, good loot, new set of "Sacred" items. Also tweaks to Chirodactyl. (#1637)

--- SWLOR.Game.Server/Feature/LootTableDefinition/KorribanLootTableDefinition.cs
+++ SWLOR.Game.Server/Feature/LootTableDefinition/KorribanLootTableDefinition.cs
@@ -3,7 +3,7 @@ using SWLOR.Game.Server.Service.LootService;
 
 namespace SWLOR.Game.Server.Feature.LootTableDefinition
 {
-    public class KorribanLootTableDefinition: ILootTableDefinition
+    public class KorribanLootTableDefinition : ILootTableDefinition
     {
         private readonly LootTableBuilder _builder = new();
 
@@ -15,7 +15,7 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
             Wraid();
             PelkoSwarm();
             SithApprentice();
-
+            KorribanFortressLoot();
             SithCryptCrates();
 
             return _builder.Build();
@@ -171,6 +171,152 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
                 .AddItem("recipe_sithelec", 1, 1, true)
 
                 .AddGold(40, 10);
+        }
+
+        private void KorribanFortressLoot()
+        {
+            _builder.Create("KORRIBAN_FORTRESS_RESOURCES")
+                .AddItem("ref_arkoxit", 1, 1, true)
+                .AddItem("ref_jasioclase", 20)
+                .AddItem("ref_gostian", 20)
+                .AddItem("elec_high", 20)
+                .AddItem("lth_high", 20)
+                .AddItem("emerald", 10)
+                .AddItem("fiberp_high", 20)
+                .AddItem("hyphae_wood", 20);
+
+            _builder.Create("KORRIBAN_FORTRESS_GEAR")
+                .AddItem("dhar005s", 1)
+                .AddItem("dlar005s", 1)
+                .AddItem("sc_armor", 1)
+                .AddItem("sc_robes", 1)
+                .AddItem("sc_tunic", 1)
+                .AddItem("dhhl005s", 1)
+                .AddItem("dlhl005s", 1)
+                .AddItem("sc_cap", 1)
+                .AddItem("sc_crown", 1)
+                .AddItem("sc_helmet", 1)
+                .AddItem("sc_shield", 1)
+                .AddItem("dhbe005s", 1)
+                .AddItem("dlbe005s", 1)
+                .AddItem("sc_belt", 1)
+                .AddItem("sc_belt_ls", 1)
+                .AddItem("sc_belt_lf", 1)
+                .AddItem("dhlg005s", 1)
+                .AddItem("dllg005s", 1)
+                .AddItem("sc_boots", 1)
+                .AddItem("sc_leggings", 1)
+                .AddItem("sc_shoes", 1)
+                .AddItem("dhbr005s", 1)
+                .AddItem("sc_bracer", 1)
+                .AddItem("dhcl005s", 1)
+                .AddItem("dlcl005s", 1)
+                .AddItem("sc_cape", 1)
+                .AddItem("sc_cloak", 1)
+                .AddItem("sc_mantle", 1)
+                .AddItem("dlbr005s", 1)
+                .AddItem("sc_gloves", 1)
+                .AddItem("sc_wraps", 1)
+                .AddItem("dhnk005s", 1)
+                .AddItem("dlnk005s", 1)
+                .AddItem("sc_amulet", 1)
+                .AddItem("sc_necklace", 1)
+                .AddItem("sc_pendant", 1)
+                .AddItem("dhrg005s", 1)
+                .AddItem("dlrg005s", 1)
+                .AddItem("sc_band", 1)
+                .AddItem("sc_ring", 1)
+                .AddItem("sc_signet", 1)
+                .AddItem("sc_knife", 1)
+                .AddItem("sc_greatswor", 1)
+                .AddItem("electroblade_s", 1)
+                .AddItem("sc_longsword", 1)
+                .AddItem("sc_katar", 1)
+                .AddItem("sc_staff", 1)
+                .AddItem("sc_twinblade", 1)
+                .AddItem("sc_spear", 1)
+                .AddItem("sc_rifle", 1)
+                .AddItem("sc_pistol", 1)
+                .AddItem("sc_shuriken", 1);
+
+            _builder.Create("KORRIBAN_MASTER_RESOURCE")
+                .AddItem("chiro_shard", 1);
+
+            _builder.Create("KORRIBAN_FORTRESS_CREDITS")
+                .AddGold(500, 1);
+
+            _builder.Create("KORRIBAN_FORGE_KEY")
+                .AddItem("valkorrdkey2", 1);
+
+            _builder.Create("KORRIBAN_LORD_KEY")
+                .AddItem("valkorrdkey3", 1);
+
+            _builder.Create("KORRIBAN_COUNCIL_KEY")
+                .AddItem("valkorrdkey4", 1);
+
+            _builder.Create("KORRIBAN_GATE_KEY")
+                .AddItem("valkorrdkey1", 1);
+
+            _builder.Create("KORRIBAN_MASTER_RECIPE")
+                .AddItem("recipe_chigswd", 10)
+                .AddItem("recipe_chispear", 10)
+                .AddItem("recipe_chiknife", 10)
+                .AddItem("recipe_chipistol", 10)
+                .AddItem("recipe_chistaff", 10)
+                .AddItem("recipe_chilngswd", 10)
+                .AddItem("recipe_chikatar", 10)
+                .AddItem("recipe_chishuri", 10)
+                .AddItem("recipe_chirifle", 10)
+                .AddItem("recipe_chitwinbl", 10)
+                .AddItem("recipe_chielec", 10)
+                .AddItem("recipe_chitelec", 10)
+                .AddItem("recipe_chshield", 10)
+                .AddItem("recipe_chcloak", 10)
+                .AddItem("recipe_chbelt", 10)
+                .AddItem("recipe_chring", 10)
+                .AddItem("recipe_chneck", 10)
+                .AddItem("recipe_chbreast", 10)
+                .AddItem("recipe_chhelm", 10)
+                .AddItem("recipe_chbracer", 10)
+                .AddItem("recipe_chlegg", 10)
+                .AddItem("recipe_mgcloak", 10)
+                .AddItem("recipe_mgbelt", 10)
+                .AddItem("recipe_mgring", 10)
+                .AddItem("recipe_mgneck", 10)
+                .AddItem("recipe_mgtunic", 10)
+                .AddItem("recipe_mgcap", 10)
+                .AddItem("recipe_mggloves", 10)
+                .AddItem("recipe_mgboots", 10)
+                .AddItem("recipe_imcloak", 10)
+                .AddItem("recipe_imbelt", 10)
+                .AddItem("recipe_imring", 10)
+                .AddItem("recipe_imneck", 10)
+                .AddItem("recipe_imtunic", 10)
+                .AddItem("recipe_imcap", 10)
+                .AddItem("recipe_imgloves", 10)
+                .AddItem("recipe_imboots", 10)
+                .AddItem("recipe_dhcl005c", 10)
+                .AddItem("recipe_dhbe005c", 10)
+                .AddItem("recipe_dhrg005c", 10)
+                .AddItem("recipe_dhnk005c", 10)
+                .AddItem("recipe_dhar005c", 10)
+                .AddItem("recipe_dhhl005c", 10)
+                .AddItem("recipe_dhbr005c", 10)
+                .AddItem("recipe_dhlg005c", 10)
+                .AddItem("recipe_dlcl005c", 10)
+                .AddItem("recipe_dlbe005c", 10)
+                .AddItem("recipe_dlrg005c", 10)
+                .AddItem("recipe_dlnk005c", 10)
+                .AddItem("recipe_dlar005c", 10)
+                .AddItem("recipe_dlhl005c", 10)
+                .AddItem("recipe_dlbr005c", 10)
+                .AddItem("recipe_dllg005c", 10)
+                .AddItem("recipe_brsushi", 10)
+                .AddItem("recipe_ocsushi", 10)
+                .AddItem("recipe_iksushi", 10)
+                .AddItem("recipe_wisushi", 10)
+                .AddItem("recipe_tesushi", 10)
+                .AddItem("recipe_dosushi", 10);
         }
     }
 }

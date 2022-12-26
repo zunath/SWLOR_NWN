ffcf2f72153ae6207c8bb01ecec92cd43a7143c4
Zunath <coolty3001@yahoo.com>
2022-12-26 09:20:28 -0500

    Look at perk level rather than ability level for Rousing Shout.

--- SWLOR.Game.Server/Feature/ChatCommandDefinition/SystemChatCommand.cs
+++ SWLOR.Game.Server/Feature/ChatCommandDefinition/SystemChatCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using SWLOR.Game.Server.Core.NWNX;
 using SWLOR.Game.Server.Enumeration;
 using SWLOR.Game.Server.Service;
 using SWLOR.Game.Server.Service.GuiService;
@@ -20,6 +21,7 @@ namespace SWLOR.Game.Server.Feature.ChatCommandDefinition
             ListEmotesCommand(builder);
             StuckCommand(builder);
             EmotesWindowCommand(builder);
+            Test(builder);
 
             return builder.Build();
         }
@@ -124,6 +126,33 @@ namespace SWLOR.Game.Server.Feature.ChatCommandDefinition
                 .Action((user, target, location, args) =>
                 {
                     Gui.TogglePlayerWindow(user, GuiWindowType.Emotes);
+                });
+        }
+
+        private static void Test(ChatCommandBuilder builder)
+        {
+            builder.Create("test")
+                .Description("testing")
+                .Permissions(AuthorizationLevel.All)
+                .Action((user, target, location, args) =>
+                {
+                    var reflex = GetReflexSavingThrow(user);
+                    var will = GetWillSavingThrow(user);
+                    var fortitude = GetFortitudeSavingThrow(user);
+
+                    Console.WriteLine($"reflex = {reflex}");
+                    Console.WriteLine($"will = {will}");
+                    Console.WriteLine($"fortitude = {fortitude}");
+
+                    CreaturePlugin.SetBaseSavingThrow(user, SavingThrow.Will, 0);
+                    CreaturePlugin.SetBaseSavingThrow(user, SavingThrow.Reflex, 0);
+                    CreaturePlugin.SetBaseSavingThrow(user, SavingThrow.Fortitude, 0);
+
+
+                    Console.WriteLine($"after reflex = {reflex}");
+                    Console.WriteLine($"after will = {will}");
+                    Console.WriteLine($"after fortitude = {fortitude}");
+
                 });
         }
     }

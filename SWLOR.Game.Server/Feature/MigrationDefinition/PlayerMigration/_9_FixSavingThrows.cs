2f8724abbdfd100dc11ab6aed9e2ea0816ca9217
Zunath <coolty3001@yahoo.com>
2022-12-26 11:47:33 -0500

    Add player migration to adjust saving throws back to zero.

---
+++ SWLOR.Game.Server/Feature/MigrationDefinition/PlayerMigration/_9_FixSavingThrows.cs
@@ -0,0 +1,17 @@
+﻿using SWLOR.Game.Server.Core.NWNX;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+
+namespace SWLOR.Game.Server.Feature.MigrationDefinition.PlayerMigration
+{
+    public class _9_FixSavingThrows: PlayerMigrationBase
+    {
+        public override int Version => 9;
+        public override void Migrate(uint player)
+        {
+            // Addresses a problem with player saving throws being incorrect due to changes in the 2DA files.
+            CreaturePlugin.SetBaseSavingThrow(player, SavingThrow.Fortitude, 0);
+            CreaturePlugin.SetBaseSavingThrow(player, SavingThrow.Will, 0);
+            CreaturePlugin.SetBaseSavingThrow(player, SavingThrow.Reflex, 0);
+        }
+    }
+}

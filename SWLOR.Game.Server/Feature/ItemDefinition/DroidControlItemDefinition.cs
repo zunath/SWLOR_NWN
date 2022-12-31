cbae1852f585f35bf5e3cd85218893199b4dcbcf
Zunath <coolty3001@yahoo.com>
2022-12-31 15:48:36 -0500

    Stub out droid control unit item

---
+++ SWLOR.Game.Server/Feature/ItemDefinition/DroidControlItemDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Service.ItemService;
+
+namespace SWLOR.Game.Server.Feature.ItemDefinition
+{
+    public class DroidControlItemDefinition: IItemListDefinition
+    {
+        private readonly ItemBuilder _builder = new();
+
+        public Dictionary<string, ItemDetail> BuildItems()
+        {
+            DroidControlUnit();
+
+            return _builder.Build();
+        }
+
+        private void DroidControlUnit()
+        {
+            _builder.Create("droid_control")
+                .Delay(3f)
+                .PlaysAnimation(Animation.LoopingGetMid)
+                .ApplyAction((user, item, target, location) =>
+                {
+
+                });
+        }
+    }
+}

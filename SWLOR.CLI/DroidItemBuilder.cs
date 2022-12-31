731e65f3a8dca997d4ce1d8a2c5af14efda66bdc
Zunath <coolty3001@yahoo.com>
2022-12-31 13:50:34 -0500

    Add ability to generate droid parts. Generate them.

--- SWLOR.CLI/DroidItemBuilder.cs
+++ SWLOR.CLI/DroidItemBuilder.cs
@@ -8,8 +8,8 @@ namespace SWLOR.CLI
     internal class DroidItemBuilder
     {
         private const string InputData = "./InputFiles/droid_item_list.tsv";
-        //private const string Template = "./Templates/droid_cpu_template.json";
-        private const string Template = "./Templates/droid_part_template.json";
+        private const string CPUTemplate = "./Templates/droid_cpu_template.json";
+        private const string PartTemplate = "./Templates/droid_part_template.json";
         private const string OutputFolder = "./OutputDroidItems/";
 
         private readonly int[] _iconIds = { 1, 2, 3, 4, 5, 6, 9, 10, 12, 13, 14, 15, 16, 17, 20, 22, 23, 24, 25, 26, 27, 30, 31, 41, 42,
@@ -21,16 +21,83 @@ namespace SWLOR.CLI
         public void Process()
         {
             ClearOutputDirectory();
+            ProcessPart();
+        }
 
+        private const string StatTemplate = @",{
+        ""__struct_id"": 0,
+        ""ChanceAppear"": {
+          ""type"": ""byte"",
+          ""value"": 100
+        },
+        ""CostTable"": {
+          ""type"": ""byte"",
+          ""value"": 45
+        },
+        ""CostValue"": {
+          ""type"": ""word"",
+          ""value"": %%STATVALUE3%%
+        },
+        ""Param1"": {
+          ""type"": ""byte"",
+          ""value"": 255
+        },
+        ""Param1Value"": {
+          ""type"": ""byte"",
+          ""value"": 0
+        },
+        ""PropertyName"": {
+          ""type"": ""word"",
+          ""value"": 121
+        },
+        ""Subtype"": {
+          ""type"": ""word"",
+          ""value"": %%STATTYPE3%%
+        }
+      }";
+        private const string AISlotsTemplate = @",{
+        ""__struct_id"": 0,
+        ""ChanceAppear"": {
+          ""type"": ""byte"",
+          ""value"": 100
+        },
+        ""CostTable"": {
+          ""type"": ""byte"",
+          ""value"": 45
+        },
+        ""CostValue"": {
+          ""type"": ""word"",
+          ""value"": %%AISLOTS%%
+        },
+        ""Param1"": {
+          ""type"": ""byte"",
+          ""value"": 255
+        },
+        ""Param1Value"": {
+          ""type"": ""byte"",
+          ""value"": 0
+        },
+        ""PropertyName"": {
+          ""type"": ""word"",
+          ""value"": 121
+        },
+        ""Subtype"": {
+          ""type"": ""word"",
+          ""value"": 3
+        }
+      }";
+
+        private void ProcessCPU()
+        {
             var input2daText = File.ReadAllLines(InputData).ToList();
-            var templateText = File.ReadAllText(Template);
+            var templateText = File.ReadAllText(CPUTemplate);
 
             foreach (var line in input2daText)
             {
                 var parsed = line.Split('\t');
                 var name = parsed[0].Trim();
                 var resref = parsed[1].Trim();
-                var slotType = GetSlotType(parsed[2].Trim());
+                var (slotType, _) = GetSlotType(parsed[2].Trim());
                 var tier = string.IsNullOrWhiteSpace(parsed[3]) ? 0.ToString() : parsed[3].Trim();
                 var aiSlots = string.IsNullOrWhiteSpace(parsed[4]) ? 0.ToString() : parsed[4].Trim();
                 var hp = string.IsNullOrWhiteSpace(parsed[6]) ? 0.ToString() : parsed[6].Trim();
@@ -75,20 +142,83 @@ namespace SWLOR.CLI
             }
         }
 
-        private int GetSlotType(string type)
+        private void ProcessPart()
+        {
+            var input2daText = File.ReadAllLines(InputData).ToList();
+            var templateText = File.ReadAllText(PartTemplate);
+
+            foreach (var line in input2daText)
+            {
+                var parsed = line.Split('\t');
+                var name = parsed[0].Trim();
+                var resref = parsed[1].Trim();
+                var (slotType, iconId) = GetSlotType(parsed[2].Trim());
+                var tier = string.IsNullOrWhiteSpace(parsed[3]) ? 0.ToString() : parsed[3].Trim();
+                var aiSlots = string.IsNullOrWhiteSpace(parsed[4]) ? 0 : Convert.ToInt32(parsed[4].Trim());
+                //var hp = string.IsNullOrWhiteSpace(parsed[6]) ? 0 : parsed[6].Trim();
+                //var stm = string.IsNullOrWhiteSpace(parsed[7]) ? 0 : parsed[7].Trim();
+                var mgt = string.IsNullOrWhiteSpace(parsed[8]) ? 0 : Convert.ToInt32(parsed[8].Trim());
+                var per = string.IsNullOrWhiteSpace(parsed[9]) ? 0 : Convert.ToInt32(parsed[9].Trim());
+                var vit = string.IsNullOrWhiteSpace(parsed[10]) ? 0 : Convert.ToInt32(parsed[10].Trim());
+                var wil = string.IsNullOrWhiteSpace(parsed[11]) ? 0 : Convert.ToInt32(parsed[11].Trim());
+                var agi = string.IsNullOrWhiteSpace(parsed[12]) ? 0 : Convert.ToInt32(parsed[12].Trim());
+                var soc = string.IsNullOrWhiteSpace(parsed[13]) ? 0 : Convert.ToInt32(parsed[13].Trim());
+                //var oneHanded = string.IsNullOrWhiteSpace(parsed[14]) ? 0 : Convert.ToInt32(parsed[14].Trim());
+                //var twoHanded = string.IsNullOrWhiteSpace(parsed[15]) ? 0 : Convert.ToInt32(parsed[15].Trim());
+                //var martialArts = string.IsNullOrWhiteSpace(parsed[16]) ? 0 : Convert.ToInt32(parsed[16].Trim());
+                //var ranged = string.IsNullOrWhiteSpace(parsed[17]) ? 0 : Convert.ToInt32(parsed[17].Trim());
+                var stat3Template = string.Empty;
+                var aiSlotsTemplate = string.Empty;
+                var (stat1Type, stat1Value, stat2Type, stat2Value, stat3Type, stat3Value) = 
+                    GetStatPoints(mgt, per, agi, vit, wil, soc);
+
+                if (stat3Type > -1 && stat3Value > -1)
+                {
+                    stat3Template = StatTemplate
+                        .Replace("%%STATTYPE3%%", stat3Type.ToString())
+                        .Replace("%%STATVALUE3%%", stat3Value.ToString());
+                }
+
+                if (aiSlots > -1)
+                {
+                    aiSlotsTemplate = AISlotsTemplate
+                        .Replace("%%AISLOTS%%", aiSlots.ToString());
+                }
+
+                var json = templateText
+                    .Replace("%%NAME%%", name)
+                    .Replace("%%ICONID%%", iconId.ToString())
+                    .Replace("%%DROIDPARTTYPE%%", slotType.ToString())
+                    .Replace("%%AISLOTS%%", aiSlotsTemplate)
+                    .Replace("%%STATTYPE1%%", stat1Type.ToString())
+                    .Replace("%%STATVALUE1%%", stat1Value.ToString())
+                    .Replace("%%STATTYPE2%%", stat2Type.ToString())
+                    .Replace("%%STATVALUE2%%", stat2Value.ToString())
+                    .Replace("%%STAT3%%", stat3Template)
+                    .Replace("%%TIER%%", tier)
+                    .Replace("%%AISLOTS%%", aiSlotsTemplate)
+
+                    .Replace("%%TAG%%", resref)
+                    .Replace("%%RESREF%%", resref);
+
+                File.WriteAllText($"{OutputFolder}/{resref}.uti.json", json);
+            }
+        }
+
+        private (int, int) GetSlotType(string type)
         {
             switch (type)
             {
                 case "CPU":
-                    return 1;
+                    return (1, -1);
                 case "Head":
-                    return 2;
+                    return (2, 110);
                 case "Body":
-                    return 3;
+                    return (3, 120);
                 case "Arms":
-                    return 4;
+                    return (4, 37);
                 case "Legs":
-                    return 5;
+                    return (5, 90);
             }
 
             throw new ArgumentOutOfRangeException();
@@ -160,6 +290,128 @@ namespace SWLOR.CLI
             }
 
             return (skill1Id, skill1Value, skill2Id, skill2Value);
+        }
+
+        private (int, int, int, int, int, int) GetStatPoints(int mgt, int per, int agi, int vit, int wil, int soc)
+        {
+            var stat1Type = -1;
+            var stat1Value = -1;
+            var stat2Type = -1;
+            var stat2Value = -1;
+            var stat3Type = -1;
+            var stat3Value = -1;
+
+            if (mgt > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 6;
+                    stat1Value = mgt;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 6;
+                    stat2Value = mgt;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 6;
+                    stat3Value = mgt;
+                }
+            }
+            if (per > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 7;
+                    stat1Value = per;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 7;
+                    stat2Value = per;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 7;
+                    stat3Value = per;
+                }
+            }
+            if (vit > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 8;
+                    stat1Value = vit;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 8;
+                    stat2Value = vit;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 8;
+                    stat3Value = vit;
+                }
+            }
+            if (wil > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 9;
+                    stat1Value = wil;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 9;
+                    stat2Value = wil;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 9;
+                    stat3Value = wil;
+                }
+            }
+
+            if (agi > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 10;
+                    stat1Value = agi;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 10;
+                    stat2Value = agi;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 10;
+                    stat3Value = agi;
+                }
+            }
+            if (soc > 0)
+            {
+                if (stat1Type == -1)
+                {
+                    stat1Type = 11;
+                    stat1Value = soc;
+                }
+                else if (stat2Type == -1)
+                {
+                    stat2Type = 11;
+                    stat2Value = soc;
+                }
+                else if (stat3Type == -1)
+                {
+                    stat3Type = 11;
+                    stat3Value = soc;
+                }
+            }
+
+            return (stat1Type, stat1Value, stat2Type, stat2Value, stat3Type, stat3Value);
         }
 
         private void ClearOutputDirectory()

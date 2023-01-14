c71b323f27374d7514b6d5f245e75761a121a89a
Tyler <3136227+zunath@users.noreply.github.com>
2023-01-14 14:39:02 -0500

    Feature/server app (#1663)
    * Server app framework
    
    * Log colors
    
    * Add new Dockerfile image to include the debug dependencies on the image.
    
    * gitignore debugserver folder
    
    * Handle starting debugger from the runner
    
    * Deployment/build changes to support debugserver
    
    * Hak building fixes. Module copying
    
    * Config changes to make the debug server run from the repo folder instead of NWN.

--- SWLOR.CLI/DeployBuild.cs
+++ SWLOR.CLI/DeployBuild.cs
@@ -5,34 +5,56 @@ namespace SWLOR.CLI
 {
     internal class DeployBuild
     {
-        private const string IniFileName = "./nwnpath.config";
-        private const string DefaultDotnetFolder = "%USERPROFILE%/Documents/Neverwinter Nights/dotnet";
+        private const string DebugServerPath = "../debugserver/";
+        private const string DotnetPath = DebugServerPath + "dotnet";
+        private const string HakPath = DebugServerPath + "hak";
+        private const string ModulesPath = DebugServerPath + "modules";
+        private const string TlkPath = DebugServerPath + "tlk";
 
+        private readonly HakBuilder _hakBuilder = new();
 
         public void Process()
         {
-            var dotnetPath = File.Exists(IniFileName)
-                ? File.ReadAllText(IniFileName)
-                : DefaultDotnetFolder;
+            CreateDebugServerDirectory();
+            CopyBinaries();
+            BuildHaks();
+            BuildModule();
+        }
 
-            if (!Directory.Exists(dotnetPath))
-                dotnetPath = DefaultDotnetFolder;
+        private void CreateDebugServerDirectory()
+        {
+            Directory.CreateDirectory(DebugServerPath);
+            Directory.CreateDirectory(DotnetPath);
+            Directory.CreateDirectory(HakPath);
+            Directory.CreateDirectory(ModulesPath);
+            Directory.CreateDirectory(TlkPath);
 
-            dotnetPath = Environment.ExpandEnvironmentVariables(dotnetPath);
-
-            if (Directory.Exists(dotnetPath))
-                Directory.Delete(dotnetPath, true);
-
-            Directory.CreateDirectory(dotnetPath);
-
-            var binPath = "./bin/Debug/net6.0/";
-
-            var source = new DirectoryInfo(binPath);
-            var target = new DirectoryInfo(dotnetPath);
+            var source = new DirectoryInfo("../SWLOR.Game.Server/Docker");
+            var target = new DirectoryInfo(DebugServerPath);
 
             CopyAll(source, target);
         }
 
+        private void CopyBinaries()
+        {
+            var binPath = "../SWLOR.Game.Server/bin/Debug/net6.0/";
+
+            var source = new DirectoryInfo(binPath);
+            var target = new DirectoryInfo(DotnetPath);
+
+            CopyAll(source, target);
+        }
+
+        private void BuildHaks()
+        {
+            _hakBuilder.Process();
+        }
+
+        private void BuildModule()
+        {
+            var modulePath = "../Module/Star Wars LOR v2.mod";
+            File.Copy(modulePath, ModulesPath + "/Star Wars LOR v2.mod", true);
+        }
 
         private static void CopyAll(DirectoryInfo source, DirectoryInfo target)
         {

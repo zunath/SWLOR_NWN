f733c1799a705060cbc73ab34ad033f0f9a0e7d5
Zunath <coolty3001@yahoo.com>
2023-01-21 22:21:17 -0500

    Stub out beast ability definitions

---
+++ SWLOR.Game.Server/Feature/AbilityDefinition/Beasts/BalancedBeastAbilityDefinition.cs
@@ -0,0 +1,17 @@
+﻿using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Service.AbilityService;
+using System.Collections.Generic;
+
+namespace SWLOR.Game.Server.Feature.AbilityDefinition.Beasts
+{
+    public class BalancedBeastAbilityDefinition : IAbilityListDefinition
+    {
+        private readonly AbilityBuilder _builder = new();
+
+        public Dictionary<FeatType, AbilityDetail> BuildAbilities()
+        {
+
+            return _builder.Build();
+        }
+    }
+}

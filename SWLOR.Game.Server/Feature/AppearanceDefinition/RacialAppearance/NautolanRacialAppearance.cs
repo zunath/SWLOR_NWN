6eefe172b2d098b3d0c3db03c72fa83b1413286c
Scott Finlay <scottfinlay@protonmail.com>
2023-03-14 19:18:57 -0500

    Nautolan ID fix, Droid IDs, texture fix
    * Fixed Nautolans misaligned internal ID
    * Adjusted droid IDs to fit new part list
    * Fixed texture on Thigh 121 for non-Humans

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/NautolanRacialAppearance.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/NautolanRacialAppearance.cs
@@ -2,7 +2,7 @@ namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
 {
     public class NautolanRacialAppearanceDefinition : RacialAppearanceBaseDefinition
     {
-        public override int[] MaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246 };
-        public override int[] FemaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246 };
+        public override int[] MaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247 };
+        public override int[] FemaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247 };
     }
 }

032267a4b0f5c05a3def04476d2e02246054ef06
Zunath <coolty3001@yahoo.com>
2023-04-05 09:53:02 -0400

    Add missing assault status effect

---
+++ SWLOR.Game.Server/Feature/StatusEffectDefinition/BeastStatusEffectDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using SWLOR.Game.Server.Core.NWScript.Enum;
+using SWLOR.Game.Server.Service.StatusEffectService;
+
+namespace SWLOR.Game.Server.Feature.StatusEffectDefinition
+{
+    public class BeastStatusEffectDefinition: IStatusEffectListDefinition
+    {
+        private readonly StatusEffectBuilder _builder = new();
+
+        public Dictionary<StatusEffectType, StatusEffectDetail> BuildStatusEffects()
+        {
+            Assault();
+
+            return _builder.Build();
+        }
+
+        private void Assault()
+        {
+            _builder.Create(StatusEffectType.Assault)
+                .Name("Assault")
+                .EffectIcon(EffectIconType.ACIncrease);
+        }
+    }
+}

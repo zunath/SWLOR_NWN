2cfd604be327de14a882b457226eef6e5dea0523
Zunath <coolty3001@yahoo.com>
2023-05-28 15:14:44 -0400

    Add method comments to BeastBuilder

--- SWLOR.Game.Server/Service/BeastMasteryService/BeastBuilder.cs
+++ SWLOR.Game.Server/Service/BeastMasteryService/BeastBuilder.cs
@@ -10,6 +10,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
         private BeastDetail _activeBeast;
         private BeastLevel _activeLevel;
 
+        /// <summary>
+        /// Creates a new beast configuration.
+        /// </summary>
+        /// <param name="type">The type of beast to associate with this configuration.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder Create(BeastType type)
         {
             _activeBeast = new BeastDetail();
@@ -18,6 +23,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the appearance type of the beast.
+        /// </summary>
+        /// <param name="appearance">The appearance to use.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder Appearance(AppearanceType appearance)
         {
             _activeBeast.Appearance = appearance;
@@ -25,6 +35,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the portrait used for the beast.
+        /// </summary>
+        /// <param name="portraitId">The Id of the portrait</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder PortraitId(int portraitId)
         {
             _activeBeast.PortraitId = portraitId;
@@ -32,6 +47,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the sound set used for the beast.
+        /// </summary>
+        /// <param name="soundSetId">The Id of the sound set</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder SoundSetId(int soundSetId)
         {
             _activeBeast.SoundSetId = soundSetId;
@@ -39,6 +59,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the role used by the beast. Roles affect available perks.
+        /// </summary>
+        /// <param name="role">The type of role to configure</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder Role(BeastRoleType role)
         {
             _activeBeast.Role = role;
@@ -46,6 +71,12 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the accuracy and damage stats used by the beast.
+        /// </summary>
+        /// <param name="accuracy">The stat used for accuracy calculations</param>
+        /// <param name="damage">The stat used for damage calculations</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder CombatStats(AbilityType accuracy, AbilityType damage)
         {
             _activeBeast.AccuracyStat = accuracy;
@@ -54,6 +85,10 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Adds a new level to be used by the beast.
+        /// </summary>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder AddLevel()
         {
             var level = _activeBeast.Levels.Count + 1;
@@ -64,6 +99,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the HP assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="amount">The amount of HP to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder HP(int amount)
         {
             _activeLevel.HP = amount;
@@ -71,6 +111,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the FP assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="amount">The amount of FP to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder FP(int amount)
         {
             _activeLevel.FP = amount;
@@ -78,6 +123,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the STM assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="amount">The amount of STM to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder STM(int amount)
         {
             _activeLevel.STM = amount;
@@ -85,6 +135,11 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the DMG assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="amount">The amount of DMG to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder DMG(int amount)
         {
             _activeLevel.DMG = amount;
@@ -92,6 +147,12 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the value of a specific stat for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="type">The stat to assign</param>
+        /// <param name="value">The amount of the stat to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder Stat(AbilityType type, int value)
         {
             _activeLevel.Stats[type] = value;
@@ -99,18 +160,35 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the max attack bonus assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="max">The max amount of Attack to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder MaxAttackBonus(int max)
         {
             _activeLevel.MaxAttackBonus = max;
 
             return this;
         }
+
+        /// <summary>
+        /// Specifies the max accuracy bonus assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="max">The max amount of Accuracy to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder MaxAccuracyBonus(int max)
         {
             _activeLevel.MaxAccuracyBonus = max;
 
             return this;
         }
+
+        /// <summary>
+        /// Specifies the max evasion bonus assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="max">The max amount of Evasion to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder MaxEvasionBonus(int max)
         {
             _activeLevel.MaxEvasionBonus = max;
@@ -118,6 +196,12 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the max defense bonus assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="type">The damage type.</param>
+        /// <param name="max">The max amount of Defense to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder MaxDefenseBonus(CombatDamageType type, int max)
         {
             _activeLevel.MaxDefenseBonuses[type] = max;
@@ -125,6 +209,12 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Specifies the max saving throw bonus assigned for the beast at the current level being configured.
+        /// </summary>
+        /// <param name="type">The saving throw type.</param>
+        /// <param name="max">The max amount of Saving Throw Bonus to assign for this level.</param>
+        /// <returns>A configured BeastBuilder object</returns>
         public BeastBuilder MaxSavingThrowBonus(SavingThrow type, int max)
         {
             _activeLevel.MaxSavingThrowBonuses[type] = max;
@@ -132,6 +222,10 @@ namespace SWLOR.Game.Server.Service.BeastMasteryService
             return this;
         }
 
+        /// <summary>
+        /// Builds the beast's definition.
+        /// </summary>
+        /// <returns>A collection of beast details.</returns>
         public Dictionary<BeastType, BeastDetail> Build()
         {
             return _beasts;

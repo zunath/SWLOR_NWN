25db75da93a078a39ac750a4f62f9ea8bdeb8a3e
Zunath <coolty3001@yahoo.com>
2023-07-19 22:29:53 -0400

    Stub out incubator UI design

--- SWLOR.Game.Server/Feature/GuiDefinition/IncubatorDefinition.cs
+++ SWLOR.Game.Server/Feature/GuiDefinition/IncubatorDefinition.cs
@@ -12,7 +12,280 @@ namespace SWLOR.Game.Server.Feature.GuiDefinition
                 .SetIsResizable(true)
                 .SetIsCollapsible(true)
                 .SetInitialGeometry(0, 0, 1000f, 600f)
-                .SetTitle("Incubator");
+                .SetTitle("Incubator")
+                
+                .AddColumn(col =>
+                {
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("DNA")
+                            .SetHeight(20f);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddButtonImage()
+                            .BindImageResref(model => model.DNAItemResref)
+                            .BindOnClicked(model => model.OnClickDNA())
+                            .SetTooltip("Select DNA")
+                            .SetHeight(64f)
+                            .SetWidth(64f);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+                        row.AddLabel()
+                            .SetText("Hydrolase");
+
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("Lyase");
+
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("Isomerase");
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddButtonImage()
+                            .BindImageResref(model => model.HydrolaseItemResref)
+                            .BindOnClicked(model => model.OnClickDNA())
+                            .SetTooltip("Select Hydrolase")
+                            .SetHeight(64f)
+                            .SetWidth(64f);
+
+                        row.AddSpacer();
+
+                        row.AddButtonImage()
+                            .BindImageResref(model => model.LyaseItemResref)
+                            .BindOnClicked(model => model.OnClickLyase())
+                            .SetTooltip("Select Lyase")
+                            .SetHeight(64f)
+                            .SetWidth(64f);
+
+                        row.AddSpacer();
+
+                        row.AddButtonImage()
+                            .BindImageResref(model => model.IsomeraseItemResref)
+                            .BindOnClicked(model => model.OnClickIsomerase())
+                            .SetTooltip("Select Isomerase")
+                            .SetHeight(64f)
+                            .SetWidth(64f);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+                        row.AddLabel()
+                            .SetText("Purities");
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("Mutation")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Attack")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Accuracy")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Evasion")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Learning")
+                            .SetColor(0, 255, 0);
+
+                        row.AddSpacer();
+                    });
+
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddSpacer();
+                    });
+
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("Phys. Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Force Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Fire Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Pois. Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Elec. Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddSpacer();
+                    });
+
+
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("Ice Def")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Fortitude")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Reflex")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("Will")
+                            .SetColor(0, 255, 0);
+
+                        row.AddLabel()
+                            .SetText("XP Penalty")
+                            .SetColor(255, 0, 0);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddLabel()
+                            .SetText("0 [+0.0%]");
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddLabel()
+                            .BindText(model => model.EstimatedTimeToCompletion);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddCheckBox()
+                            .SetText(" Use Erratic Genius")
+                            .BindIsChecked(model => model.IsErraticGeniusChecked)
+                            .BindIsEnabled(model => model.IsErraticGeniusEnabled);
+
+                        row.AddSpacer();
+                    });
+
+                    col.AddRow(row =>
+                    {
+                        row.AddSpacer();
+
+                        row.AddButton()
+                            .SetText("Start Job")
+                            .BindOnClicked(model => model.OnClickStartJob())
+                            .BindIsEnabled(model => model.IsStartJobEnabled);
+
+                        row.AddSpacer();
+                    });
+
+                });
 
             return _builder.Build();
         }
--- SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/IncubatorViewModel.cs
+++ SWLOR.Game.Server/Feature/GuiDefinition/ViewModel/IncubatorViewModel.cs
@@ -1,12 +1,257 @@
-﻿using SWLOR.Game.Server.Service.GuiService;
+﻿using System;
+using SWLOR.Game.Server.Service.GuiService;
 
 namespace SWLOR.Game.Server.Feature.GuiDefinition.ViewModel
 {
     public class IncubatorViewModel: GuiViewModelBase<IncubatorViewModel, GuiPayloadBase>
     {
+        private const string _blank = "Blank";
+
+        private string _dnaItem;
+        private string _hydrolaseItem;
+        private string _isomeraseItem;
+        private string _lyaseItem;
+
+        public string DNAItemResref
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string HydrolaseItemResref
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string IsomeraseItemResref
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string LyaseItemResref
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public bool IsStartJobEnabled
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public bool IsErraticGeniusEnabled
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public string CurrentExperimentationStage
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string EstimatedTimeToCompletion
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public bool IsErraticGeniusChecked
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        public string CurrentMutationChance
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string CurrentAttackPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentAccuracyPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentEvasionPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentLearningPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentPhysicalDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentForceDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentFireDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentPoisonDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentElectricalDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentIceDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentFortitudePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentReflexPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string CurrentWillPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string CurrentXPPenalty
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string BonusAttackPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusAccuracyPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusEvasionPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusLearningPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusPhysicalDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusForceDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusFireDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusPoisonDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusElectricalDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusIceDefensePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusFortitudePurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusReflexPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string BonusWillPurity
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public string ExtraXPPenalty
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public string BonusMutationChance
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
         protected override void Initialize(GuiPayloadBase initialPayload)
         {
-            
+            DNAItemResref = _blank;
+            HydrolaseItemResref = _blank;
+            IsomeraseItemResref = _blank;
+            LyaseItemResref = _blank;
+
+            WatchOnClient(model => model.IsErraticGeniusChecked);
         }
+
+        public Action OnClickDNA() => () =>
+        {
+
+        };
+
+        public Action OnClickHydrolase() => () =>
+        {
+
+        };
+
+        public Action OnClickLyase() => () =>
+        {
+
+        };
+
+        public Action OnClickIsomerase() => () =>
+        {
+
+        };
+
+        public Action OnClickStartJob() => () =>
+        {
+
+        };
+
     }
 }

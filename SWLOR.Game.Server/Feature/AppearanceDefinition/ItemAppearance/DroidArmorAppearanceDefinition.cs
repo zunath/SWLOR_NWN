efcf35f80027f3b963fe95877c895bd77b3be96b
SHOVELSANDINTOMYGAPINGMOUTH <littleincubus13@gmail.com>
2024-01-08 03:42:17 +0800

    new definitions for new armor

--- SWLOR.Game.Server/Feature/AppearanceDefinition/ItemAppearance/DroidArmorAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/ItemAppearance/DroidArmorAppearanceDefinition.cs
@@ -4,19 +4,19 @@
     {
         public override int[] Helmet { get; } = {0 };
         public override int[] Cloak { get; } = {0 };
-        public override int[] Neck { get; } = {0, 1, 2, 7, 200, 201, 202, 203, 204, 205, 206, };
-        public override int[] Torso { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214 };
+        public override int[] Neck { get; } = {0, 1, 2, 7, 200, 201, 202, 203, 204, 205, 206, 207 };
+        public override int[] Torso { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215 };
         public override int[] Belt { get; } = { 0 };
-        public override int[] Pelvis { get; } = {1, 2, 3, 4, 5, 6, 7, 8 };
+        public override int[] Pelvis { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
                 
         public override int[] Shoulder { get; } = {0, };
-        public override int[] Bicep { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212 };
-        public override int[] Forearm { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] Hand { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211 };
+        public override int[] Bicep { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213 };
+        public override int[] Forearm { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215 };
+        public override int[] Hand { get; } = {1, 2, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212 };
                 
-        public override int[] Thigh { get; } = {1, 2, 3, 4, 5, 6, 7, 8  };
-        public override int[] Shin { get; } = {1, 2, 3, 4, 5, 6, 7, 8 };
-        public override int[] Foot { get; } = {1, 2, 3, 4, 5, 6, 7, 8 };
+        public override int[] Thigh { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public override int[] Shin { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public override int[] Foot { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
         public override int[] Robe { get; } = {0, };
     }
 }

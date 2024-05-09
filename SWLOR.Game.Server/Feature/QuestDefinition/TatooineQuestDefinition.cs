70fc0cf96ce2b4d2cdf899e8a3eb7c680e5726e0
NZNightwolf <Joshuaroycox@gmail.co.nz>
2024-05-09 21:59:32 +1200

    Fabrication Upates / Area Edits

--- SWLOR.Game.Server/Feature/QuestDefinition/TatooineQuestDefinition.cs
+++ SWLOR.Game.Server/Feature/QuestDefinition/TatooineQuestDefinition.cs
@@ -11,6 +11,7 @@ namespace SWLOR.Game.Server.Feature.QuestDefinition
             var builder = new QuestBuilder();
             WorkinForTheMan(builder);
             StinkyWomprats(builder);
+            TuskenRampage(builder);
 
             return builder.Build();
         }
@@ -42,6 +43,43 @@ namespace SWLOR.Game.Server.Feature.QuestDefinition
 
                 .AddGoldReward(1000)
                 .AddXPReward(1750);
+        }
+
+        private static void TuskenRampage(QuestBuilder builder)
+        {
+            builder.Create("tusken_rampage", "Tusken Rampage")
+
+                .AddState()
+                .SetStateJournalText("The Militia wants you to kill Fifty Tusken Raider's.")
+                .AddKillObjective(NPCGroupType.Tatooine_TuskenRaider, 50)
+
+                .AddState()
+                .SetStateJournalText("That was easier then it should have been. Report back to that man.")
+
+                .AddState()
+                .SetStateJournalText("Another Fifty, that killed moisture farmers this time. Easy enough.")
+                .AddKillObjective(NPCGroupType.Tatooine_TuskenRaider, 50)
+
+                .AddState()
+                .SetStateJournalText("Those were dealt with. Hopefully the moisture is farmed more now. Back to the man.")
+
+                .AddState()
+                .SetStateJournalText("Another Fifty... Apparently this time they're going after the trading routes.")
+                .AddKillObjective(NPCGroupType.Tatooine_TuskenRaider, 50)
+
+                .AddState()
+                .SetStateJournalText("Well, that was quick. Report back to the man once more.")
+
+                .AddState()
+                .SetStateJournalText("Now he wants you to kill Five Hundred of them... Better get to work.")
+                .AddKillObjective(NPCGroupType.Tatooine_TuskenRaider, 50)
+
+                .AddState()
+                .SetStateJournalText("Fifty... You've killed Fifty... There is no way you're doing another Four Hundred. Go talk to that man!")
+
+                .AddGoldReward(10000)
+                .AddXPReward(10000)
+                .AddItemReward("recipe_hazrdwall", 1);
         }
     }
 }

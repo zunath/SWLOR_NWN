ca1fb220d3c7a8432f963da88c89e332f1e71bfe
Guil Kishin <guil_sama@hotmail.com>
2024-06-20 15:01:19 -0300

    test

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/EwokRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/EwokRacialAppearanceDefinition.cs
@@ -2,6 +2,8 @@ namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
 {
     public class EwokRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
+        public override float MaximumScale => 0.9f;
+        public override float MinimumScale => 0.7f;
         public override int[] MaleHeads { get; } = { 111, 112, 113, 114 };
         public override int[] FemaleHeads { get; } = { 111, 112, 113, 114 };
     }

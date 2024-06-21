4dc2b7425a5b76349d49860b0b074f99fa04aa41
Guil Kishin <guil_sama@hotmail.com>
2024-06-21 14:59:29 -0300

    Ewok bodie test

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/EwokRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/EwokRacialAppearanceDefinition.cs
@@ -6,5 +6,20 @@ namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
         public override float MinimumScale => 0.7f;
         public override int[] MaleHeads { get; } = { 111, 112, 113, 114 };
         public override int[] FemaleHeads { get; } = { 111, 112, 113, 114 };
+
+        public override int[] Torsos { get; } = { 208, 209 };
+        public override int[] Pelvis { get; } = { 208, 209 };
+        public override int[] RightBicep { get; } = { 208 };
+        public override int[] RightForearm { get; } = { 208 };
+        public override int[] RightHand { get; } = { 208 };
+        public override int[] RightThigh { get; } = { 208 };
+        public override int[] RightShin { get; } = { 208 };
+        public override int[] RightFoot { get; } = { 208 };
+        public override int[] LeftBicep { get; } = { 208 };
+        public override int[] LeftForearm { get; } = { 208 };
+        public override int[] LeftHand { get; } = { 208 };
+        public override int[] LeftThigh { get; } = { 208 };
+        public override int[] LeftShin { get; } = { 208 };
+        public override int[] LeftFoot { get; } = { 208 };
     }
 }

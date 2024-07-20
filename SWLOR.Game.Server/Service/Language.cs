6995c0b5dd7135425727966ad8c2ff361143d9a5
BamgaLab <106176791+BamgaLab@users.noreply.github.com>
2024-07-19 23:33:30 -0300

    Ewok Race (#1848)
    * Create EwokRacialAppearanceDefinition
    
    * Update RacialType.cs (Ewok)
    
    * Update EwokRacialAppearanceDefinition (Ewok)
    
    * Update AppearanceEditorViewModel.cs (Ewok)
    
    * Update Race.cs (Ewok)
    
    * Create TranslatorEwokese (Ewok)
    
    * Update TranslatorEwokese
    
    * Update and rename TranslatorEwokese to TranslatorEwokese.cs
    
    * Update Language.cs (Ewok)
    
    * Update SkillType.cs (Ewok)
    
    * Update PlayerInitialization.cs (Ewok)
    
    * Update TranslatorEwokese.cs
    
    * some clean-up
    
    * test
    
    * Ewok bodie test
    
    * body test
    
    * chest test 2
    
    * torso test
    
    * Racial changes (Ewok)
    
    * size and base apearan tweaks
    
    * Fix formatting weirdness.
    
    * Update Language.cs
    
    * Update SkillType.cs
    
    ---------
    
    Co-authored-by: Guil Kishin <guil_sama@hotmail.com>
    Co-authored-by: Tyler <3136227+zunath@users.noreply.github.com>

--- SWLOR.Game.Server/Service/Language.cs
+++ SWLOR.Game.Server/Service/Language.cs
@@ -40,7 +40,8 @@ namespace SWLOR.Game.Server.Service
                 { SkillType.MonCalamarian, new TranslatorMonCalamarian() },
                 { SkillType.Ugnaught, new TranslatorUgnaught() },
                 { SkillType.KelDor, new TranslatorKelDor() },
-                { SkillType.Nautila, new TranslatorNautila() }
+                { SkillType.Nautila, new TranslatorNautila() },
+                { SkillType.Ewokese, new TranslatorEwokese() },
             };
         }
 
@@ -204,6 +205,7 @@ namespace SWLOR.Game.Server.Service
                 case SkillType.MonCalamarian: r = 128; g = 128; b = 192; break;
                 case SkillType.Ugnaught: r = 255; g = 193; b = 233; break;
                 case SkillType.Nautila: r = 76; g = 230; b = 104; break;
+                case SkillType.Ewokese: r = 112; g = 28; b = 28; break;
             }
 
             return (r, g, b);
@@ -230,6 +232,7 @@ namespace SWLOR.Game.Server.Service
                 case SkillType.MonCalamarian: return "Mon Calamarian";
                 case SkillType.Ugnaught: return "Ugnaught";
                 case SkillType.Nautila: return "Nautila";
+                case SkillType.Ewokese: return "Ewokese";
             }
 
             return "Basic";
@@ -287,6 +290,7 @@ namespace SWLOR.Game.Server.Service
                         new LanguageCommand("Twi'leki", SkillType.Twileki, new []{"twileki", "ryl"}),
                         new LanguageCommand("Ugnaught", SkillType.Ugnaught, new []{"ugnaught"}),
                         new LanguageCommand("Zabraki", SkillType.Zabraki, new []{"zabraki", "zabrak"}),
+                        new LanguageCommand("Ewokese", SkillType.Ewokese, new []{"Ewokese", "YubNub"}),
                     };
 
                     _languages = languages;

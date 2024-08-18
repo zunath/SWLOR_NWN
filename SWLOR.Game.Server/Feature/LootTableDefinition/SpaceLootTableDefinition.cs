8e9b47704356a0e739dfe71e80f43daeaeafc933
Taelon <kivash0081@yahoo.com>
2024-08-18 13:08:45 -0400

    SpaceLootTable
    Added Rare Tables

--- SWLOR.Game.Server/Feature/LootTableDefinition/SpaceLootTableDefinition.cs
+++ SWLOR.Game.Server/Feature/LootTableDefinition/SpaceLootTableDefinition.cs
@@ -21,28 +21,24 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
         private void Tier1()
         {
             _builder.Create("SPACE_BASIC_T1")
-                .AddItem("elec_ruined", 7, 1)
-                .AddItem("ref_tilarium", 5, 1)
-                .AddGold(500, 2)
-                .AddItem("ref_veldite", 2, 1)
-                .AddItem("ship_missile", 2, 4)
-                .AddItem("ship_fuelcapsule", 1, 1);
-
-            _builder.Create("SPACE_BASIC_T1_RARES")
-                .IsRare()
+                .AddItem("elec_ruined", 3, 1)
+                .AddItem("ref_tilarium", 3, 1)
+                .AddGold(300, 1)
+                .AddItem("ref_veldite", 1, 1)
+                .AddItem("ship_missile", 1, 4)
                 .AddItem("jade", 1, 1, true)
                 .AddItem("aluminum", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T1")
-                .AddItem("elec_ruined", 3, 1)
-                .AddItem("ref_tilarium", 3, 1)
-                .AddGold(2000, 1)
-                .AddItem("ref_veldite", 3, 1)
-                .AddItem("lth_ruined", 2, 1)
+                .AddItem("elec_ruined", 1, 1)
+                .AddItem("ref_tilarium", 1, 1)
+                .AddGold(300, 1)
+                .AddItem("ref_veldite", 1, 1)
+                .AddItem("lth_ruined", 1, 1)
                 .AddItem("jade", 1, 1)
                 .AddItem("aluminum", 1, 1)
-                .AddItem("wood", 3, 1)
-                .AddItem("fiberp_ruined", 3, 1)
+                .AddItem("wood", 1, 1)
+                .AddItem("fiberp_ruined", 1, 1)
                 .AddItem("quadrenium", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T1")
@@ -53,28 +49,24 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
         private void Tier2()
         {
             _builder.Create("SPACE_BASIC_T2")
-                .AddItem("elec_flawed", 7, 1)
-                .AddItem("ref_scordspar", 5, 1)
-                .AddGold(750, 2)
-                .AddItem("ref_currian", 2, 1)
-                .AddItem("ship_fuelcapsule", 1, 2)
-                .AddItem("ship_missile", 2, 6);
-
-            _builder.Create("SPACE_BASIC_T2_RARES")
-                .IsRare()
+                .AddItem("elec_flawed", 3, 1)
+                .AddItem("ref_scordspar", 3, 1)
+                .AddGold(600, 1)
+                .AddItem("ref_currian", 1, 1)
+                .AddItem("ship_missile", 1, 6)
                 .AddItem("agate", 1, 1, true)
                 .AddItem("steel", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T2")
-                .AddItem("elec_flawed", 3, 1)
-                .AddItem("ref_scordspar", 3, 1)
-                .AddGold(1000, 1)
-                .AddItem("ref_currian", 3, 1)
-                .AddItem("lth_flawed", 3, 2)
+                .AddItem("elec_flawed", 1, 1)
+                .AddItem("ref_scordspar", 1, 1)
+                .AddGold(600, 1)
+                .AddItem("ref_currian", 1, 1)
+                .AddItem("lth_flawed", 1, 1)
                 .AddItem("agate", 1, 1)
                 .AddItem("steel", 1, 1)
-                .AddItem("fine_wood", 3, 2)
-                .AddItem("fiberp_flawed", 3, 2)
+                .AddItem("fine_wood", 1, 1)
+                .AddItem("fiberp_flawed", 1, 1)
                 .AddItem("vintrium", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T2")
@@ -85,28 +77,24 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
         private void Tier3()
         {
             _builder.Create("SPACE_BASIC_T3")
-                .AddItem("elec_good", 7, 1)
-                .AddItem("ref_plagionite", 5, 1)
-                .AddGold(1000, 2)
-                .AddItem("ref_idailia", 2, 1)
-                .AddItem("ship_fuelcapsule", 1, 3)
-                .AddItem("ship_missile", 2, 8);
-
-            _builder.Create("SPACE_BASIC_T3_RARES")
-                .IsRare()
+                .AddItem("elec_good", 3, 1)
+                .AddItem("ref_plagionite", 3, 1)
+                .AddGold(900, 1)
+                .AddItem("ref_idailia", 1, 1)
+                .AddItem("ship_missile", 1, 8)
                 .AddItem("citrine", 1, 1, true)
                 .AddItem("obsidian", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T3")
-                .AddItem("elec_good", 3, 1)
-                .AddItem("ref_plagionite", 3, 1)
-                .AddGold(1250, 1)
-                .AddItem("ref_idailia", 3, 1)
-                .AddItem("lth_good", 3, 2)
+                .AddItem("elec_good", 1, 1)
+                .AddItem("ref_plagionite", 1, 1)
+                .AddGold(900, 1)
+                .AddItem("ref_idailia", 1, 1)
+                .AddItem("lth_good", 1, 1)
                 .AddItem("citrine", 1, 1)
                 .AddItem("obsidian", 1, 1)
-                .AddItem("ancient_wood", 3, 2)
-                .AddItem("fiberp_good", 3, 2)
+                .AddItem("ancient_wood", 1, 1)
+                .AddItem("fiberp_good", 1, 1)
                 .AddItem("ionite", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T3")
@@ -117,28 +105,24 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
         private void Tier4()
         {
             _builder.Create("SPACE_BASIC_T4")
-                .AddItem("elec_imperfect", 7, 1)
-                .AddItem("ref_keromber", 5, 1)
-                .AddGold(1250, 1)
-                .AddItem("ref_barinium", 2, 1)
-                .AddItem("ship_fuelcapsule", 1, 4)
-                .AddItem("ship_missile", 2, 10);
-
-            _builder.Create("SPACE_BASIC_T4_RARES")
-                .IsRare()
+                .AddItem("elec_imperfect", 3, 1)
+                .AddItem("ref_keromber", 3, 1)
+                .AddGold(1500, 1)
+                .AddItem("ref_barinium", 1, 1)
+                .AddItem("ship_missile", 1, 10)
                 .AddItem("crystal", 1, 1, true)
                 .AddItem("ruby", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T4")
-                .AddItem("elec_imperfect", 3, 1)
-                .AddItem("ref_keromber", 3, 1)
+                .AddItem("elec_imperfect", 1, 1)
+                .AddItem("ref_keromber", 1, 1)
                 .AddGold(1500, 1)
-                .AddItem("ref_barinium", 3, 1)
-                .AddItem("lth_imperfect", 3, 2)
+                .AddItem("ref_barinium", 1, 1)
+                .AddItem("lth_imperfect", 1, 1)
                 .AddItem("crystal", 1, 1)
                 .AddItem("ruby", 1, 1)
-                .AddItem("aracia_wood", 3, 2)
-                .AddItem("fiberp_imperfect", 3, 2)
+                .AddItem("aracia_wood", 1, 1)
+                .AddItem("fiberp_imperfect", 1, 1)
                 .AddItem("katrium", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T4")
@@ -149,62 +133,54 @@ namespace SWLOR.Game.Server.Feature.LootTableDefinition
         private void Tier5()
         {
             _builder.Create("SPACE_BASIC_T5")
-                .AddItem("elec_high", 7, 1)
-                .AddItem("ref_gostian", 5, 1)
-                .AddGold(1500, 1)
-                .AddItem("ref_jasioclase", 2, 1)
-                .AddItem("ship_fuelcapsule", 1, 5)
-                .AddItem("ship_missile", 2, 12);
-            
-            _builder.Create("SPACE_BASIC_T5_RARES")
-                .IsRare()
+                .AddItem("elec_high", 3, 1)
+                .AddItem("ref_gostian", 3, 1)
+                .AddGold(2000, 1)
+                .AddItem("ref_jasioclase", 1, 1)
+                .AddItem("ship_missile", 1, 12)
                 .AddItem("diamond", 1, 1, true)
                 .AddItem("emerald", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T5")
-                .AddItem("elec_high", 3, 1)
-                .AddItem("ref_gostian", 3, 1)
+                .AddItem("elec_high", 1, 1)
+                .AddItem("ref_gostian", 1, 1)
                 .AddGold(2000, 1)
-                .AddItem("ref_jasioclase", 3, 1)
-                .AddItem("lth_high", 3, 2)
+                .AddItem("ref_jasioclase", 1, 1)
+                .AddItem("lth_high", 1, 1)
                 .AddItem("diamond", 1, 1)
                 .AddItem("emerald", 1, 1)
-                .AddItem("hyphae_wood", 3, 2)
-                .AddItem("fiberp_high", 3, 2)
+                .AddItem("hyphae_wood", 1, 1)
+                .AddItem("fiberp_high", 1, 1)
                 .AddItem("proton_bomb", 1, 1, true)
                 .AddItem("zinsiam", 1, 1, true)
-                .AddItem("ref_arda", 2, 1, true);
+                .AddItem("ref_arda", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T5")
                 .AddGold(10000, 1)
                 .AddItem("zinsiam", 1, 2);
 
             _builder.Create("SPACE_BASIC_T6")
-                .AddItem("elec_high", 8, 2)
-                .AddItem("ref_gostian", 5, 2)
-                .AddGold(3000, 2)
-                .AddItem("ref_jasioclase", 2, 2)
-                .AddItem("ship_fuelcapsule", 1, 6)
-                .AddItem("ship_missile", 2, 15);
-
-            _builder.Create("SPACE_BASIC_T6_RARES")
-                .IsRare()
+                .AddItem("elec_high", 3, 2)
+                .AddItem("ref_gostian", 3, 2)
+                .AddGold(3000, 1)
+                .AddItem("ref_jasioclase", 1, 2)
+                .AddItem("ship_missile", 1, 15)
                 .AddItem("diamond", 1, 1, true)
                 .AddItem("emerald", 1, 1, true);
 
             _builder.Create("SPACE_ELITE_T6")
-                .AddItem("elec_high", 4, 2)
-                .AddItem("ref_gostian", 4, 2)
+                .AddItem("elec_high", 1, 2)
+                .AddItem("ref_gostian", 1, 2)
                 .AddGold(3000, 1)
-                .AddItem("ref_jasioclase", 2, 1)
-                .AddItem("lth_high", 2, 2)
-                .AddItem("diamond", 3, 1)
-                .AddItem("emerald", 3, 1)
-                .AddItem("hyphae_wood", 3, 2)
-                .AddItem("fiberp_high", 3, 2)
-                .AddItem("proton_bomb", 2, 1, true)
-                .AddItem("zinsiam", 2, 1, true)
-                .AddItem("ref_arda", 3, 1, true);
+                .AddItem("ref_jasioclase", 1, 1)
+                .AddItem("lth_high", 1, 1)
+                .AddItem("diamond", 1, 1)
+                .AddItem("emerald", 1, 1)
+                .AddItem("hyphae_wood", 1, 1)
+                .AddItem("fiberp_high", 1, 1)
+                .AddItem("proton_bomb", 1, 1, true)
+                .AddItem("zinsiam", 1, 1, true)
+                .AddItem("ref_arda", 1, 1, true);
 
             _builder.Create("SPACE_BOSS_T6")
                 .AddGold(15000, 1)

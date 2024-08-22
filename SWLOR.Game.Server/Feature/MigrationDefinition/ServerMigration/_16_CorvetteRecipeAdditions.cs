523f390c804e5df3d195da582a2d2f499d362a4c
Taelon <kivash0081@yahoo.com>
2024-08-22 17:25:10 -0400

    Added  Jehavey'ir Corvette
    Added  Jehavey'ir Corvette
    Added Migration To Add New Recipe to chaters who already know the Corvette Hulls.

---
+++ SWLOR.Game.Server/Feature/MigrationDefinition/ServerMigration/_16_CorvetteRecipeAdditions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWLOR.Game.Server.Core.Bioware;
+using SWLOR.Game.Server.Core.NWNX;
+using SWLOR.Game.Server.Core.NWScript.Enum.Item;
+using SWLOR.Game.Server.Entity;
+using SWLOR.Game.Server.Service;
+using SWLOR.Game.Server.Service.DBService;
+using SWLOR.Game.Server.Service.LogService;
+using SWLOR.Game.Server.Service.MigrationService;
+
+namespace SWLOR.Game.Server.Feature.MigrationDefinition.ServerMigration
+{
+    public class _16_CorvetteRecipeAdditions: ServerMigrationBase, IServerMigration
+    {
+        public int Version => 16;
+        public MigrationExecutionType ExecutionType => MigrationExecutionType.PostCacheLoad;
+        public void Migrate()
+        {
+            var query = new DBQuery<Player>();
+            var count = (int)DB.SearchCount(query);
+            var dbPlayers = DB.Search(query
+                .AddPaging(count, 0));
+
+            foreach (var player in dbPlayers)
+            {
+                if (player.UnlockedRecipes.ContainsKey(Service.CraftService.RecipeType.CorvetteNeutThranta) == true)
+                {
+                    player.UnlockedRecipes[Service.CraftService.RecipeType.CorvetteJehaveyFrigate] = DateTime.UtcNow;
+                }
+                DB.Set(player);
+            }
+        }
+    }
+}

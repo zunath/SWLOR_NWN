5e71741e93b82c7fe3322ebaa77ebfe64cfd9036
Scott Finlay <scottfinlay@protonmail.com>
2025-03-05 19:25:52 -0600

    new droid part IDs
    new part IDs for droids

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/DroidRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/DroidRacialAppearanceDefinition.cs
@@ -3,8 +3,8 @@
     public class DroidRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
         public override float MaximumScale => 1.3f;
-        public override int[] MaleHeads { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
-        public override int[] FemaleHeads { get; } = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
+        public override int[] MaleHeads { get; } = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
+        public override int[] FemaleHeads { get; } = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
 
         public override int[] Torsos { get; } = { 1 };
         public override int[] Pelvis { get; } = { 1, };

1b9334a31c1b1bf36dc2e9b85b4af0368a4fa462
Zunath <coolty3001@yahoo.com>
2025-09-19 21:48:24 -0400

    Remove SWLOR.Web. Move Readmes to root, add as project folder

--- SWLOR.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SWLOR.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

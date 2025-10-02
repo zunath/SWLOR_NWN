f4cd48095a628fe68f6718d0e01a72d6e1f6e825
zunath <coolty3001@yahoo.com>
2025-10-02 16:40:48 -0400

    Add missing InfrastructureLogGroup

---
+++ SWLOR.Shared.Core/Log/LogGroup/InfrastructureLogGroup.cs
@@ -0,0 +1,12 @@
+﻿using SWLOR.Shared.Abstractions.Contracts;
+using SWLOR.Shared.Abstractions.Enums;
+
+namespace SWLOR.Shared.Core.Log.LogGroup
+{
+    public class InfrastructureLogGroup: ILogGroup
+    {
+        public string Name => "Attack";
+        public ServerEnvironmentType EnvironmentType => ServerEnvironmentType.Development | ServerEnvironmentType.Test;
+        public bool AlwaysPrintToConsole => false;
+    }
+}

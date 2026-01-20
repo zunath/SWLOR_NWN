a6969a0f22498a7ddd4a8253651c995ba086237d
Scott Finlay <scottfinlay@protonmail.com>
2026-01-20 15:27:02 -0600

    New IDs
    Raised IDs up to 300 to give myself room to work

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/ZabrakRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/ZabrakRacialAppearanceDefinition.cs
@@ -3,7 +3,7 @@
     public class ZabrakRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
         public override float MaximumScale => 1.3f;
-        public override int[] MaleHeads { get; } = { 56, 57, 58, 59, 60, 61, 62, 103 };
-        public override int[] FemaleHeads { get; } = { 38, 69, 70, 71, 72, 73, 120 };
+        public override int[] MaleHeads { get; } = { 56, 57, 58, 59, 60, 61, 62, 103, 266 };
+        public override int[] FemaleHeads { get; } = { 38, 69, 70, 71, 72, 73, 120, 259 };
     }
 }

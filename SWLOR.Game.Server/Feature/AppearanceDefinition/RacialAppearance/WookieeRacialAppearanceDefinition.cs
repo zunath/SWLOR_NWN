3bc2265b74debf519e5279bc1a9034e153338f23
Scott Finlay <scottfinlay@protonmail.com>
2026-02-27 05:10:52 -0600

    Removed more unused IDs
    only using 209 for chest and pelvis, not limbs

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/WookieeRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/WookieeRacialAppearanceDefinition.cs
@@ -8,17 +8,17 @@
         public override int[] FemaleHeads { get; } = { 110, 185, 186, 190, 192, 193, 195, 196, 197, 198, 199};
         public override int[] Torsos { get; } = { 208, 209, 210, 211, 212, 213, 214};
         public override int[] Pelvis { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightBicep { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightForearm { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightHand { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightThigh { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightShin { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] RightFoot { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftBicep { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftForearm { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftHand { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftThigh { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftShin { get; } = { 208, 209, 210, 211, 212, 213, 214 };
-        public override int[] LeftFoot { get; } = { 208, 209, 210, 211, 212, 213, 214 };
+        public override int[] RightBicep { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] RightForearm { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] RightHand { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] RightThigh { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] RightShin { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] RightFoot { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftBicep { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftForearm { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftHand { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftThigh { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftShin { get; } = { 208, 210, 211, 212, 213, 214 };
+        public override int[] LeftFoot { get; } = { 208, 210, 211, 212, 213, 214 };
     }
 }

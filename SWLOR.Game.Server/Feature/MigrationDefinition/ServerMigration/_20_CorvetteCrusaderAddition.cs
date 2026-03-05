96de592906451fd0d669cd4c0fdfd25452581a64
Leudoberct1 <49315400+Leudoberct1@users.noreply.github.com>
2026-03-05 20:27:00 +0000

    Changes migration number to 20

---
+++ SWLOR.Game.Server/Feature/MigrationDefinition/ServerMigration/_20_CorvetteCrusaderAddition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SWLOR.Game.Server.Entity;
+using SWLOR.Game.Server.Service;
+using SWLOR.Game.Server.Service.DBService;
+using SWLOR.Game.Server.Service.MigrationService;
+
+namespace SWLOR.Game.Server.Feature.MigrationDefinition.ServerMigration
+{
+    public class _20_CorvetteCrusaderAddition: ServerMigrationBase, IServerMigration
+    {
+        public int Version => 16;
+        public MigrationExecutionType ExecutionType => MigrationExecutionType.PostDatabaseLoad;
+        public void Migrate()
+        {
+            var query = new DBQuery<Player>();
+            var count = (int)DB.SearchCount(query);
+            var dbPlayers = DB.Search(query
+                .AddPaging(count, 0));
+
+            foreach (var player in dbPlayers)
+            {
+                if (player.UnlockedRecipes.ContainsKey(Service.CraftService.RecipeType.CorvetteNeutThranta))
+                {
+                    player.UnlockedRecipes[Service.CraftService.RecipeType.CorvetteCrusader] = DateTime.UtcNow;
+                }
+                DB.Set(player);
+            }
+        }
+    }
+}

1cffd3d9a29aeb6e8fba02a107995b4f77c20be3
Scott Finlay <scottfinlay@protonmail.com>
2026-03-16 16:28:33 -0500

    New Head IDs for Nautolan, Zabrak
    Removed unused IDs for Human

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/NautolanRacialAppearance.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/NautolanRacialAppearance.cs
@@ -3,6 +3,6 @@ namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
     public class NautolanRacialAppearanceDefinition : RacialAppearanceBaseDefinition
     {
         public override int[] MaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247 };
-        public override int[] FemaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 257 };
+        public override int[] FemaleHeads { get; } = { 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 257, 278 };
     }
 }

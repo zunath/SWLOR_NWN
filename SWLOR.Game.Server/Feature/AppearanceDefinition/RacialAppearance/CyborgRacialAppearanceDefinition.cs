f16d669d79fac530d423efff43a4b7af1674f5f9
Scott Finlay <scottfinlay@protonmail.com>
2026-04-10 21:51:07 -0500

    new head IDs for Chiss/Echani/Mirialan/Cyborg
    Added new head IDs for Chiss, Echani, Mirialan and Cyborg.

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/CyborgRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/CyborgRacialAppearanceDefinition.cs
@@ -2,8 +2,8 @@
 {
     public class CyborgRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
-        public override int[] MaleHeads { get; } = { 74, 88, 156, 168, 181, 187 };
-        public override int[] FemaleHeads { get; } = { 41, 109 };
+        public override int[] MaleHeads { get; } = { 74, 88, 156, 168, 181, 187, 267, 268, 269, 270, 271, 272, 273, 274 };
+        public override int[] FemaleHeads { get; } = { 41, 109, 262, 263, 264, 265, 266, 267, 268, 269 };
 
     }
 }

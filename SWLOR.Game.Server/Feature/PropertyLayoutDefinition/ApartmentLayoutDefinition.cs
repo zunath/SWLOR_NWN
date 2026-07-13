14c436d9eaeefa7c7b9bdb9eb92090218bf0e344
Aephyros <annaoman5@gmail.com>
2026-07-13 13:42:06 -0500

    New Apartments

--- SWLOR.Game.Server/Feature/PropertyLayoutDefinition/ApartmentLayoutDefinition.cs
+++ SWLOR.Game.Server/Feature/PropertyLayoutDefinition/ApartmentLayoutDefinition.cs
@@ -50,9 +50,33 @@ namespace SWLOR.Game.Server.Feature.PropertyLayoutDefinition
                 .InitialPrice(7000)
                 .PricePerDay(300)
                 .AreaInstance("playerap_s_fur");
-        }
 
-        private void MediumApartments()
+			_builder.Create(PropertyLayoutType.ApartmentSmallStyle2)
+                .PropertyType(PropertyType.Apartment)
+                .Name("Player Apartment: Small - Unfurnished")
+                .StructureLimit(30)
+                .ItemStorageLimit(25)
+                .BuildingLimit(0)
+                .ResearchDeviceLimit(0)
+                .InitialPrice(7000)
+                .PricePerDay(300)
+                .AreaInstance("v_apartment_s00");
+
+			_builder.Create(PropertyLayoutType.ApartmentSmallStyle2Furnished)
+                .PropertyType(PropertyType.Apartment)
+                .Name("Player Apartment: Small - Furnished")
+                .StructureLimit(30)
+                .ItemStorageLimit(25)
+                .BuildingLimit(0)
+                .ResearchDeviceLimit(0)
+                .InitialPrice(7000)
+                .PricePerDay(300)
+                .PricePerDay(300)
+				.AreaInstance("v_apartment_s01");
+
+		}
+
+		private void MediumApartments()
         {
             _builder.Create(PropertyLayoutType.ApartmentMediumStyle1)
                 .PropertyType(PropertyType.Apartment)
@@ -86,9 +110,32 @@ namespace SWLOR.Game.Server.Feature.PropertyLayoutDefinition
                 .InitialPrice(13000)
                 .PricePerDay(400)
                 .AreaInstance("playerap_m_fur");
-        }
 
-        private void LargeApartments()
+			_builder.Create(PropertyLayoutType.ApartmentMediumStyle2)
+			    .PropertyType(PropertyType.Apartment)
+			    .Name("Player Apartment: Medium - Unfurnished")
+			    .StructureLimit(50)
+			    .ItemStorageLimit(40)
+			    .BuildingLimit(0)
+			    .ResearchDeviceLimit(0)
+			    .InitialPrice(13000)
+			    .PricePerDay(400)
+			    .AreaInstance("v_apartment_m00");
+
+			_builder.Create(PropertyLayoutType.ApartmentMediumStyle2Furnished)
+                .PropertyType(PropertyType.Apartment)
+                .Name("Player Apartment: Medium - Furnished")
+                .StructureLimit(50)
+                .ItemStorageLimit(40)
+                .BuildingLimit(0)
+                .ResearchDeviceLimit(0)
+                .InitialPrice(13000)
+                .PricePerDay(400)
+                .AreaInstance("v_apartment_m01");
+
+		}
+
+		private void LargeApartments()
         {
             _builder.Create(PropertyLayoutType.ApartmentLargeStyle1)
                 .PropertyType(PropertyType.Apartment)

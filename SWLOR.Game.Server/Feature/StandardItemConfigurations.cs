f0ed3a24ea192ce4b02a774974522bcd3402c6e9
Tyler <3136227+zunath@users.noreply.github.com>
2026-07-21 18:51:33 -0500

    Adopt native sling identity for pistols

--- SWLOR.Game.Server/Feature/StandardItemConfigurations.cs
+++ SWLOR.Game.Server/Feature/StandardItemConfigurations.cs
@@ -55,7 +55,7 @@ namespace SWLOR.Game.Server.Feature
                     BaseItem.Scimitar,
                     BaseItem.Scythe,
                     BaseItem.Pistol,
-                    BaseItem.PistolWithShield,
+                    BaseItem.LegacyPistol,
                     BaseItem.ShortSpear,
                     BaseItem.ShortSword,
                     BaseItem.Shuriken,

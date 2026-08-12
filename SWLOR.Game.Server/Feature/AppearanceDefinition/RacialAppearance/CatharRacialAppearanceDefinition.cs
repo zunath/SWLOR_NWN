468f9f961885fd4e348709eb3ec16f5898eb8b29
Scott Finlay <scottfinlay@protonmail.com>
2026-08-12 18:04:14 -0500

    New Head IDs
    Human/Chiss/Echani/Mirialan Male: 307 - 323
    Human/Chiss/Echani/Mirialan Female: 278 - 290, 310 - 313
    Cathar Female: 19 - 24
    Zabrak Female: 315

--- SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/CatharRacialAppearanceDefinition.cs
+++ SWLOR.Game.Server/Feature/AppearanceDefinition/RacialAppearance/CatharRacialAppearanceDefinition.cs
@@ -1,9 +1,9 @@
-namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
+﻿namespace SWLOR.Game.Server.Feature.AppearanceDefinition.RacialAppearance
 {
     public class CatharRacialAppearanceDefinition: RacialAppearanceBaseDefinition
     {
         public override int[] MaleHeads { get; } = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 26, 27, 28, 29, };
-        public override int[] FemaleHeads { get; } = { 1, 2, 3, 4, 6, 7, 8, 9, 13, 14, 18 };
+        public override int[] FemaleHeads { get; } = { 1, 2, 3, 4, 6, 7, 8, 9, 13, 14, 18, 19, 20, 21, 22, 23, 24};
         public override int[] Torsos { get; } = { 201 };
         public override int[] Pelvis { get; } = { 201 };
         public override int[] RightBicep { get; } = { 201 };

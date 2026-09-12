0bd490711984ecd6fffbfcb057d93157a5272b65
Zunath <coolty3001@yahoo.com>
2026-09-12 14:11:20 -0400

    Remove unrelated server and API warning cleanup

--- SWLOR.Game.Server/Service/GuiService/Component/GuiWidget.cs
+++ SWLOR.Game.Server/Service/GuiService/Component/GuiWidget.cs
@@ -45,7 +45,7 @@ namespace SWLOR.Game.Server.Service.GuiService.Component
         private string IsEncouragedBindName { get; set; }
         private bool IsEncouragedBound => !string.IsNullOrWhiteSpace(IsEncouragedBindName);
 
-        private GuiColor Color { get; set; }
+        private GuiColor? Color { get; set; }
         private string ColorBindName { get; set; }
         private bool IsColorBound => !string.IsNullOrWhiteSpace(ColorBindName);
 
--- SWLOR.NWN.API/NWNX/Enum/QuickBarSlot.cs
+++ SWLOR.NWN.API/NWNX/Enum/QuickBarSlot.cs
@@ -8,10 +8,10 @@ namespace SWLOR.Game.Server.Core.NWNX.Enum
         public uint? SecondaryItem { get; set; }
         public QuickBarSlotType ObjectType { get; set; }
         public int MultiClass { get; set; }
-        public string? Resref { get; set; }
-        public string? CommandLabel { get; set; }
-        public string? CommandLine { get; set; }
-        public string? ToolTip { get; set; }
+        public string Resref { get; set; }
+        public string CommandLabel { get; set; }
+        public string CommandLine { get; set; }
+        public string ToolTip { get; set; }
         public int INTParam1 { get; set; }
         public int MetaType { get; set; }
         public int DomainLevel { get; set; }

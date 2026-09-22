b72f7b0abfda0f68403f317ec9c03788f1b0ba61
Scorchys12 <93101257+Scorchys12@users.noreply.github.com>
2026-09-21 23:03:12 -0400

    Wire Eshan quest targets into live spawns

---
+++ SWLOR.Game.Server/Feature/SpawnDefinition/EshanSpawnDefinition.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using SWLOR.Game.Server.Service.SpawnService;
+using SWLOR.NWN.API.NWScript.Enum;
+
+namespace SWLOR.Game.Server.Feature.SpawnDefinition
+{
+    public class EshanSpawnDefinition : ISpawnListDefinition
+    {
+        private readonly SpawnTableBuilder _builder = new();
+
+        public Dictionary<string, SpawnTable> BuildSpawnTables()
+        {
+            EshanSilverwoodExpanse();
+            EshanWinterMarches();
+            EshanBattlegrounds();
+
+            return _builder.Build();
+        }
+
+        private void EshanSilverwoodExpanse()
+        {
+            _builder.Create("ESHAN_SILVERWOOD_EXPANSE", "Eshan - Silverwood Expanse")
+                .AddSpawn(ObjectType.Creature, "esh_direwolf")
+                .WithFrequency(100)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_frostwolf")
+                .WithFrequency(60)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_wolfalpha")
+                .WithFrequency(10)
+                .RandomlyWalks()
+                .ReturnsHome();
+        }
+
+        private void EshanWinterMarches()
+        {
+            _builder.Create("ESHAN_WINTER_MARCHES", "Eshan - Winter Marches")
+                .AddSpawn(ObjectType.Creature, "esh_nc_scout")
+                .WithFrequency(100)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_nc_hunter")
+                .WithFrequency(70)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_nc_vanguard")
+                .WithFrequency(50)
+                .RandomlyWalks()
+                .ReturnsHome();
+        }
+
+        private void EshanBattlegrounds()
+        {
+            _builder.Create("ESHAN_BATTLEGROUNDS", "Eshan - Battlegrounds")
+                .AddSpawn(ObjectType.Creature, "esh_nc_heavy")
+                .WithFrequency(100)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_nc_medic")
+                .WithFrequency(80)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_nc_captain")
+                .WithFrequency(40)
+                .RandomlyWalks()
+                .ReturnsHome()
+
+                .AddSpawn(ObjectType.Creature, "esh_sunguard")
+                .WithFrequency(20)
+                .RandomlyWalks()
+                .ReturnsHome();
+        }
+    }
+}

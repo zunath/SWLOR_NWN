1ba65244ff635183644473a8b23f3cb62dc93e11
Scorchys12 <93101257+Scorchys12@users.noreply.github.com>
2026-09-21 21:16:12 -0400

    Fix Eshan creature quest and loot inheritance

---
+++ SWLOR.Game.Server/Feature/LootTableDefinition/EshanLootTableDefinition.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using SWLOR.Game.Server.Service.LootService;
+
+namespace SWLOR.Game.Server.Feature.LootTableDefinition
+{
+    public class EshanLootTableDefinition : ILootTableDefinition
+    {
+        private readonly LootTableBuilder _builder = new();
+
+        public Dictionary<string, LootTable> BuildLootTables()
+        {
+            _builder.Create("ESHAN_NEOCRUSADER")
+                .AddItem("esh_mando_salv", 10, 2)
+                .AddGold(150, 5);
+
+            _builder.Create("ESHAN_DIRE_WOLF")
+                .AddItem("esh_wolf_pelt", 10, 2);
+
+            _builder.Create("ESHAN_FROST_WOLF")
+                .AddItem("esh_wolf_pelt", 10, 2)
+                .AddItem("esh_frost_fang", 4, 1);
+
+            _builder.Create("ESHAN_DIRE_WOLF_ALPHA")
+                .AddItem("esh_wolf_pelt", 10, 3)
+                .AddItem("esh_frost_fang", 10, 2);
+
+            _builder.Create("ESHAN_SUN_GUARD")
+                .AddItem("esh_sun_insignia", 10, 1)
+                .AddItem("esh_mando_salv", 5, 2)
+                .AddGold(500, 10);
+
+            return _builder.Build();
+        }
+    }
+}
